97e4eafd61f22c758ce4492ceb978ada99f104d3
Alexander Harris <alharris@amazon.com>
2022-03-16 14:27:17 -0700

    chore: updates after minor inter-package dep changes were pushed

--- scripts/split-transformer-packages.sh
+++ scripts/split-transformer-packages.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Install common tools
+brew install jq
+npm i -g relaxed-json
+
 #######################################################
 # https://github.com/aws-amplify/amplify-category-api #
 #######################################################
@@ -65,10 +69,6 @@ git reset --hard cli/master
     amplify-util-uibuilder \
     amplify-velocity-template)
 
-# Add missing type dependencies
-yarn add -D @types/glob -W
-lerna add --dev @types/lodash packages/amplify-e2e-tests
-
 # Remove references to packaging step
 rm -rf pkg
 jq 'del(.scripts."pkg-clean", .scripts."pkg-all", .scripts."pkg-all-local")' package.json > package.json.updated
@@ -83,83 +83,20 @@ jq '(.scripts."link-dev", .scripts."link-win") |= sub("packages/amplify-cli";"no
 mv package.json.updated package.json
 
 # Do CLI hoisting stuff (stolen from codegen builds)
-jq '.scripts."setup-dev" = "(yarn && lerna run build) && yarn add-cli-no-save && (yarn hoist-cli && yarn rm-dev-link && yarn link-dev)"' package.json > package.json.updated
+jq '.scripts."setup-dev" = "(yarn && lerna run build) && yarn add-cli-no-save && (yarn hoist-cli && yarn rm-dev-link && yarn link-dev)" | .scripts."setup-dev-win" = "(yarn && lerna run build) && yarn add-cli-no-save && (yarn hoist-cli && yarn rm-dev-link && yarn link-win)" | .scripts."add-cli-no-save" = "yarn add @aws-amplify/cli -W && git restore package.json" | .scripts."hoist-cli" = "rimraf node_modules/amplify-cli && mkdir node_modules/amplify-cli && cp -r node_modules/@aws-amplify/cli/ node_modules/amplify-cli"' package.json > package.json.updated
 mv package.json.updated package.json
-jq '.scripts."setup-dev-win" = "(yarn && lerna run build) && yarn add-cli-no-save && (yarn hoist-cli && yarn rm-dev-link && yarn link-win)"' package.json > package.json.updated
-mv package.json.updated package.json
-jq '.scripts."add-cli-no-save" = "yarn add @aws-amplify/cli -W && git restore package.json"' package.json > package.json.updated
-mv package.json.updated package.json
-jq '.scripts."hoist-cli" = "rimraf node_modules/amplify-cli && mkdir node_modules/amplify-cli && cp -r node_modules/@aws-amplify/cli/ node_modules/amplify-cli"' package.json > package.json.updated
-mv package.json.updated package.json
-
-# Remove references to amplify-app packages and steps
-# removed references to amplify-app *aa build targets
-# TODO
 
 # Remove files not related to api category
 rm amplifycli_react_tutorial.md
 rm native_guide.md
 
-# Update unit test with undeclared dependency
-cat << EOM > packages/graphql-transformer-core/src/__tests__/util/amplifyUtils.test.ts
-import { getSanityCheckRules, SanityCheckRules } from '../../util/amplifyUtils';
-import { FeatureFlags } from 'amplify-cli-core';
-
-jest.mock('amplify-cli-core');
-
-const buildMockedFeatureFlags = (flagValue: boolean) => {
-  return {
-    getBoolean: jest.fn(() => flagValue),
-    getString: jest.fn(),
-    getNumber: jest.fn(),
-    getObject: jest.fn(),
-  };
-};
-
-describe('get sanity check rules', () => {
-  test('empty list when api is in create status', () => {
-    const sanityCheckRules: SanityCheckRules = getSanityCheckRules(true, buildMockedFeatureFlags(true));
-    expect(sanityCheckRules.diffRules.length).toBe(0);
-    expect(sanityCheckRules.projectRules.length).toBe(0);
-  });
-
-  test('sanitycheck rule list when api is in update status and ff enabled', () => {
-    const sanityCheckRules: SanityCheckRules = getSanityCheckRules(false, buildMockedFeatureFlags(true));
-    const diffRulesFn = sanityCheckRules.diffRules.map(func => func.name);
-    const projectRulesFn = sanityCheckRules.projectRules.map(func => func.name);
-    expect(diffRulesFn).toMatchSnapshot();
-    expect(projectRulesFn).toMatchSnapshot();
-  });
-
-  test('sanitycheck rule list when api is in update status and no ff enabled', () => {
-    const sanityCheckRules: SanityCheckRules = getSanityCheckRules(false, buildMockedFeatureFlags(false));
-    const diffRulesFn = sanityCheckRules.diffRules.map(func => func.name);
-    const projectRulesFn = sanityCheckRules.projectRules.map(func => func.name);
-    expect(diffRulesFn).toMatchSnapshot();
-    expect(projectRulesFn).toMatchSnapshot();
-  });
-
-  test('sanity check rule list when destructive changes flag is present and ff enabled', () => {
-    const sanityCheckRules: SanityCheckRules = getSanityCheckRules(false, buildMockedFeatureFlags(true), true);
-    const diffRulesFn = sanityCheckRules.diffRules.map(func => func.name);
-    const projectRulesFn = sanityCheckRules.projectRules.map(func => func.name);
-    expect(diffRulesFn).toMatchSnapshot();
-    expect(projectRulesFn).toMatchSnapshot();
-  });
-
-  test('sanity check rule list when destructive changes flag is present but ff not enabled', () => {
-    const sanityCheckRules: SanityCheckRules = getSanityCheckRules(false, buildMockedFeatureFlags(false), true);
-    const diffRulesFn = sanityCheckRules.diffRules.map(func => func.name);
-    const projectRulesFn = sanityCheckRules.projectRules.map(func => func.name);
-    expect(diffRulesFn).toMatchSnapshot();
-    expect(projectRulesFn).toMatchSnapshot();
-  });
-});
-EOM
-
 # You need to commit, otherwise the `add-cli-no-save` build step will undo the `package.json` updates.
 git add .
 git commit -m "Updates to support split from CLI repo"
+
+# Use Node v12 (current LTS)
+nvm install v12.22.10
+nvm use v12.22.10
 
 # Run Build
 yarn setup-dev
@@ -171,7 +108,8 @@ git commit --amend
 yarn test
 
 # Run E2E Tests
-yarn e2e # command is SUPER slow, give it a a few minutes
+# Configure .env file (see /packages/e2e-tests/Readme.md for instructions)
+yarn e2e
 
 # Tests currently failing due to issues running ts-node based scripts
 # Currently added npx to commands to get around the hanging
@@ -190,3 +128,68 @@ git push --set-upstream origin auto-split
 # Clone Repo
 git clone git@github.com:aws-amplify/amplify-cli.git
 cd amplify-cli
+
+git checkout -b split-graphql-from-cli
+
+# Remove packages we won't be importing
+(cd packages && rm -rf \
+    amplify-graphql-auth-transformer \
+    amplify-graphql-default-value-transformer \
+    amplify-graphql-function-transformer \
+    amplify-graphql-http-transformer \
+    amplify-graphql-index-transformer \
+    amplify-graphql-maps-to-transformer \
+    amplify-graphql-migration-tests \
+    amplify-graphql-model-transformer \
+    amplify-graphql-predictions-transformer \
+    amplify-graphql-relational-transformer \
+    amplify-graphql-schema-test-library \
+    amplify-graphql-searchable-transformer \
+    amplify-graphql-transformer-core \
+    amplify-graphql-transformer-interfaces \
+    amplify-graphql-transformer-migrator \
+    amplify-migration-tests \
+    graphql-auth-transformer \
+    graphql-connection-transformer \
+    graphql-dynamodb-transformer \
+    graphql-elasticsearch-transformer \
+    graphql-function-transformer \
+    graphql-http-transformer \
+    graphql-key-transformer \
+    graphql-mapping-template \
+    graphql-predictions-transformer \
+    graphql-relational-schema-transformer \
+    graphql-transformer-common \
+    graphql-transformer-core \
+    graphql-transformers-e2e-tests \
+    graphql-versioned-transformer)
+
+# Remove files not related to api category
+rm graphql-transform-tutorial.md how-to-write-a-transformer.md testing-custom-indexes.md
+
+# Remove reference to graphql-common in amplify-dotnet-function-template-provider and amplify-nodejs-function-template-provider
+rjson packages/amplify-dotnet-function-template-provider/tsconfig.json | jq 'del(.references[] | select(.path == "../graphql-transformer-core"))' > packages/amplify-dotnet-function-template-provider/tsconfig.json.bak
+mv packages/amplify-dotnet-function-template-provider/tsconfig.json.bak packages/amplify-dotnet-function-template-provider/tsconfig.json
+
+rjson packages/amplify-nodejs-function-template-provider/tsconfig.json | jq 'del(.references[] | select(.path == "../graphql-transformer-core"))' > packages/amplify-nodejs-function-template-provider/tsconfig.json.bak
+mv packages/amplify-nodejs-function-template-provider/tsconfig.json.bak packages/amplify-nodejs-function-template-provider/tsconfig.json
+
+# Remove references to our packages in amplify-category-api
+rjson packages/amplify-category-api/tsconfig.json | jq 'del(.references[] | select(.path == ("../graphql-transformer-core", "../amplify-graphql-transformer-migrator")))' > packages/amplify-category-api/tsconfig.json.bak
+mv packages/amplify-category-api/tsconfig.json.bak packages/amplify-category-api/tsconfig.json
+
+# Remove references ot our packages in amplify-provider-awscloudformation
+rjson packages/amplify-provider-awscloudformation/tsconfig.json | jq 'del(.references[] | select(.path == ("../graphql-auth-transformer", "../graphql-connection-transformer", "../graphql-dynamodb-transformer", "../graphql-elasticsearch-transformer", "../graphql-function-transformer", "../graphql-http-transformer", "../graphql-key-transformer", "../graphql-predictions-transformer", "../graphql-transformer-core", "../graphql-versioned-transformer", "../amplify-graphql-transformer-core", "../amplify-graphql-transformer-interfaces", "../amplify-graphql-maps-to-transformer")))' > packages/amplify-provider-awscloudformation/tsconfig.json.bak
+mv packages/amplify-provider-awscloudformation/tsconfig.json.bak packages/amplify-provider-awscloudformation/tsconfig.json
+
+# Remove references ot our packages in amplify-category-function
+rjson packages/amplify-category-function/tsconfig.json | jq 'del(.references[] | select(.path == "../graphql-transformer-core"))' > packages/amplify-category-function/tsconfig.json.bak
+mv packages/amplify-category-function/tsconfig.json.bak packages/amplify-category-function/tsconfig.json
+
+# Remove references ot our packages in amplify-util-mock
+rjson packages/amplify-util-mock/tsconfig.json | jq 'del(.references[] | select(.path == ("../amplify-graphql-auth-transformer", "../amplify-graphql-model-transformer", "../amplify-graphql-maps-to-transformer")))' > packages/amplify-util-mock/tsconfig.json.bak
+mv packages/amplify-util-mock/tsconfig.json.bak packages/amplify-util-mock/tsconfig.json
+
+# Remove references ot our packages in amplify-cli
+rjson packages/amplify-cli/tsconfig.json | jq 'del(.references[] | select(.path == "../graphql-transformer-core"))' > packages/amplify-cli/tsconfig.json.bak
+mv packages/amplify-cli/tsconfig.json.bak packages/amplify-cli/tsconfig.json
