9c3dec847a49efd334a2fa28433afd0a90c05c0f
Christopher Sundersingh <83315412+sundersc@users.noreply.github.com>
2022-06-28 16:53:32 -0700

    feat(graphql): add runtime filtering support for subscriptions (#551)
    * feat(graphql): subscription runtime filtering
    
    * added dynamic groups support
    
    * update unit and e2e tests
    
    * added more test cases for auth precedence and different operators
    
    * fix enum list type on subscription filter
    
    * update model transformer screenshots
    
    * add test for enum field type
    
    * updated error message on a test

--- packages/amplify-graphql-model-transformer/src/graphql-types/common.ts
+++ packages/amplify-graphql-model-transformer/src/graphql-types/common.ts
@@ -21,7 +21,15 @@ import {
   makeNamedType,
   makeValueNode,
   ModelResourceIDs,
+  toPascalCase,
 } from 'graphql-transformer-common';
+import {
+  EnumWrapper,
+  FieldWrapper,
+  InputFieldWrapper,
+  InputObjectDefinitionWrapper,
+  ObjectDefinitionWrapper,
+} from '@aws-amplify/graphql-transformer-core';
 import {
   ATTRIBUTE_TYPES,
   BOOLEAN_CONDITIONS,
@@ -36,14 +44,12 @@ import {
   STRING_CONDITIONS,
   STRING_FUNCTIONS,
   API_KEY_DIRECTIVE,
+  SUBSCRIPTION_STRING_CONDITIONS,
+  SUBSCRIPTION_ID_CONDITIONS,
+  SUBSCRIPTION_INT_CONDITIONS,
+  SUBSCRIPTION_FLOAT_CONDITIONS,
+  SUBSCRIPTION_BOOLEAN_CONDITIONS,
 } from '../definitions';
-import {
-  EnumWrapper,
-  FieldWrapper,
-  InputFieldWrapper,
-  InputObjectDefinitionWrapper,
-  ObjectDefinitionWrapper,
-} from '@aws-amplify/graphql-transformer-core';
 
 /**
  * Creates the condition/filter input for a model
@@ -59,36 +65,96 @@ export const makeConditionFilterInput = (
   const supportsConditions = true;
   const input = InputObjectDefinitionWrapper.create(name);
   const wrappedObject = new ObjectDefinitionWrapper(object);
-  for (let field of wrappedObject.fields) {
+  for (const field of wrappedObject.fields) {
     const fieldType = ctx.output.getType(field.getTypeName());
     const isEnumType = fieldType && fieldType.kind === Kind.ENUM_TYPE_DEFINITION;
     if (field.isScalar() || isEnumType) {
-      const conditionTypeName =
-        isEnumType && field.isList()
-          ? ModelResourceIDs.ModelFilterListInputTypeName(field.getTypeName(), !supportsConditions)
-          : ModelResourceIDs.ModelFilterScalarInputTypeName(field.getTypeName(), !supportsConditions);
+      const conditionTypeName = isEnumType && field.isList()
+        ? ModelResourceIDs.ModelFilterListInputTypeName(field.getTypeName(), !supportsConditions)
+        : ModelResourceIDs.ModelFilterScalarInputTypeName(field.getTypeName(), !supportsConditions);
       const inputField = InputFieldWrapper.create(field.name, conditionTypeName, true);
       input.addField(inputField);
     }
   }
 
   // additional conditions of list type
-  for (let additionalField of ['and', 'or']) {
+  for (const additionalField of ['and', 'or']) {
     const inputField = InputFieldWrapper.create(additionalField, name, true, true);
     input.addField(inputField);
   }
   // additional conditions of non-list type
-  for (let additionalField of ['not']) {
+  for (const additionalField of ['not']) {
     const inputField = InputFieldWrapper.create(additionalField, name, true, false);
     input.addField(inputField);
   }
   return input;
 };
 
+/**
+ * Generates subscription filter input type
+ */
+export const makeSubscriptionFilterInput = (
+  ctx: TransformerTransformSchemaStepContextProvider,
+  name: string,
+  object: ObjectTypeDefinitionNode,
+): InputObjectDefinitionWrapper => {
+  const supportsConditions = true;
+  const input = InputObjectDefinitionWrapper.create(name);
+  const wrappedObject = new ObjectDefinitionWrapper(object);
+  for (const field of wrappedObject.fields) {
+    const fieldType = ctx.output.getType(field.getTypeName());
+    const isEnumType = fieldType && fieldType.kind === Kind.ENUM_TYPE_DEFINITION;
+    if (field.isScalar() || isEnumType) {
+      const conditionTypeName = ModelResourceIDs.ModelFilterScalarInputTypeName(
+        isEnumType ? 'String' : field.getTypeName(),
+        !supportsConditions,
+        true,
+      );
+      const inputField = InputFieldWrapper.create(field.name, conditionTypeName, true);
+      input.addField(inputField);
+    }
+  }
+
+  // additional conditions of list type
+  for (const additionalField of ['and', 'or']) {
+    const inputField = InputFieldWrapper.create(additionalField, name, true, true);
+    input.addField(inputField);
+  }
+
+  return input;
+};
+
+/**
+ * Generates the Subscription filter input type name
+ */
+export const getSubscriptionFilterInputName = (name: string): string => toPascalCase(['ModelSubscription', name, 'FilterInput']);
+
+/**
+ * Removes the given attribute from the subscription filter input type
+ */
+export const removeSubscriptionFilterInputAttribute = (
+  ctx: TransformerTransformSchemaStepContextProvider,
+  typeName: string,
+  fieldName: string,
+): void => {
+  const filterTypeName = getSubscriptionFilterInputName(typeName);
+  const filterType = ctx.output.getType(filterTypeName) as InputObjectTypeDefinitionNode;
+  if (!filterType) {
+    return;
+  }
+  const newFilterType: InputObjectTypeDefinitionNode = {
+    ...filterType,
+    fields: filterType.fields?.filter(field => field.name.value !== fieldName),
+  };
+  ctx.output.putType(newFilterType);
+};
+
+/**
+ * Generates model condition input type
+ */
 export const addModelConditionInputs = (ctx: TransformerTransformSchemaStepContextProvider): void => {
-  const conditionsInput: TypeDefinitionNode[] = ['String', 'Int', 'Float', 'Boolean', 'ID'].map(scalarName =>
-    makeModelScalarFilterInputObject(scalarName, true),
-  );
+  const conditionsInput: TypeDefinitionNode[] = ['String', 'Int', 'Float', 'Boolean', 'ID'].map(scalarName => makeModelScalarFilterInputObject(scalarName, true));
+  ['String', 'Int', 'Float', 'Boolean', 'ID'].map(scalarName => conditionsInput.push(makeModelScalarFilterInputObject(scalarName, true, true)));
   conditionsInput.push(makeAttributeTypeEnum());
   conditionsInput.push(makeSizeInputType());
   conditionsInput.forEach(input => {
@@ -104,14 +170,17 @@ export const addModelConditionInputs = (ctx: TransformerTransformSchemaStepConte
  * @param typeName Name of the scalar type
  * @param includeFilter add filter suffix to input
  */
-export function generateModelScalarFilterInputName(typeName: string, includeFilter: boolean): string {
+export function generateModelScalarFilterInputName(typeName: string, includeFilter: boolean, isSubscriptionFilter = false): string {
   const nameOverride = DEFAULT_SCALARS[typeName];
   if (nameOverride) {
-    return `Model${nameOverride}${includeFilter ? 'Filter' : ''}Input`;
+    return `Model${isSubscriptionFilter ? 'Subscription' : ''}${nameOverride}${includeFilter ? 'Filter' : ''}Input`;
   }
-  return `Model${typeName}${includeFilter ? 'Filter' : ''}Input`;
+  return `Model${isSubscriptionFilter ? 'Subscription' : ''}${typeName}${includeFilter ? 'Filter' : ''}Input`;
 }
 
+/**
+ * Creates Enum Model Filters
+ */
 export const createEnumModelFilters = (
   ctx: TransformerTransformSchemaStepContextProvider,
   type: ObjectTypeDefinitionNode,
@@ -132,11 +201,15 @@ export const createEnumModelFilters = (
  * @param type scalar type name
  * @param supportsConditions add filter suffix to input
  */
-export function makeModelScalarFilterInputObject(type: string, supportsConditions: boolean): InputObjectTypeDefinitionNode {
-  const name = generateModelScalarFilterInputName(type, !supportsConditions);
-  const conditions = getScalarConditions(type);
+export function makeModelScalarFilterInputObject(
+  type: string,
+  supportsConditions: boolean,
+  isSubscriptionFilter = false,
+): InputObjectTypeDefinitionNode {
+  const name = generateModelScalarFilterInputName(type, !supportsConditions, isSubscriptionFilter);
+  const conditions = isSubscriptionFilter ? getSubscriptionScalarConditions(type) : getScalarConditions(type);
   const scalarConditionInput = InputObjectDefinitionWrapper.create(name);
-  for (let condition of conditions) {
+  for (const condition of conditions) {
     let typeName;
     switch (condition) {
       case 'and':
@@ -147,12 +220,16 @@ export function makeModelScalarFilterInputObject(type: string, supportsCondition
         typeName = type;
     }
     const field = InputFieldWrapper.create(condition, typeName, true);
-    if (condition === 'between') {
+    if (condition === 'between' || condition === 'in' || condition === 'notIn') {
       field.wrapListType();
     }
     scalarConditionInput.addField(field);
   }
-  makeFunctionInputFields(type).map(f => scalarConditionInput.addField(f));
+
+  if (!isSubscriptionFilter) {
+    makeFunctionInputFields(type).map(f => scalarConditionInput.addField(f));
+  }
+
   return scalarConditionInput.serialize();
 }
 
@@ -168,6 +245,23 @@ function getScalarConditions(type: string): string[] {
       return FLOAT_CONDITIONS;
     case 'Boolean':
       return BOOLEAN_CONDITIONS;
+    default:
+      throw new Error('Valid types are String, ID, Int, Float, Boolean');
+  }
+}
+
+function getSubscriptionScalarConditions(type: string): string[] {
+  switch (type) {
+    case 'String':
+      return SUBSCRIPTION_STRING_CONDITIONS;
+    case 'ID':
+      return SUBSCRIPTION_ID_CONDITIONS;
+    case 'Int':
+      return SUBSCRIPTION_INT_CONDITIONS;
+    case 'Float':
+      return SUBSCRIPTION_FLOAT_CONDITIONS;
+    case 'Boolean':
+      return SUBSCRIPTION_BOOLEAN_CONDITIONS;
     default:
       throw new Error('Valid types are String, ID, Int, Float, Boolean');
   }
@@ -209,21 +303,30 @@ function makeFunctionInputFields(typeName: string): InputFieldWrapper[] {
   return fields;
 }
 
+/**
+ * Makes Attribute Type Enum
+ */
 export function makeAttributeTypeEnum(): EnumTypeDefinitionNode {
   return EnumWrapper.create('ModelAttributeTypes', ATTRIBUTE_TYPES).serialize();
 }
 
+/**
+ * Makes subscription field
+ */
 export function makeSubscriptionField(fieldName: string, returnTypeName: string, mutations: string[]): FieldDefinitionNode {
   return makeField(fieldName, [], makeNamedType(returnTypeName), [
     makeDirective('aws_subscribe', [makeArgument('mutations', makeValueNode(mutations))]),
   ]);
 }
 
+/**
+ * Makes Input Type size
+ */
 export function makeSizeInputType(): InputObjectTypeDefinitionNode {
   const name = 'ModelSizeInput';
   const input = InputObjectDefinitionWrapper.create(name);
 
-  for (let condition of SIZE_CONDITIONS) {
+  for (const condition of SIZE_CONDITIONS) {
     const field = InputFieldWrapper.create(condition, 'Int', true);
     if (condition === 'between') field.wrapListType();
     input.addField(field);
@@ -231,6 +334,9 @@ export function makeSizeInputType(): InputObjectTypeDefinitionNode {
   return input.serialize();
 }
 
+/**
+ * Makes enum filter input
+ */
 export function makeEnumFilterInput(fieldWrapper: FieldWrapper): InputObjectTypeDefinitionNode {
   const supportsConditions = true;
   const conditionTypeName = fieldWrapper.isList()
@@ -252,12 +358,15 @@ export function makeEnumFilterInput(fieldWrapper: FieldWrapper): InputObjectType
   return input.serialize();
 }
 
+/**
+ * Adds the directive to the field
+ */
 export const addDirectivesToField = (
   ctx: TransformerTransformSchemaStepContextProvider,
   typeName: string,
   fieldName: string,
   directives: Array<DirectiveNode>,
-) => {
+): void => {
   const type = ctx.output.getType(typeName) as ObjectTypeDefinitionNode;
   if (type) {
     const field = type.fields?.find(f => f.name.value === fieldName);
@@ -274,12 +383,15 @@ export const addDirectivesToField = (
   }
 };
 
+/**
+ * Adds directives to operation
+ */
 export const addDirectivesToOperation = (
   ctx: TransformerTransformSchemaStepContextProvider,
   typeName: string,
   operationName: string,
   directives: Array<DirectiveNode>,
-) => {
+): void => {
   // add directives to the given operation
   addDirectivesToField(ctx, typeName, operationName, directives);
 
@@ -300,6 +412,9 @@ export const addDirectivesToOperation = (
   }
 };
 
+/**
+ * Extends type with directives
+ */
 export const extendTypeWithDirectives = (
   ctx: TransformerTransformSchemaStepContextProvider,
   typeName: string,
@@ -310,17 +425,23 @@ export const extendTypeWithDirectives = (
   ctx.output.addObjectExtension(objectTypeExtension);
 };
 
-export function makeModelSortDirectionEnumObject(): EnumTypeDefinitionNode {
+/**
+ * Makes model sort direction enum object
+ */
+export const makeModelSortDirectionEnumObject = (): EnumTypeDefinitionNode => {
   const name = 'ModelSortDirection';
   return EnumWrapper.create(name, ['ASC', 'DESC']).serialize();
-}
+};
 // the smaller version of it's @auth equivalent since we only support
 // apikey as the only global auth rule
+/**
+ * Propagates api key to nested types
+ */
 export const propagateApiKeyToNestedTypes = (
   ctx: TransformerContextProvider,
   def: ObjectTypeDefinitionNode,
   seenNonModelTypes: Set<string>,
-) => {
+): void => {
   const nonModelTypePredicate = (fieldType: TypeDefinitionNode): TypeDefinitionNode | undefined => {
     if (fieldType) {
       if (fieldType.kind !== 'ObjectTypeDefinition') {
