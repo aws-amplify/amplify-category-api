aab94d29ebf6d58956875c0c23fb75b0facff418
Edward Foyle <foyleef@amazon.com>
2022-07-07 21:13:58 -0700

    fix: update object reference to match cap change (#627)

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/dynamic-imports.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/dynamic-imports.ts
@@ -4,7 +4,7 @@ export const serviceMetadataFor = async (service: string) =>
   (await import(path.join('..', '..', 'supported-services'))).supportedServices[service];
 
 export const datasourceMetadataFor = async (datasource: string) =>
-  (await import(path.join('..', '..', 'supported-datasources'))).supportedDatasources[datasource];
+  (await import(path.join('..', '..', 'supported-datasources'))).supportedDataSources[datasource];
 
 export const getServiceWalkthrough = async (walkthroughFilename: string) =>
   (await import(path.join('..', 'service-walkthroughs', walkthroughFilename))).serviceWalkthrough;
