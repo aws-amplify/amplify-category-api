20cffc0810c23f85127a939c0a3b812f87c2a601
Christopher Sundersingh <sundersc@amazon.com>
2022-08-04 12:24:28 -0700

    fix(graphql): revert subscriptions server-side filtering

--- packages/amplify-graphql-model-transformer/src/resolvers/subscriptions.ts
+++ packages/amplify-graphql-model-transformer/src/resolvers/subscriptions.ts
@@ -1,25 +1,11 @@
-import {
-  compoundExpression, Expression, obj, printBlock, str, toJson, nul, iff, not, isNullOrEmpty, ref,
-} from 'graphql-mapping-template';
+import { compoundExpression, Expression, obj, printBlock, str, toJson, nul } from 'graphql-mapping-template';
 
-/**
- * Generates subscription request template
- */
 export const generateSubscriptionRequestTemplate = (): string => {
   const statements: Expression[] = [toJson(obj({ version: str('2018-05-29'), payload: obj({}) }))];
   return printBlock('Subscription Request template')(compoundExpression(statements));
 };
 
-/**
- * Generates subscription response template
- */
 export const generateSubscriptionResponseTemplate = (): string => {
-  const statements: Expression[] = [
-    iff(
-      not(isNullOrEmpty(ref('ctx.args.filter'))),
-      ref('extensions.setSubscriptionFilter($util.transform.toSubscriptionFilter($ctx.args.filter))'),
-    ),
-    toJson(nul()),
-  ];
+  const statements: Expression[] = [toJson(nul())];
   return printBlock('Subscription Response template')(compoundExpression(statements));
 };
