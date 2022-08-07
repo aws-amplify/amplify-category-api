53adc533fbbfab52283162f9b2b4196f8ac569ab
Alexander Harris <ademp90@gmail.com>
2022-08-06 17:47:17 -0700

    chore: temp

--- packages/amplify-category-api/src/graphql-transformer/sandbox-mode-helpers.ts
+++ packages/amplify-category-api/src/graphql-transformer/sandbox-mode-helpers.ts
@@ -3,41 +3,59 @@ import { $TSContext } from 'amplify-cli-core';
 import { printer } from 'amplify-prompts';
 import { parse } from 'graphql';
 import { hasApiKey } from './api-key-helpers';
+import { promptToAddApiKey } from '../provider-utils/awscloudformation/prompt-to-add-api-key';
 
 const AMPLIFY = 'AMPLIFY';
 const AUTHORIZATION_RULE = 'AuthRule';
 const ALLOW = 'allow';
 const PUBLIC = 'public';
 
-export async function showSandboxModePrompts(context: $TSContext): Promise<any> {
-  if (!(await hasApiKey(context))) {
-    printer.info(
-      `
+/**
+ * If the app does not yet have an API key config, show a warning explaning why we need one, and prompt the user to set one up.
+ * @param context the request context
+ * @returns a promise of type void, or an api key config.
+ */
+export const showSandboxModePrompts = async (context: $TSContext): Promise<any> => {
+  const appHasApiKey = await hasApiKey(context);
+
+  if (appHasApiKey) {
+    return Promise.resolve();
+  }
+
+  printer.info(
+    `
 ⚠️  WARNING: Global Sandbox Mode has been enabled, which requires a valid API key. If
 you'd like to disable, remove ${chalk.green('"input AMPLIFY { globalAuthRule: AuthRule = { allow: public } }"')}
 from your GraphQL schema and run 'amplify push' again. If you'd like to proceed with
 sandbox mode disabled, do not create an API Key.
 `,
-      'yellow',
-    );
-    return await context.amplify.invokePluginMethod(context, 'api', undefined, 'promptToAddApiKey', [context]);
-  }
-}
+    'yellow',
+  );
 
-export function showGlobalSandboxModeWarning(doclink: string): void {
+  return promptToAddApiKey(context);
+};
+
+/**
+ * Render out a warning about the limitations of the global api key config.
+ * @param doclink link to the relevant auth docs.
+ */
+export const showGlobalSandboxModeWarning = (doclink: string): void => {
   printer.info(
     `
 ⚠️  WARNING: your GraphQL API currently allows public create, read, update, and delete access to all models via an API Key. To configure PRODUCTION-READY authorization rules, review: ${doclink}
 `,
     'yellow',
   );
-}
+};
 
-function matchesGlobalAuth(field: any): boolean {
-  return ['global_auth_rule', 'globalAuthRule'].includes(field.name.value);
-}
+/**
+ * Return whether or not a globla auth rule is defined on the AMPLIFY input object.
+ * @param field the field definition to check
+ * @returns true if the globalAuthRule name scheme is found
+ */
+const matchesGlobalAuth = (field: any): boolean => ['global_auth_rule', 'globalAuthRule'].includes(field.name.value);
 
-export function schemaHasSandboxModeEnabled(schema: string, docLink: string): boolean {
+export const schemaHasSandboxModeEnabled = (schema: string, docLink: string): boolean => {
   const { definitions } = parse(schema);
   const amplifyInputType: any = definitions.find((d: any) => d.kind === 'InputObjectTypeDefinition' && d.name.value === AMPLIFY);
 
@@ -61,9 +79,7 @@ export function schemaHasSandboxModeEnabled(schema: string, docLink: string): bo
 
   if (authScalarMatch && defaultValueNameMatch && defaultValueValueMatch) {
     return true;
-  } else {
-    throw Error(
-      `There was a problem with your auth configuration. Learn more about auth here: ${docLink}`,
-    );
   }
-}
+
+  throw Error(`There was a problem with your auth configuration. Learn more about auth here: ${docLink}`);
+};
--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/global-sandbox-mode.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/global-sandbox-mode.ts
@@ -1,6 +1,4 @@
-export function defineGlobalSandboxMode(link: string): string {
-  return `# This "input" configures a global authorization rule to enable public access to
+export const defineGlobalSandboxMode = (link: string): string => `# This "input" configures a global authorization rule to enable public access to
 # all models in this schema. Learn more about authorization rules here: ${link}
 input AMPLIFY { globalAuthRule: AuthRule = { allow: public } } # FOR TESTING ONLY!\n
 `;
-}
