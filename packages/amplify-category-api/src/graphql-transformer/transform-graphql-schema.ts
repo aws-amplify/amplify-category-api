4fd95ec9ef8611d597439cd15b9aa4875d6f8341
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-08-10 16:04:37 -0400

    Try to find the error

--- packages/amplify-category-api/src/graphql-transformer/transform-graphql-schema.ts
+++ packages/amplify-category-api/src/graphql-transformer/transform-graphql-schema.ts
@@ -1,8 +1,10 @@
 import { DeploymentResources as DeploymentResourcesV2 } from '@aws-amplify/graphql-transformer-core';
 import { DeploymentResources as DeploymentResourcesV1 } from 'graphql-transformer-core';
 import { $TSAny, $TSContext, ApiCategoryFacade } from 'amplify-cli-core';
+import { printer } from 'amplify-prompts';
 import { transformGraphQLSchemaV1 } from './transform-graphql-schema-v1';
 import { transformGraphQLSchemaV2 } from './transform-graphql-schema-v2';
+
 
 /**
  * Determine which transformer version is in effect, and execute the appropriate transformation.
@@ -12,7 +14,11 @@ export const transformGraphQLSchema = async (
   options: $TSAny,
 ): Promise<DeploymentResourcesV2 | DeploymentResourcesV1 | undefined> => {
   const transformerVersion = await ApiCategoryFacade.getTransformerVersion(context);
-  return transformerVersion === 2
-    ? transformGraphQLSchemaV2(context, options)
-    : transformGraphQLSchemaV1(context, options);
+  try {
+    return transformerVersion === 2
+      ? transformGraphQLSchemaV2(context, options)
+      : transformGraphQLSchemaV1(context, options);
+  } catch (e) {
+    printer.error(e.stack);
+  }
 };
