ea99a62ea5200be0fcaf97d31d290df727a39c49
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-08-11 14:38:30 -0400

    Revert "Try to find the error"
    This reverts commit 4fd95ec9ef8611d597439cd15b9aa4875d6f8341.

--- packages/amplify-category-api/src/graphql-transformer/transform-graphql-schema.ts
+++ packages/amplify-category-api/src/graphql-transformer/transform-graphql-schema.ts
@@ -1,10 +1,8 @@
 import { DeploymentResources as DeploymentResourcesV2 } from '@aws-amplify/graphql-transformer-core';
 import { DeploymentResources as DeploymentResourcesV1 } from 'graphql-transformer-core';
 import { $TSAny, $TSContext, ApiCategoryFacade } from 'amplify-cli-core';
-import { printer } from 'amplify-prompts';
 import { transformGraphQLSchemaV1 } from './transform-graphql-schema-v1';
 import { transformGraphQLSchemaV2 } from './transform-graphql-schema-v2';
-
 
 /**
  * Determine which transformer version is in effect, and execute the appropriate transformation.
@@ -14,11 +12,7 @@ export const transformGraphQLSchema = async (
   options: $TSAny,
 ): Promise<DeploymentResourcesV2 | DeploymentResourcesV1 | undefined> => {
   const transformerVersion = await ApiCategoryFacade.getTransformerVersion(context);
-  try {
-    return transformerVersion === 2
-      ? transformGraphQLSchemaV2(context, options)
-      : transformGraphQLSchemaV1(context, options);
-  } catch (e) {
-    printer.error(e.stack);
-  }
+  return transformerVersion === 2
+    ? transformGraphQLSchemaV2(context, options)
+    : transformGraphQLSchemaV1(context, options);
 };
