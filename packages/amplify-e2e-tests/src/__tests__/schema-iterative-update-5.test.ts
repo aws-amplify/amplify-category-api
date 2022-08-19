1a2072438b22764cdce439b1dee23c673b0be7c0
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-08-19 12:15:53 -0400

    Add another E2E for sort key changes

--- packages/amplify-e2e-tests/src/__tests__/schema-iterative-update-5.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/schema-iterative-update-5.test.ts
@@ -11,13 +11,13 @@ import {
   amplifyPushUpdate,
 } from 'amplify-category-api-e2e-core';
 
-describe('Schema iterative update - add sort key to primary key', () => {
+describe('Schema iterative update - sort key modifications', () => {
   let projectDir: string;
 
   beforeAll(async () => {
     projectDir = await createNewProjectDir('schemaIterative');
     await initJSProjectWithProfile(projectDir, {
-      name: 'iterativetest1',
+      name: 'iterativetest5',
     });
 
     addFeatureFlag(projectDir, 'graphqltransformer', 'enableiterativegsiupdates', true);
@@ -26,8 +26,9 @@ describe('Schema iterative update - add sort key to primary key', () => {
     await deleteProject(projectDir);
     deleteProjectDir(projectDir);
   });
-  it('should error out when pushed without allowing destructive updates', async () => {
-    const apiName = 'iterativetest1';
+
+  it('should error out when sort key addition pushed without allowing destructive updates', async () => {
+    const apiName = 'iterativetest5';
 
     const initialSchema = path.join('iterative-push', 'add-sort-to-primary-key', 'initial-schema.graphql');
     await addApiWithoutSchema(projectDir, { apiKeyExpirationDays: 7, transformerVersion: 2 });
@@ -35,6 +36,19 @@ describe('Schema iterative update - add sort key to primary key', () => {
     await amplifyPush(projectDir);
 
     const finalSchema = path.join('iterative-push', 'add-sort-to-primary-key', 'final-schema.graphql');
+    await updateApiSchema(projectDir, apiName, finalSchema);
+    await expect(amplifyPushUpdate(projectDir)).rejects.toBeTruthy();
+  });
+
+  it('should error out when sort key removal pushed without allowing destructive updates', async () => {
+    const apiName = 'iterativetest5';
+
+    const initialSchema = path.join('iterative-push', 'add-sort-to-primary-key', 'final-schema.graphql');
+    await addApiWithoutSchema(projectDir, { apiKeyExpirationDays: 7, transformerVersion: 2 });
+    await updateApiSchema(projectDir, apiName, initialSchema);
+    await amplifyPush(projectDir);
+
+    const finalSchema = path.join('iterative-push', 'add-sort-to-primary-key', 'initial-schema.graphql');
     await updateApiSchema(projectDir, apiName, finalSchema);
     await expect(amplifyPushUpdate(projectDir)).rejects.toBeTruthy();
   });
