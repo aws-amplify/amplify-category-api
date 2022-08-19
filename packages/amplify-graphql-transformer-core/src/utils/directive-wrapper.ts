1ab8b186c7875cf41d472afe7a345f7a4fc05b73
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-08-19 14:31:36 -0400

    Fix FeatureFlags usage in case provider not available

--- packages/amplify-graphql-transformer-core/src/utils/directive-wrapper.ts
+++ packages/amplify-graphql-transformer-core/src/utils/directive-wrapper.ts
@@ -1,6 +1,7 @@
 import { ArgumentNode, DirectiveNode, NameNode, valueFromASTUntyped, ValueNode, Location } from 'graphql';
 import _ from 'lodash';
 import { FeatureFlagProvider } from '@aws-amplify/graphql-transformer-interfaces';
+import { FeatureFlags } from 'amplify-cli-core';
 
 export class ArgumentWrapper {
   public readonly name: NameNode;
@@ -42,7 +43,10 @@ export class DirectiveWrapper {
       }),
       {},
     );
-    if (featureFlags?.getBoolean('shouldDeepMergeDirectiveConfigDefaults', false)) {
+    const useDeepMerge = featureFlags != null
+      ? featureFlags?.getBoolean('shouldDeepMergeDirectiveConfigDefaults', false)
+      : FeatureFlags.getBoolean('graphqltransformer.shouldDeepMergeDirectiveConfigDefaults');
+    if (useDeepMerge) {
       return _.merge(defaultValue, argValues);
     }
     return Object.assign(defaultValue, argValues);
