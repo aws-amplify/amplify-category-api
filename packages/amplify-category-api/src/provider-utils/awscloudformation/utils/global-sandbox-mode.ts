045a85dedf44bc2efffd6d25ca55c511acc8e65b
Alexander Harris <alharris@amazon.com>
2022-09-07 12:33:05 -0700

    revert changes not related to new test app

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/global-sandbox-mode.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/global-sandbox-mode.ts
@@ -1,4 +1,6 @@
-export const defineGlobalSandboxMode = (link: string): string => `# This "input" configures a global authorization rule to enable public access to
+export function defineGlobalSandboxMode(link: string): string {
+  return `# This "input" configures a global authorization rule to enable public access to
 # all models in this schema. Learn more about authorization rules here: ${link}
 input AMPLIFY { globalAuthRule: AuthRule = { allow: public } } # FOR TESTING ONLY!\n
 `;
+}
