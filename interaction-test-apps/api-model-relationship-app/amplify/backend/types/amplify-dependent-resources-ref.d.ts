f1da21d8a4fbe2da9df9fff9f6fe2ef0c1c17fa5
Alexander Harris <ademp90@gmail.com>
2022-09-06 18:37:34 -0700

    remove currently unused test apps, and remove files that should be gitignored, update gitignore

--- interaction-test-apps/api-model-relationship-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "simplemodel": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
