d964994eaa4e3d07b2843629072e5325495cb1e4
Kamil Sobol <sobkamil@amazon.com>
2022-10-11 09:09:18 -0700

    feat: migrate searchable transformer to CDK v2

--- packages/amplify-graphql-searchable-transformer/src/cdk/create-cfnParameters.ts
+++ packages/amplify-graphql-searchable-transformer/src/cdk/create-cfnParameters.ts
@@ -1,5 +1,5 @@
 import { ResourceConstants } from 'graphql-transformer-common';
-import { CfnParameter, Stack } from '@aws-cdk/core';
+import { CfnParameter, Stack } from 'aws-cdk-lib';
 
 export const createParametersStack = (stack: Stack): Map<string, CfnParameter> => {
   const {
