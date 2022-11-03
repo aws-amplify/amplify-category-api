0f675f222e24c07084b2907bc62024d7126f574e
Kamil Sobol <sobkamil@amazon.com>
2022-11-03 14:20:03 -0700

    fix: container api cfn generation (#947)
    * chore: use CLI with CDK v2
    
    * chore: use CLI with CDK v2
    
    * chore: undo this
    
    * fix: container api cfn generation
    
    * fix: container api cfn generation - add snapshot test
    
    * fix: container api cfn generation - comments.

--- packages/amplify-category-api/src/provider-utils/awscloudformation/base-api-stack.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/base-api-stack.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
@@ -75,7 +77,7 @@ export abstract class ContainersStack extends cdk.Stack {
   protected readonly awaiterS3Key: string;
 
   constructor(scope: Construct, id: string, private readonly props: ContainersStackProps) {
-    super(scope, id);
+    super(scope, id, { synthesizer: new cdk.LegacyStackSynthesizer() });
 
     const {
       parameters,
@@ -497,6 +499,23 @@ export abstract class ContainersStack extends cdk.Stack {
     const pipelineName = this.getPipelineName();
     return `https://${region}.console.aws.amazon.com/codesuite/codepipeline/pipelines/${pipelineName}/view`;
   }
+
+  /**
+   * This function renderers a full CFN template for this stack.
+   * It is inspired by
+   * https://github.com/aws/aws-cdk/blob/bd056d1d38a2d3f43efe4f857c4d38b30fb9b681/packages/%40aws-cdk/assertions/lib/template.ts#L298-L310.
+   * This replaces private prepareApp (from CDK v1) and this._toCloudFormation() (the latter does not function properly without the former).
+   */
+  private renderCfnTemplate(): any {
+    const root = this.node.root as cdk.Stage;
+    const assembly = root.synth();
+    if (this.nestedStackParent) {
+      // if this is a nested stack (it has a parent), then just read the template as a string
+      return JSON.parse(fs.readFileSync(path.join(assembly.directory, this.templateFile)).toString('utf-8'));
+    }
+    return assembly.getStackArtifact(this.artifactId).template;
+  }
+
   toCloudFormation() {
     this.node
       .findAll()
@@ -511,7 +530,7 @@ export abstract class ContainersStack extends cdk.Stack {
         }
       });
 
-    const cfn = this._toCloudFormation();
+    const cfn = this.renderCfnTemplate();
 
     Object.keys(cfn.Parameters).forEach(k => {
       if (k.startsWith('AssetParameters')) {
