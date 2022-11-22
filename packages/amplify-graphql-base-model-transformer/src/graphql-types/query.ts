158b1b034d5c0c3891d48c635b59fded4f67ef06
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-11-22 12:14:45 -0500

    WIP: factoring out generic model transform

---
+++ packages/amplify-graphql-base-model-transformer/src/graphql-types/query.ts
@@ -0,0 +1,32 @@
+import { TransformerTransformSchemaStepContextProvider } from '@aws-amplify/graphql-transformer-interfaces';
+import { InputObjectTypeDefinitionNode, ObjectTypeDefinitionNode } from 'graphql';
+import { FieldWrapper, ObjectDefinitionWrapper } from '@aws-amplify/graphql-transformer-core';
+import { makeConditionFilterInput, makeSubscriptionFilterInput } from './common';
+export const makeListQueryFilterInput = (
+  ctx: TransformerTransformSchemaStepContextProvider,
+  name: string,
+  object: ObjectTypeDefinitionNode,
+): InputObjectTypeDefinitionNode => {
+  return makeConditionFilterInput(ctx, name, object).serialize();
+};
+
+export const makeSubscriptionQueryFilterInput = (
+  ctx: TransformerTransformSchemaStepContextProvider,
+  name: string,
+  object: ObjectTypeDefinitionNode,
+): InputObjectTypeDefinitionNode => {
+  return makeSubscriptionFilterInput(ctx, name, object).serialize();
+};
+
+export const makeListQueryModel = (type: ObjectTypeDefinitionNode, modelName: string, isSyncEnabled: boolean): ObjectTypeDefinitionNode => {
+  const outputType = ObjectDefinitionWrapper.create(modelName);
+
+  outputType.addField(FieldWrapper.create('items', type.name.value, true, true));
+  outputType.addField(FieldWrapper.create('nextToken', 'String', true, false));
+
+  if (isSyncEnabled) {
+    outputType.addField(FieldWrapper.create('startedAt', 'AWSTimestamp', true, false));
+  }
+
+  return outputType.serialize();
+};
