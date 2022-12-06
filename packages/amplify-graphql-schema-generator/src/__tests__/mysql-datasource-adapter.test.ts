2ec2769c54024814ff57bf9f9c674f8d54e39421
Christopher Sundersingh <sundersc@amazon.com>
2022-12-05 18:28:52 -0800

    add mysql datatype mapping

--- packages/amplify-graphql-schema-generator/src/__tests__/mysql-datasource-adapter.test.ts
+++ packages/amplify-graphql-schema-generator/src/__tests__/mysql-datasource-adapter.test.ts
@@ -1,7 +1,16 @@
-import { DataSourceAdapter } from '../datasource-adapter';
-import { Field, Index } from '../schema-representation';
+import { DataSourceAdapter, MySQLDataSourceAdapter } from '../datasource-adapter';
+import { Field, FieldType, Index } from '../schema-representation';
 
 class TestDataSourceAdapter extends DataSourceAdapter {
+  public async initialize(): Promise<void> {
+    // Do Nothing
+  }
+  public mapDataType(type: string, nullable: boolean): FieldType {
+    return {
+      kind: 'Scalar',
+      name: 'String',
+    }
+  }
   public async getTablesList(): Promise<string[]> {
     return ['Test'];
   }
@@ -26,10 +35,49 @@ describe('testDataSourceAdapter', () => {
     adapter.getFields = jest.fn(async () => []);
     adapter.getPrimaryKey = jest.fn();
     adapter.getIndexes = jest.fn(async () => []);
+    adapter.mapDataType = jest.fn();
     await adapter.getModels();
     expect(adapter.getTablesList).toBeCalledTimes(1);
     expect(adapter.getFields).toBeCalledTimes(1);
     expect(adapter.getIndexes).toBeCalledTimes(1);
     expect(adapter.getPrimaryKey).toBeCalledTimes(1);
+  });
+
+  it('test mysql datatype mapping', () => {
+    const config = {
+      host: "host",
+      database: "database",
+      port: 1234,
+      username: "username",
+      password: "password",
+    };
+    const adapter = new MySQLDataSourceAdapter(config);
+    expect(adapter.mapDataType('varchar', true)).toEqual({
+      "kind": "Scalar",
+      "name": "String",
+    });
+    expect(adapter.mapDataType('char', true)).toEqual({
+      "kind": "Scalar",
+      "name": "String",
+    });
+    expect(adapter.mapDataType('enum', true)).toEqual({
+      "kind": "Scalar",
+      "name": "String",
+    });
+    expect(adapter.mapDataType('bool', true)).toEqual({
+      "kind": "Scalar",
+      "name": "Boolean",
+    });
+    expect(adapter.mapDataType('decimal', true)).toEqual({
+      "kind": "Scalar",
+      "name": "Float",
+    });
+    expect(adapter.mapDataType('year', false)).toEqual({
+      "kind": "NonNull",
+      "type": {
+        "kind": "Scalar",
+        "name": "Int",
+      },
+    });
   });
 });
--- packages/amplify-graphql-schema-generator/src/datasource-adapter/mysql-datasource-adapter.ts
+++ packages/amplify-graphql-schema-generator/src/datasource-adapter/mysql-datasource-adapter.ts
@@ -1,4 +1,4 @@
-import { Field, Index } from "../schema-representation";
+import { DefaultType, Field, FieldDataType, FieldType, Index } from "../schema-representation";
 import { DataSourceAdapter } from "./datasource-adapter";
 import { knex } from 'knex';
 import { printer } from 'amplify-prompts';
@@ -20,15 +20,30 @@ interface MySQLIndex {
   nullable: boolean;  
 }
 
+interface MySQLColumn {
+  tableName: string;
+  columnName: string;
+  sequence: number;
+  default: string;
+  datatype: string;
+  nullable: boolean;  
+  length: number | null | undefined;  
+}
+
 export class MySQLDataSourceAdapter extends DataSourceAdapter {
   private dbBuilder: any;
   private indexes: MySQLIndex[] = [];
+  private fields: MySQLColumn[] = [];
   private readonly PRIMARY_KEY_INDEX_NAME = 'PRIMARY';
 
   constructor(private config: MySQLDataSourceConfig) {
     super();
-    this.establishDBConnection();
-    this.loadAllIndexes();
+  }
+  
+  public async initialize(): Promise<void> {
+    await this.establishDBConnection();
+    await this.loadAllFields();
+    await this.loadAllIndexes();
   }
 
   private establishDBConnection() {
@@ -64,39 +79,63 @@ export class MySQLDataSourceAdapter extends DataSourceAdapter {
 
   public async getTablesList(): Promise<string[]> {
     let result = (await this.dbBuilder.raw("SHOW TABLES"))[0];
-    const tables: string[] = [];
-
-    for (const row of result) {
+    
+    const tables: string[] = result.map((row: any) => {
       const [firstKey] = Object.keys(row);
       const tableName = row[firstKey];
-      tables.push(tableName);
-    }
-
+      return tableName;
+    });
+    
     return tables;
   }
 
   public async getFields(tableName: string): Promise<Field[]> {
-    const fields: Field[] = [];
-    let columnResult = (await this.dbBuilder.raw(`SHOW COLUMNS FROM ${tableName}`))[0];
-    for (const item of columnResult) {
-      const field: any = {
-        name: item["Field"],
-        type: item["Type"], // TODO: Datatype mapping is required here
+    const fieldsName: string[] = [...new Set(this.fields
+      .filter(f => f.tableName === tableName)
+      .sort((a,b) => a.sequence - b.sequence)
+      .map(f => f.columnName))]; 
+    
+    const modelFields = fieldsName.map(columnName => {
+      const dbField = this.fields
+        .find(field => field.tableName === tableName && field.columnName === columnName)!;
+      const field: Field = {
+        name: dbField.columnName,
+        type: this.mapDataType(dbField.datatype, dbField.nullable),
+        length: dbField.length,
+        default: dbField.default ? {
+          kind: 'DB_GENERATED',
+          value: dbField.default,
+        } : undefined,
       };
-      if (item["Default"]) {
-        field.default = item["Default"];
-      }
-      fields.push(field);
-    }
-    return fields;
+      return field;
+    });
+
+    return modelFields;
+  }
+
+  private async loadAllFields(): Promise<void> {
+    this.fields = [];
+    // Query INFORMATION_SCHEMA.COLUMNS table and load fields of all the tables from the database
+    let columnResult = (await this.dbBuilder.raw(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '${this.config.database}'`))[0];
+    this.fields = columnResult.map((item: any) => {
+      return {
+        tableName: item["TABLE_NAME"],
+        columnName: item["COLUMN_NAME"],
+        default: item["COLUMN_DEFAULT"],
+        sequence: item["ORDINAL_POSITION"],
+        datatype: item["DATA_TYPE"],
+        nullable: item["IS_NULLABLE"] === 'YES' ? true : false,
+        length: item["CHARACTER_MAXIMUM_LENGTH"],
+      };
+    });
   }
 
   private async loadAllIndexes(): Promise<void> {
     this.indexes = [];
     // Query INFORMATION_SCHEMA.STATISTICS table and load indexes of all the tables from the database
     let indexResult = (await this.dbBuilder.raw(`SELECT * FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = '${this.config.database}'`))[0];
-    for (const item of indexResult) {
-      const index: MySQLIndex = {
+    this.indexes = indexResult.map((item: any) => {
+      return {
         tableName: item["TABLE_NAME"],
         indexName: item["INDEX_NAME"],
         nonUnique: item["NON_UNIQUE"],
@@ -104,13 +143,12 @@ export class MySQLDataSourceAdapter extends DataSourceAdapter {
         sequence: item["SEQ_IN_INDEX"],
         nullable: item["NULLABLE"] === 'YES' ? true : false,
       };
-      this.indexes.push(index);
-    }
+    });
   }
 
   public async getPrimaryKey(tableName: string): Promise<Index | null> {
     const key = this.indexes
-      .filter(index => index.tableName == tableName && index.indexName === this.PRIMARY_KEY_INDEX_NAME)
+      .filter(index => index.tableName === tableName && index.indexName === this.PRIMARY_KEY_INDEX_NAME)
       .sort((a,b) => a.sequence - b.sequence);
 
     if (!key || key.length == 0) {
@@ -124,23 +162,102 @@ export class MySQLDataSourceAdapter extends DataSourceAdapter {
   }
 
   public async getIndexes(tableName: string): Promise<Index[]> {
-    const tableIndexes: Index[] = [];
     const indexNames: string[] = [...new Set(this.indexes
       .filter(i => i.tableName === tableName && i.indexName !== this.PRIMARY_KEY_INDEX_NAME).map(i => i.indexName))]; 
     
-    for (const indexName of indexNames) {
+    const tableIndexes = indexNames.map((indexName: string) => {
       const key = this.indexes
         .filter(index => index.tableName == tableName && index.indexName === indexName)
         .sort((a,b) => a.sequence - b.sequence);
       const index: Index = new Index(indexName);
       index.setFields(key.map(k => k.columnName));
-      tableIndexes.push(index);
-    }
+      return index;
+    });
 
     return tableIndexes;
   }
 
   public cleanup(): void {
     this.dbBuilder && this.dbBuilder.destroy();
+  }
+  
+  public mapDataType(type: string, nullable: boolean): FieldType {
+    let datatype: FieldDataType = 'String';
+    let listtype = false;
+
+    switch (type.toUpperCase()) {
+      case 'VARCHAR':
+      case 'CHAR':
+      case 'BINARY':
+      case 'VARBINARY':
+      case 'TINYBLOB':
+      case 'TINYTEXT':
+      case 'TEXT':
+      case 'BLOB':
+      case 'MEDIUMTEXT':
+      case 'MEDIUMBLOB':
+      case 'LONGTEXT':
+      case 'LONGBLOB':
+      case 'ENUM':
+        datatype = 'String';
+        break;
+      case 'SET':
+        datatype = 'String';
+        listtype = true;
+        break;
+      case 'BOOLEAN':
+      case 'BOOL':
+        datatype = 'Boolean';
+        break;
+      case 'BIT':
+      case 'TINYINT':
+      case 'SMALLINT':
+      case 'MEDIUMINT':
+      case 'INT':
+      case 'INTEGER':
+      case 'BIGINT':
+      case 'YEAR':
+        datatype = 'Int';
+        break;
+      case 'FLOAT':
+      case 'DOUBLE':
+      case 'DECIMAL':
+      case 'DEC':
+      case 'NUMERIC':
+        datatype = 'Float';
+        break;
+      case 'DATE':
+        datatype = 'AWSDate';
+        break;
+      case 'DATETIME':
+        datatype = 'AWSDateTime';
+        break;
+      case 'TIMESTAMP':
+        datatype = 'AWSTimestamp';
+        break;
+      case 'TIME':
+        datatype = 'AWSTime';
+        break;
+      case 'JSON':
+        datatype = 'AWSJSON';
+        break;
+      default:
+        datatype = 'String';
+        break;
+    }
+
+    let result: FieldType = {
+      kind: 'Scalar',
+      name: datatype,
+    };
+
+    if (!nullable) {
+      result = {
+        kind: 'NonNull',
+        type: result,
+      };
+    }
+
+    return result;
   }
 }
