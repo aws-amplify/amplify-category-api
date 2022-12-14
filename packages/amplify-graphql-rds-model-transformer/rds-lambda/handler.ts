f99a8b65044be27b02b7f9df90de71b7518551f0
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-12-14 10:46:04 -0500

    Add pagination support to rds lambda, clean up types

--- packages/amplify-graphql-rds-model-transformer/rds-lambda/handler.ts
+++ packages/amplify-graphql-rds-model-transformer/rds-lambda/handler.ts
@@ -1,8 +1,6 @@
 import { postgresPasswordClient } from './clients/postgres/PostgresPasswordClient.js';
 
 export const run = async (event) => {
-  console.log(`Logging Event: ${JSON.stringify(event)}`);
   const result = await postgresPasswordClient.executeRequest(event);
-  console.log(JSON.stringify(result));
   return result;
 };
