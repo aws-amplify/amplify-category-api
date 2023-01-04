0e019aae0e3cbd5dd6e0b2d8767e62e1851db0e1
Manaswi Manthena <manamaha@amazon.com>
2023-01-04 14:59:14 -0800

    add unit tests

--- packages/amplify-graphql-transformer-core/src/__tests__/transformation/transform.test.ts
+++ packages/amplify-graphql-transformer-core/src/__tests__/transformation/transform.test.ts
@@ -1,9 +1,12 @@
+import { $TSAny } from 'amplify-cli-core';
 import { AppSyncAuthConfiguration, TransformerPluginProvider, TransformerPluginType } from '@aws-amplify/graphql-transformer-interfaces';
 import { App } from '@aws-cdk/core';
 import { GraphQLApi } from '../../graphql-api';
 import { GraphQLTransform } from '../../transformation/transform';
 import { TransformerOutput } from '../../transformer-context/output';
 import { StackManager } from '../../transformer-context/stack-manager';
+import { TransformerContext } from '../../transformer-context';
+import { printer } from 'amplify-prompts';
 
 class TestGraphQLTransform extends GraphQLTransform {
   testGenerateGraphQlApi(stackManager: StackManager, output: TransformerOutput): GraphQLApi {
@@ -26,7 +29,39 @@ const mockTransformer: TransformerPluginProvider = {
   typeDefinitions: [],
 };
 
+const printerMock = printer as jest.Mocked<typeof printer>;
+printerMock.info = jest.fn();
+
+const mockContext = {
+  isProjectUsingDataStore: jest.fn(),
+  featureFlags: {
+    getBoolean: jest.fn(),
+    getNumber: jest.fn(),
+    getObject: jest.fn(),
+  },
+  isProjectUsingCPK: jest.fn(),
+} as $TSAny as TransformerContext;
+
 describe('GraphQLTransform', () => {
+  const CPK_ERROR_MESSAGE = expect.stringContaining(
+    `
+⚠️  WARNING: Your schema has a custom primary key but the Feature Flag \"respectPrimaryKeyAttributesOnConnectionField\" is disabled. Check the value in your "amplify/cli.json" file, change it to "true" and re-run
+`,
+  );
+
+  it('has isProjectUsingCPK which returns true if a model has a primary key', () => {
+    const transform = new GraphQLTransform({
+      transformers: [mockTransformer],
+    });
+    const schema = `  
+    type Post @model {  
+      id: ID! @primaryKey
+      title: String!
+    `;
+    const ctx: TransformerContext = mockContext;
+        
+  });
+
   it('throws on construction with no transformers', () => {
     expect(() => {
       // eslint-disable-next-line no-new
@@ -41,6 +76,55 @@ describe('GraphQLTransform', () => {
       transformers: [mockTransformer],
     });
     expect(transform).toBeDefined();
+  });
+
+  it(`throws a cpk warning when datastore exists and FF is false but primary key exists`, () => {
+    const transform = new GraphQLTransform({
+      transformers: [mockTransformer],
+    });
+    mockContext.isProjectUsingDataStore = jest.fn().mockReturnValueOnce(true);
+    mockContext.featureFlags.getBoolean = jest.fn().mockReturnValueOnce(false);
+    mockContext.isProjectUsingCPK = jest.fn().mockReturnValueOnce(true);
+    transform.validateCPKFeatureFlag(mockContext)
+    expect(printerMock.info).toBeCalledWith(
+      `
+⚠️  WARNING: Your schema has a custom primary key but the Feature Flag \"respectPrimaryKeyAttributesOnConnectionField\" is disabled. Check the value in your "amplify/cli.json" file, change it to "true" and re-run
+  `,
+      'yellow',
+    );
+  });
+
+  it(`doesn't throw a cpk warning when datastore exists and FF is true and also primary key exists`, () => {
+    const transform = new GraphQLTransform({
+      transformers: [mockTransformer],
+    });
+    mockContext.isProjectUsingDataStore = jest.fn().mockReturnValueOnce(true);
+    mockContext.featureFlags.getBoolean = jest.fn().mockReturnValueOnce(true);
+    mockContext.isProjectUsingCPK = jest.fn().mockReturnValueOnce(true);
+    transform.validateCPKFeatureFlag(mockContext);
+    expect(printerMock.info).not.toBeCalledWith(CPK_ERROR_MESSAGE);
+  });
+
+  it(`doesn't throw a cpk warning when datastore doesn't exist and FF is false and also primary key exists`, () => {
+    const transform = new GraphQLTransform({
+      transformers: [mockTransformer],
+    });
+    mockContext.isProjectUsingDataStore = jest.fn().mockReturnValueOnce(false);
+    mockContext.featureFlags.getBoolean = jest.fn().mockReturnValueOnce(false);
+    mockContext.isProjectUsingCPK = jest.fn().mockReturnValueOnce(true);
+    transform.validateCPKFeatureFlag(mockContext);
+    expect(printerMock.info).not.toBeCalledWith(CPK_ERROR_MESSAGE);
+  });
+
+  it(`doesn't throw a cpk warning when primary key doesn't exists`, () => {
+    const transform = new GraphQLTransform({
+      transformers: [mockTransformer],
+    });
+    mockContext.isProjectUsingDataStore = jest.fn().mockReturnValueOnce(true);
+    mockContext.featureFlags.getBoolean = jest.fn().mockReturnValueOnce(false);
+    mockContext.isProjectUsingCPK = jest.fn().mockReturnValueOnce(false);
+    transform.validateCPKFeatureFlag(mockContext);
+    expect(printerMock.info).not.toBeCalledWith(CPK_ERROR_MESSAGE);
   });
 
   describe('generateGraphQlApi', () => {
