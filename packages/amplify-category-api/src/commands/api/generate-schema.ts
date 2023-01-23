90af4cbb58dce239779147388e4a3677a4ffe2fc
phani-srikar <phanisrikar93ume@gmail.com>
2023-01-23 08:49:02 -0800

    chore: address previous review comments

--- packages/amplify-category-api/src/commands/api/generate-schema.ts
+++ packages/amplify-category-api/src/commands/api/generate-schema.ts
@@ -8,7 +8,7 @@ import { getDBUserSecretsWalkthrough } from '../../provider-utils/awscloudformat
 import { ImportedRDSType, RDS_SCHEMA_FILE_NAME } from '../../provider-utils/awscloudformation/service-walkthrough-types/import-appsync-api-types';
 import { getRDSGlobalAmplifyInput, getRDSDBConfigFromAmplifyInput, constructRDSGlobalAmplifyInput } from '../../provider-utils/awscloudformation/utils/import-rds-utils/globalAmplifyInputs';
 import { getAppSyncAPIName, getAPIResourceDir } from '../../provider-utils/awscloudformation/utils/amplify-meta-utils';
-import { getExistingConnectionSecrets, storeConnectionSecrets } from '../../provider-utils/awscloudformation/utils/rds-secrets/database-secrets';
+import { getExistingConnectionSecrets, RDSConnectionSecrets, storeConnectionSecrets } from '../../provider-utils/awscloudformation/utils/rds-secrets/database-secrets';
 import { writeSchemaFile } from '../../provider-utils/awscloudformation/utils/graphql-schema-utils';
 import * as os from 'os';
 
@@ -22,59 +22,71 @@ export const run = async (context: $TSContext) => {
 
   // proceed if there are any existing imported Relational Data Sources
   const pathToSchemaFile = path.join(apiResourceDir, RDS_SCHEMA_FILE_NAME);
-  if(fs.existsSync(pathToSchemaFile)) {
-    // read and validate the RDS connection parameters
-    const amplifyInput = await getRDSGlobalAmplifyInput(context, pathToSchemaFile);
-    const config = await getRDSDBConfigFromAmplifyInput(context, amplifyInput);
 
-    // read and validate the RDS connection secrets
-    let secretsExistInParameterStore = true;
-    let secrets = await getExistingConnectionSecrets(context, config.database, apiName);
-    if(!secrets) {
-      secrets = await getDBUserSecretsWalkthrough(config.database);
-      secretsExistInParameterStore = false;
-    }
-    config.username = secrets?.username;
-    config.password = secrets?.password;
-    
-    // Establish the connection
-    let adapter: DataSourceAdapter;
-    let schema: Schema;
-    switch(config.engine) {
-      case ImportedRDSType.MYSQL:
-        adapter = new MySQLDataSourceAdapter(config as MySQLDataSourceConfig);
-        schema = new Schema(new Engine('MySQL'));
-        break;
-      default:
-        printer.error('Only MySQL Data Source is supported.');
-    }
-    
-    try {
-      await adapter.initialize();
-    } catch(error) {
-      printer.error('Failed to connect to the specified RDS Data Source. Check the connection details in the schema and re-try. Use "amplify api update-secrets" to update the user credentials.');
-      console.log(error?.message);
-      throw(error);      
-    };
-
-    // If connection is successful, store the secrets in parameter store
-    if(!secretsExistInParameterStore) {
-      await storeConnectionSecrets(context, config.database, secrets, apiName);
-    }
-
-    const models = await adapter.getModels();
-    adapter.cleanup();
-    models.forEach(m => schema.addModel(m));
-
-    const schemaString = await constructRDSGlobalAmplifyInput(context, config, amplifyInput) + os.EOL + os.EOL + generateGraphQLSchema(schema);
-    writeSchemaFile(pathToSchemaFile, schemaString);
-
-    if(_.isEmpty(schemaString)) {
-      printer.warn('If your schema file is empty, it is likely that your database has no tables.');
-    }
-    printer.info(`Successfully imported the schema definition for ${config.database} database into ${pathToSchemaFile}`);
-  }
-  else {
+  if(!fs.existsSync(pathToSchemaFile)) {
     printer.info('No imported Data Sources to Generate GraphQL Schema.');
+    return;
   }
+
+  // read and validate the RDS connection parameters
+  const amplifyInput = await getRDSGlobalAmplifyInput(context, pathToSchemaFile);
+  const config = await getRDSDBConfigFromAmplifyInput(context, amplifyInput);
+
+  // read and validate the RDS connection secrets
+  const { secrets, storeSecrets } = await getConnectionSecrets(context, config.database, apiName);
+  config.username = secrets.username;
+  config.password = secrets.password;
+  
+  // Establish the connection
+  let adapter: DataSourceAdapter;
+  let schema: Schema;
+  switch(config.engine) {
+    case ImportedRDSType.MYSQL:
+      adapter = new MySQLDataSourceAdapter(config as MySQLDataSourceConfig);
+      schema = new Schema(new Engine('MySQL'));
+      break;
+    default:
+      printer.error('Only MySQL Data Source is supported.');
+  }
+  
+  try {
+    await adapter.initialize();
+  } catch(error) {
+    printer.error('Failed to connect to the specified RDS Data Source. Check the connection details in the schema and re-try. Use "amplify api update-secrets" to update the user credentials.');
+    console.log(error?.message);
+    throw(error);      
+  };
+
+  // If connection is successful, store the secrets in parameter store
+  if(storeSecrets) {
+    await storeConnectionSecrets(context, config.database, secrets, apiName);
+  }
+
+  const models = await adapter.getModels();
+  adapter.cleanup();
+  models.forEach(m => schema.addModel(m));
+
+  const schemaString = await constructRDSGlobalAmplifyInput(context, config, amplifyInput) + os.EOL + os.EOL + generateGraphQLSchema(schema);
+  writeSchemaFile(pathToSchemaFile, schemaString);
+
+  if(_.isEmpty(schemaString)) {
+    printer.warn('If your schema file is empty, it is likely that your database has no tables.');
+  }
+  printer.info(`Successfully imported the schema definition for ${config.database} database into ${pathToSchemaFile}`);
+};
+
+const getConnectionSecrets = async (context: $TSContext, database: string, apiName: string): Promise<{ secrets: RDSConnectionSecrets, storeSecrets: boolean }> => {
+  const existingSecrets = await getExistingConnectionSecrets(context, database, apiName);
+  if(existingSecrets) {
+    return {
+      secrets: existingSecrets,
+      storeSecrets: false 
+    };
+  }
+
+  const userInputSecrets = await getDBUserSecretsWalkthrough(database);
+  return {
+    secrets: userInputSecrets,
+    storeSecrets: true 
+  };
 };
--- packages/amplify-category-api/src/commands/api/update-secrets.ts
+++ packages/amplify-category-api/src/commands/api/update-secrets.ts
@@ -20,37 +20,38 @@ export const run = async (context: $TSContext) => {
 
   // proceed if there are any existing imported Relational Data Sources
   const pathToSchemaFile = path.join(apiResourceDir, RDS_SCHEMA_FILE_NAME);
-  if(fs.existsSync(pathToSchemaFile)) {
-    // read and validate the RDS connection parameters
-    const amplifyInput = await getRDSGlobalAmplifyInput(context, pathToSchemaFile);
-    const config = await getRDSDBConfigFromAmplifyInput(context, amplifyInput);
-
-    const secrets = await getDBUserSecretsWalkthrough(config.database);
-    config.username = secrets?.username;
-    config.password = secrets?.password;
-
-    await storeConnectionSecrets(context, config.database, secrets, apiName);
-    
-    // Establish the connection
-    let adapter: DataSourceAdapter;
-    switch(config.engine) {
-      case ImportedRDSType.MYSQL:
-        adapter = new MySQLDataSourceAdapter(config as MySQLDataSourceConfig);
-        break;
-      default:
-        printer.error('Only MySQL Data Source is supported.');
-    }
-    
-    try {
-      await adapter.initialize();
-    } catch(error) {
-      printer.error('Failed to connect to the specified RDS Data Source. Check the connection details in the schema and re-try. Use "amplify api update-secrets" to update the user credentials.');
-      console.log(error?.message);
-      throw(error);      
-    };
-    adapter.cleanup();
-  }
-  else {
+  if(!fs.existsSync(pathToSchemaFile)) {
     printer.info('No imported Data Sources to update the secrets.');
+    return;
   }
+
+  // read and validate the RDS connection parameters
+  const amplifyInput = await getRDSGlobalAmplifyInput(context, pathToSchemaFile);
+  const config = await getRDSDBConfigFromAmplifyInput(context, amplifyInput);
+
+  const secrets = await getDBUserSecretsWalkthrough(config.database);
+  config.username = secrets.username;
+  config.password = secrets.password;
+
+  await storeConnectionSecrets(context, config.database, secrets, apiName);
+  
+  // Establish the connection
+  let adapter: DataSourceAdapter;
+  switch(config.engine) {
+    case ImportedRDSType.MYSQL:
+      adapter = new MySQLDataSourceAdapter(config as MySQLDataSourceConfig);
+      break;
+    default:
+      printer.error('Only MySQL Data Source is supported.');
+  }
+  
+  try {
+    await adapter.initialize();
+  } catch(error) {
+    printer.error('Failed to connect to the specified RDS Data Source. Check the connection details in the schema and re-try. Use "amplify api update-secrets" to update the user credentials.');
+    console.log(error?.message);
+    throw(error);      
+  };
+  adapter.cleanup();
+  printer.info(`Successfully updated the secrets for ${config.database} database.`);
 };
