7f53a0920627f572fa126b3433860c39bef502a3
Manaswi Manthena <manamaha@amazon.com>
2023-01-29 23:50:27 -0800

    feat: initial commit to add a helper method

--- packages/amplify-graphql-model-transformer/src/resolvers/common.ts
+++ packages/amplify-graphql-model-transformer/src/resolvers/common.ts
@@ -34,3 +34,54 @@ export const generateAuthExpressionForSandboxMode = (enabled: boolean): string =
 export const generateResolverKey = (typeName: string, fieldName: string): string => {
   return `${typeName}.${fieldName}`;
 };
+
+/**
+ * Util function to convert any GraphQL input filter argument to an AWS RDS query expression 
+ */
+export const toRDSFilterExpression = (filter: any): string => { 
+  let rdsExpression = '';
+  if (filter !== null && filter !== undefined) { 
+    const subExpressions = [];
+    for (const key in filter) {
+      const filterValue = filter[key];
+      if (filterValue !== null && filterValue !== undefined) {
+        if (key === 'contains') {
+          subExpressions.push(` like '%${filterValue}%'`);
+        } else if (key === 'eq') {
+          subExpressions.push(` = '${filterValue}'`);
+        } else if (key === 'ne') {
+          subExpressions.push(` != '${filterValue}'`);
+        } else if (key === 'lt') {
+          subExpressions.push(` < '${filterValue}'`);
+        } else if (key === 'le') {
+          subExpressions.push(` <= '${filterValue}'`);
+        } else if (key === 'gt') {
+          subExpressions.push(` > '${filterValue}'`);
+        } else if (key === 'ge') {
+          subExpressions.push(` >= '${filterValue}'`);
+        } else if (key === 'between') {
+          subExpressions.push(` between '${filterValue[0]}' and '${filterValue[1]}'`);
+        } else if (key === 'beginsWith') {
+          subExpressions.push(` like '${filterValue}%'`);
+        } else if (key === 'attributeExists') {
+          subExpressions.push(` is not null`);
+        } else if (key === 'attributeType') {
+          subExpressions.push(` is ${filterValue}`);
+        } else if (key === 'attributeNotExists') {
+          subExpressions.push(` is null`);
+        } else if (key === 'in') {
+          subExpressions.push(` in (${filterValue.join(',')})`);
+        } else if (key === 'notContains') {
+          subExpressions.push(` not like '%${filterValue}%'`);
+        } else if (key === 'notIn') {
+          subExpressions.push(` not in (${filterValue.join(',')})`);
+        } else if (key === 'and' || key === 'or') {
+          const subExpression = toRDSFilterExpression(filterValue);
+          subExpressions.push(` ${key} ${subExpression}`);
+        }
+      }
+    } 
+    rdsExpression = subExpressions.join(' and ');
+  }
+  return rdsExpression;
+};
