33ba2914f9a75571fba7e05acdcc6313a79aec7d
Marc VandenBerg <marc.vandenberg1@gmail.com>
2023-02-01 22:34:54 -0500

    Create resources for RDS lambda

--- packages/amplify-graphql-model-transformer/src/resources/rds-model-resource-generator.ts
+++ packages/amplify-graphql-model-transformer/src/resources/rds-model-resource-generator.ts
@@ -1,7 +1,10 @@
-import * as cdk from '@aws-cdk/core';
 import { TransformerContextProvider } from '@aws-amplify/graphql-transformer-interfaces';
+import { ResourceConstants } from 'graphql-transformer-common';
 import { ModelResourceGenerator } from './model-resource-generator';
 import { ModelVTLGenerator, RDSModelVTLGenerator } from '../resolvers';
+import { createRdsLambda, createRdsLambdaRole } from '../resolvers/rds';
+
+export const RDS_STACK_NAME = 'RdsApiStack';
 
 /**
  * An implementation of ModelResourceGenerator responsible for generated CloudFormation resources
@@ -10,7 +13,20 @@ import { ModelVTLGenerator, RDSModelVTLGenerator } from '../resolvers';
 export class RdsModelResourceGenerator extends ModelResourceGenerator {
   protected readonly generatorType = 'RdsModelResourceGenerator';
 
-  generateResources(ctx: TransformerContextProvider): void {
+  generateResources(context: TransformerContextProvider): void {
+    const { RDSLambdaIAMRoleLogicalID, RDSLambdaLogicalID, RDSLambdaDataSourceLogicalID } = ResourceConstants.RESOURCES;
+    const lambdaRoleStack = context.stackManager.getStackFor(RDSLambdaIAMRoleLogicalID, RDS_STACK_NAME);
+    const lambdaStack = context.stackManager.getStackFor(RDSLambdaLogicalID, RDS_STACK_NAME);
+    const role = createRdsLambdaRole(context.resourceHelper.generateIAMRoleName(RDSLambdaIAMRoleLogicalID), lambdaRoleStack);
+    const lambda = createRdsLambda(lambdaStack, context.api, role);
+
+    const lambdaDataSourceStack = context.stackManager.getStackFor(RDSLambdaDataSourceLogicalID, RDS_STACK_NAME);
+    const rdsDatasource = context.api.host.addLambdaDataSource(
+      `${RDSLambdaDataSourceLogicalID}DataSource`,
+      lambda,
+      { name: RDSLambdaDataSourceLogicalID },
+      lambdaDataSourceStack,
+    );
   }
 
   // eslint-disable-next-line class-methods-use-this
