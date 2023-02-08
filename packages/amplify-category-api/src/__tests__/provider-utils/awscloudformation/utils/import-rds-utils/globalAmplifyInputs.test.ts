3981bfa12dedafebb636c10ea24b0c64414c2408
phani-srikar <phanisrikar93ume@gmail.com>
2023-02-07 21:38:50 -0800

    fix: update tests

--- packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/utils/import-rds-utils/globalAmplifyInputs.test.ts
+++ packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/utils/import-rds-utils/globalAmplifyInputs.test.ts
@@ -27,10 +27,7 @@ describe('Amplify Input read/write from schema', () => {
 
   it('constructs valid default input parameters for MySQL datasource with global auth rule', async () => {
     const expectedGraphQLInputString = `input Amplify {
-      engine: String = \"mysql\"  
-      host: String = \"ENTER YOUR DATABASE HOSTNAME HERE\"  
-      port: Int = 3306 # ENTER PORT NUMBER HERE
-      database: String = \"ENTER YOUR DATABASE NAME HERE\" 
+      engine: String = \"mysql\"
       globalAuthRule: AuthRule = { allow: public } # This "input" configures a global authorization rule to enable public access to all models in this schema. Learn more about authorization rules here:https://docs.amplify.aws/cli/graphql/authorization-rules 
     }`;
     const constructedInputString = await constructDefaultGlobalAmplifyInput(mockContext, ImportedRDSType.MYSQL);
@@ -59,42 +56,16 @@ describe('Amplify Input read/write from schema', () => {
     expect(readInputNode).toMatchSnapshot();
   });
 
-  it('fetch DB config from the input arguments for database connection details', async () => {
-    const mockValidInputs = {
-      engine: 'mysql',
-      host: 'mockdatabase.rds.amazonaws.com',
-      port: '1010',
-      database: 'mockdatabase'
-    };
-
-    const mockInputSchema = `input Amplify {
-      engine: String = \"${mockValidInputs.engine}\"  
-      host: String = \"${mockValidInputs.host}\"  
-      port: Int = ${mockValidInputs.port} # ENTER PORT NUMBER HERE
-      database: String = \"${mockValidInputs.database}\" 
-      globalAuthRule: AuthRule = { allow: public } # This "input" configures a global authorization rule to enable public access to all models in this schema. Learn more about authorization rules here:https://docs.amplify.aws/cli/graphql/authorization-rules 
-    }`;
-    readFileSync_mock.mockReturnValue(mockInputSchema);
-
-    const readInputs = await readRDSGlobalAmplifyInput('');
-    const readConfig = await getRDSDBConfigFromAmplifyInput(mockContext, readInputs);
-
-    expect(readConfig).toEqual(mockValidInputs);
-  });
-
   it('constructs the global Amplify input from given config', async () => {
     const mockValidInputs = {
-      engine: 'mysql',
+      engine: 'postgresql',
       host: 'mockdatabase.rds.amazonaws.com',
       port: '1010',
       database: 'mockdatabase'
     };
 
     const mockInputSchema = `input Amplify {
-      engine: String = \"${mockValidInputs.engine}\"  
-      host: String = \"${mockValidInputs.host}\"  
-      port: Int = ${mockValidInputs.port} # ENTER PORT NUMBER HERE
-      database: String = \"${mockValidInputs.database}\" 
+      engine: String = \"${mockValidInputs.engine}\" 
       globalAuthRule: AuthRule = { allow: public } # This "input" configures a global authorization rule to enable public access to all models in this schema. Learn more about authorization rules here:https://docs.amplify.aws/cli/graphql/authorization-rules 
     }`;
 
@@ -103,45 +74,12 @@ describe('Amplify Input read/write from schema', () => {
     const readInputNode = await readRDSGlobalAmplifyInput('mock/path');
 
     const userInputs = {
-      host: 'otherdatabase.rds.amazonaws.com',
-      port: 2020,
-      database: 'otherdatabase'
+      host: 'mockdatabase.rds.amazonaws.com',
+      port: 1010,
+      database: 'mockdatabase'
     };
 
     const constructedInputDefinition = await constructRDSGlobalAmplifyInput(mockContext, userInputs, '');
     expect(constructedInputDefinition).toMatchSnapshot();
-  });
-
-  it('reports missing input arguments for database connection details', async () => {
-    const mockInvalidInputs = {
-      engine: 'mysql',
-      port: '1010',
-      database: 'mockdatabase'
-    };
-
-    try {
-      // await validateRDSInputDBConfig(mockContext, mockInvalidInputs);
-      fail('invalid input configuration is not reported');
-    }
-    catch(error) {
-      expect(error?.message).toEqual('The database connection parameters: host, are missing. Specify them in the schema and re-run.');
-    }
-  });
-
-  it('reports invalid input arguments for database connection details', async () => {
-    const mockInvalidInputs = {
-      engine: 'mysql',
-      host: 'ENTER YOUR DATABASE HOSTNAME HERE', //invalid entry
-      port: '1010',
-      database: 'mockdatabase'
-    };
-
-    try {
-      // await validateRDSInputDBConfig(mockContext, mockInvalidInputs);
-      fail('invalid input configuration is not reported');
-    }
-    catch(error) {
-      expect(error?.message).toEqual('The database connection parameters: host, might be invalid. Correct them in the schema and re-run.');
-    }
   });
 });
