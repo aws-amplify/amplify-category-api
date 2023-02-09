73389da088e794f89b5c649342ee680cbbb441c9
Christopher Sundersingh <sundersc@amazon.com>
2023-02-09 14:39:21 -0800

    fix(graphql): index rds query

--- packages/amplify-graphql-model-transformer/rds-lambda/clients/mysql/MySQLClient.ts
+++ packages/amplify-graphql-model-transformer/rds-lambda/clients/mysql/MySQLClient.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex';
 import { DBClient } from '../DBClient';
 import { BaseRequest, Request } from '../../interfaces/BaseRequest';
-import { ListRequest } from '../../interfaces/ListRequest';
+import { IndexRequest, ListRequest } from '../../interfaces/ListRequest';
 
 export abstract class MySQLClient extends DBClient {
   client: Knex;
@@ -20,6 +20,8 @@ export abstract class MySQLClient extends DBClient {
           return this.executeList(request as ListRequest);
         case 'DELETE':
           return this.executeDelete(request as Request);
+        case 'INDEX':
+          return this.executeIndex(request as IndexRequest);
         default:
           throw Error('Invalid operation');
       }
@@ -88,5 +90,23 @@ export abstract class MySQLClient extends DBClient {
     await query.delete();
 
     return result ? result[0] : {};
+  }
+
+  private executeIndex = async (request: IndexRequest): Promise<any> => {
+    const searchFilter = JSON.parse(JSON.stringify(request.args.input));
+    ['filter', 'limit', 'nextToken', 'sortDirection'].forEach(key => {
+      delete searchFilter[key];
+    });
+    const nextOffset = request.args?.nextToken
+      ? Number.parseInt(Buffer.from(request.args.nextToken, 'base64').toString('utf-8'), 10)
+      : 0;
+    const limit = request.args?.limit ?? 100;
+    const query = (await this.getClient())(request.table).where(searchFilter).select().offset(nextOffset).limit(limit);
+
+    this.addSortConditions(query, request);
+    const result = await query.returning('*');
+    const endOfResults = result?.length && result?.length < limit;
+    const nextToken = endOfResults ? null : Buffer.from((nextOffset + request.args.limit).toString()).toString('base64');
+    return { items: result, nextToken };
   }
 }
