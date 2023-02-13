79c684a9112f2c1c4f4c04100c5b100c92aadb3b
phani-srikar <phanisrikar93ume@gmail.com>
2023-02-12 16:13:06 -0800

    chore: fix minor issues

--- .circleci/post_publish.sh
+++ .circleci/post_publish.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash
 
 REPO_NAME=amplify-cli
 REPO_URL="git@github.com:aws-amplify/$REPO_NAME.git"
+GIT_USER_NAME="amplify-data-dev-git"
+GIT_USER_EMAIL="amplify-data-dev+github@amazon.com"
 FEATURE_BRANCH=guest/data/update-data-packages
 BASE_BRANCH=dev
 COMMIT_MESSAGE="chore: bump API plugin dependencies"
@@ -18,46 +20,47 @@ Build and Unit test jobs run successfully.
 #### Checklist
 
 - [x] PR description included
-- [x] `yarn test` passes
+- [x] 'yarn test' passes
 
 By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.
 "
 
-function createVersionBumpPR {
-  # Create a temporary directory
-  tmp_dir=$(mktemp -d)
-  curr_dir=$pwd
-  echo "Current Directory: $curr_dir"
-  cd $tmp_dir
-  echo "Current Temp Directory: $pwd"
+# Create a temporary directory
+tmp_dir=$(mktemp -d)
+curr_dir=$pwd
+echo "Current Directory: $curr_dir"
+cd $tmp_dir
+echo "Current Temp Directory: $tmp_dir"
 
-  # Clone the CLI repo
-  git clone $REPO_URL --depth 1
-  cd $REPO_NAME
+# Clone the CLI repo
+git clone $REPO_URL --depth 1
+cd $REPO_NAME
 
-  # Checkout the feature branch to be used for version bumps
-  if git show-ref --quiet refs/heads/$FEATURE_BRANCH; then
-    git branch -D $FEATURE_BRANCH # Delete if already exists locally
-  fi
-  git checkout -b $FEATURE_BRANCH
+# Checkout the feature branch to be used for version bumps
+if git show-ref --quiet refs/heads/$FEATURE_BRANCH; then
+  git branch -D $FEATURE_BRANCH # Delete if already exists locally
+fi
+git checkout -b $FEATURE_BRANCH
 
-  # Pull latest changes from remote if any
-  git pull
+# Pull latest changes from remote if any
+git pull
 
-  # Bump the versions locally
-  yarn update-data-packages
+# Bump the versions locally
+yarn update-data-packages
 
-  # Stage and commit the files
-  git add yarn.lock --no-verify
-  git add packages/*/package.json
-  git commit -m "'$COMMIT_MESSAGE'"
+# Stage and commit the files
+git status
+git config user.name "'$GIT_USER_NAME'"
+git config user.email "'$GIT_USER_EMAIL'"
+git add yarn.lock
+git add packages/*/package.json
+git commit -m "'$COMMIT_MESSAGE'"
 
-  # Sanity check the version bump changes before pushing
-  yarn build && yarn test
+# Sanity check the version bump changes before pushing
+yarn build && yarn test
 
-  # Push to upstream
-  git push --set-upstream origin $FEATURE_BRANCH
+# Push to upstream
+git push --set-upstream origin $FEATURE_BRANCH
 
-  # Create a PR to dev branch of CLI
-  gh pr create --base $BASE_BRANCH --head $FEATURE_BRANCH --title "'$COMMIT_MESSAGE'" --body "'$PR_BODY'"
-}
+# Create a PR to dev branch of CLI
+gh pr create --base $BASE_BRANCH --head $FEATURE_BRANCH --title "'$COMMIT_MESSAGE'" --body "'$PR_BODY'"
