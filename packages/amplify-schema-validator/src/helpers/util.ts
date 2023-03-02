ee7cdc67cd32284b45c8a6ed4fe2fb61cb9e4162
Surbhi Bansal <bzsurbhi@amazon.com>
2023-03-02 10:19:34 -0800

    fix: schema validations

--- packages/amplify-schema-validator/src/helpers/util.ts
+++ packages/amplify-schema-validator/src/helpers/util.ts
@@ -1,12 +1,10 @@
-export function getGraphqlName(val: string): string {
+export const getGraphqlName = (val: string): string => {
   if (!val.trim()) {
     /* istanbul ignore next */
     return '';
   }
   const cleaned = val.replace(/^[^_A-Za-z]+|[^_0-9A-Za-z]/g, '');
   return cleaned;
-}
+};
 
-export function toUpper(word: string): string {
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
+export const toUpper = (word: string): string => word.charAt(0).toUpperCase() + word.slice(1);
