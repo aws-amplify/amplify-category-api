ad8caf7c609a1c05d34f76cb45a71174a08be48d
Surbhi Bansal <bzsurbhi@amazon.com>
2023-03-15 13:17:36 -0700

    fix: added schema validation

--- packages/amplify-schema-validator/src/validators/correct-type-in-many-to-many-relation.ts
+++ packages/amplify-schema-validator/src/validators/correct-type-in-many-to-many-relation.ts
@@ -15,73 +15,72 @@ import { resolveFieldTypeName } from '../helpers/resolve-field-type-name';
  */
 export const validateCorrectTypeInManyToManyRelation = (schema: DocumentNode): Error[] => {
   const errors: Error[] = [];
-    const objectTypeDefinitions = schema.definitions.filter(
-      (defintion) => defintion.kind === Kind.OBJECT_TYPE_DEFINITION,
-    ) as ObjectTypeDefinitionNode[];
+  const objectTypeDefinitions = schema.definitions.filter(
+    (defintion) => defintion.kind === Kind.OBJECT_TYPE_DEFINITION,
+  ) as ObjectTypeDefinitionNode[];
 
-    const relationObjectNames = {} as { [relationName: string]: string[] };
-    objectTypeDefinitions.forEach((objectTypeDefinition) => {
-      const directiveFields = objectTypeDefinition.fields?.filter((objectField) => objectField.directives?.find(
-        (directive) => directive.name.value === 'manyToMany',
-      ));
+  const relationObjectNames = {} as { [relationName: string]: string[] };
+  objectTypeDefinitions.forEach((objectTypeDefinition) => {
+    const directiveFields = objectTypeDefinition.fields?.filter((objectField) => objectField.directives?.find(
+      (directive) => directive.name.value === 'manyToMany',
+    ));
 
-      const objectName = objectTypeDefinition.name.value;
+    const objectName = objectTypeDefinition.name.value;
 
-      directiveFields?.forEach((directiveField) => {
-        const directiveArgs = directiveField.directives?.filter((directive) => directive.arguments && directive.arguments.length > 0);
-        directiveArgs?.forEach((directiveArg) => {
-          const fieldArg = directiveArg?.arguments?.find((arg) => arg.name.value === 'relationName');
-          if (!fieldArg) {
-            /* istanbul ignore next */
-            return;
-          }
+    directiveFields?.forEach((directiveField) => {
+      const directiveArgs = directiveField.directives?.filter((directive) => directive.arguments && directive.arguments.length > 0);
+      directiveArgs?.forEach((directiveArg) => {
+        const fieldArg = directiveArg?.arguments?.find((arg) => arg.name.value === 'relationName');
+        if (!fieldArg) {
+          /* istanbul ignore next */
+          return;
+        }
 
-          const relationName = (fieldArg?.value as StringValueNode)?.value;
+        const relationName = (fieldArg?.value as StringValueNode)?.value;
 
-          if (relationObjectNames[relationName]) {
-            relationObjectNames[relationName].push(objectName);
-          } else {
-            const objectNames = [];
-            objectNames.push(objectName);
-            relationObjectNames[relationName] = objectNames;
-          }
-        })
-      })
+        if (relationObjectNames[relationName]) {
+          relationObjectNames[relationName].push(objectName);
+        } else {
+          const objectNames = [];
+          objectNames.push(objectName);
+          relationObjectNames[relationName] = objectNames;
+        }
+      });
     });
+  });
 
+  objectTypeDefinitions.forEach((objectTypeDefinition) => {
+    const directiveFields = objectTypeDefinition.fields?.filter((objectField) => objectField.directives?.find(
+      (directive) => directive.name.value === 'manyToMany',
+    ));
+    const objectName = objectTypeDefinition.name.value;
+    directiveFields?.forEach((directiveField) => {
+      const typeName = resolveFieldTypeName(directiveField.type);
+      const directiveArgs = directiveField.directives?.filter((directive) => directive.arguments && directive.arguments.length > 0);
+      directiveArgs?.forEach((directiveArg) => {
+        const fieldArg = directiveArg?.arguments?.find((arg) => arg.name.value === 'relationName');
+        if (!fieldArg) {
+          /* istanbul ignore next */
+          return;
+        }
 
-    objectTypeDefinitions.forEach((objectTypeDefinition) => {
-      const directiveFields = objectTypeDefinition.fields?.filter((objectField) => objectField.directives?.find(
-        (directive) => directive.name.value === 'manyToMany',
-      ));
-      const objectName = objectTypeDefinition.name.value;
-      directiveFields?.forEach((directiveField) => {
-        const typeName = resolveFieldTypeName(directiveField.type);
-        const directiveArgs = directiveField.directives?.filter((directive) => directive.arguments && directive.arguments.length > 0);
-        directiveArgs?.forEach((directiveArg) => {
-          const fieldArg = directiveArg?.arguments?.find((arg) => arg.name.value === 'relationName');
-          if (!fieldArg) {
-            /* istanbul ignore next */
-            return;
-          }
+        const relationName = (fieldArg?.value as StringValueNode)?.value;
 
-          const relationName = (fieldArg?.value as StringValueNode)?.value;
+        const objectNames = relationObjectNames[relationName];
 
-          const objectNames = relationObjectNames[relationName];
-
-          if (typeName === objectName || !objectNames.includes(typeName)) {
-            let expectedName;
-            objectNames.forEach((name) => {
-              if (name !== objectName) {
-                expectedName = name;
-              }
-            });
-            errors.push(new InvalidDirectiveError(
-              `@manyToMany relation ${relationName} expects ${expectedName} but got ${typeName}`,
-            ));
-          }
-        })
-      })
+        if (typeName === objectName || !objectNames.includes(typeName)) {
+          let expectedName;
+          objectNames.forEach((name) => {
+            if (name !== objectName) {
+              expectedName = name;
+            }
+          });
+          errors.push(new InvalidDirectiveError(
+            `@manyToMany relation ${relationName} expects ${expectedName} but got ${typeName}`,
+          ));
+        }
+      });
     });
+  });
   return errors;
 };
