74aa8bb7bb76175563838179dbed50c5dea338bd
Dane Pilcher <dppilche@amazon.com>
2023-05-30 13:37:08 -0600

    refactor: remove unnecessary optional chaining

--- packages/amplify-graphql-index-transformer/src/graphql-index-transformer.ts
+++ packages/amplify-graphql-index-transformer/src/graphql-index-transformer.ts
@@ -69,7 +69,7 @@ export class IndexTransformer extends TransformerPluginBase {
   public after = (ctx: TransformerContextProvider): void => {
     if (!ctx.isProjectUsingDataStore()) return;
 
-    const overriddenResources = getResourceOverrides([this], ctx.filepaths.getBackendDirPath(), ctx?.stackManager);
+    const overriddenResources = getResourceOverrides([this], ctx.filepaths.getBackendDirPath(), ctx.stackManager);
     // construct sync VTL code
     this.resolverMap.forEach((syncVTLContent, resource) => {
       if (syncVTLContent) {
