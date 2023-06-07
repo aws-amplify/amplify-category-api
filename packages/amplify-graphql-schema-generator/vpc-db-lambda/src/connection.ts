2c18de6f9ca4723f3f80d355224cd3b4cfe8c423
Christopher Sundersingh <sundersc@amazon.com>
2023-06-07 08:30:24 -0700

    update js to ts files

---
+++ packages/amplify-graphql-schema-generator/vpc-db-lambda/src/connection.ts
@@ -0,0 +1,32 @@
+import knex, { Knex } from 'knex';
+
+export const establishDBConnection = (config: any): any => {
+  const databaseConfig = {
+    host: config.host,
+    database: config.database,
+    port: config.port,
+    user: config.username,
+    password: config.password,
+    ssl: { rejectUnauthorized: false},
+  };
+  try {
+    return knex({
+      client: 'mysql2',
+      connection: databaseConfig,
+      pool: {
+        min: 5,
+        max: 30,
+        createTimeoutMillis: 30000,
+        acquireTimeoutMillis: 30000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100
+      },
+      debug: false,
+    });
+  }
+  catch(err) {
+    console.log(err);
+    throw err;
+  }
+}
