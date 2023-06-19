7d13f6c7a348b0dc90c6f7ec0d9dae7770f9d8ee
Christopher Sundersingh <sundersc@amazon.com>
2023-06-19 14:07:42 -0700

    feat(api): accept database url in import api command

--- packages/amplify-category-api/src/provider-utils/awscloudformation/service-walkthroughs/import-appsync-api-walkthrough.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/service-walkthroughs/import-appsync-api-walkthrough.ts
@@ -15,6 +15,7 @@ import {
 } from '@aws-amplify/graphql-transformer-core';
 import { PREVIEW_BANNER, category } from '../../../category-constants';
 import { storeConnectionSecrets, getSecretsKey, getExistingConnectionSecrets } from '../utils/rds-resources/database-resources';
+import { parseDatabaseUrl } from '../utils/database-url';
 import * as path from 'path';
 import { RDS_SCHEMA_FILE_NAME } from '@aws-amplify/graphql-transformer-core';
 import { constructDefaultGlobalAmplifyInput } from '../utils/rds-input-utils';
@@ -54,50 +55,82 @@ export const importAppSyncAPIWalkthrough = async (context: $TSContext): Promise<
   await storeConnectionSecrets(context, databaseConfig, apiName, secretsKey);
 
   return {
-    apiName: apiName,
+    apiName,
     dataSourceConfig: databaseConfig,
   };
 };
 
-export const writeDefaultGraphQLSchema = async (context: $TSContext, pathToSchemaFile: string, databaseConfig: ImportedDataSourceConfig) => {
+export const writeDefaultGraphQLSchema = async (
+  context: $TSContext,
+  pathToSchemaFile: string,
+  databaseConfig: ImportedDataSourceConfig,
+): Promise<void> => {
   const dataSourceType = databaseConfig?.engine;
-  if(Object.values(ImportedRDSType).includes(dataSourceType)) {
+  if (Object.values(ImportedRDSType).includes(dataSourceType)) {
     const globalAmplifyInputTemplate = await constructDefaultGlobalAmplifyInput(context, databaseConfig.engine);
     writeSchemaFile(pathToSchemaFile, globalAmplifyInputTemplate);
-  }
-  else {
+  } else {
     throw new Error(`Data source type ${dataSourceType} is not supported.`);
   }
 };
 
-export const databaseConfigurationInputWalkthrough = async (engine: ImportedDataSourceType, database?: string): Promise<ImportedDataSourceConfig> => {
-  const databaseName = database || await prompter.input(`Enter the name of the ${formatEngineName(engine)} database to import:`);
-  const host = await prompter.input(`Enter the host for ${databaseName} database:`);
-  const port = await prompter.input<'one', number>(`Enter the port for ${databaseName} database:`, {
-    transform: input => Number.parseInt(input, 10),
-    validate: integer(),
-    initial: 3306
-  });
+export const databaseConfigurationInputWalkthrough = async (
+  engine: ImportedDataSourceType,
+): Promise<ImportedDataSourceConfig> => {
+  printer.info('Please provide the following database connection information:');
+  const url = await prompter.input('Enter the database url or host name:');
+
+  let isValidUrl = true;
+  const parsedDatabaseUrl = parseDatabaseUrl(url);
+  let {
+    host,
+    port,
+    database,
+    username,
+    password,
+  } = parsedDatabaseUrl;
+
+  if (!host) {
+    isValidUrl = false;
+    host = url;
+  }
+  if (!isValidUrl || !port) {
+    port = await prompter.input<'one', number>('Enter the port number:', {
+      transform: (input) => Number.parseInt(input, 10),
+      validate: integer(),
+      initial: 3306,
+    });
+  }
+
   // Get the database user credentials
-  const username = await prompter.input(`Enter the username for ${databaseName} database user:`);
-  const password = await prompter.input(`Enter the password for ${databaseName} database user:`, { hidden: true });
+  if (!isValidUrl || !username) {
+    username = await prompter.input('Enter the username:');
+  }
+
+  if (!isValidUrl || !password) {
+    password = await prompter.input('Enter the password:', { hidden: true });
+  }
+
+  if (!isValidUrl || !database) {
+    database = await prompter.input('Enter the database name:');
+  }
 
   return {
-    engine: engine,
-    database: databaseName,
-    host: host,
-    port: port,
-    username: username,
-    password: password
+    engine,
+    database,
+    host,
+    port,
+    username,
+    password,
   };
 };
 
-export const formatEngineName = (engine: ImportedDataSourceType) => {
-  switch(engine) {
+export const formatEngineName = (engine: ImportedDataSourceType): string => {
+  switch (engine) {
     case ImportedRDSType.MYSQL:
-      return "MySQL";
+      return 'MySQL';
     case ImportedRDSType.POSTGRESQL:
-      return "PostgreSQL";
+      return 'PostgreSQL';
     default:
       throw new Error(`Unsupported database engine: ${engine}`);
   }
