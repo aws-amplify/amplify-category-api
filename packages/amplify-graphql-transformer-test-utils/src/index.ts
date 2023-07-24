8e3278210f83ca1c195fbec3a6b4825c852fa85b
Al Harris <alharris@amazon.com>
2023-07-24 12:14:23 -0700

    fix: default-value, index, predictions, and searchable tests pass

--- packages/amplify-graphql-transformer-test-utils/src/index.ts
+++ packages/amplify-graphql-transformer-test-utils/src/index.ts
@@ -1,3 +1,4 @@
 export { testTransform } from './test-transform';
 export type { TestTransformParameters } from './test-transform';
 export type { Template, StackMapping, ResolversFunctionsAndSchema, NestedStacks, DeploymentResources } from './deployment-resources';
+export type { AmplifyApiGraphQlResourceStackTemplate } from './cdk-compat/amplify-api-resource-stack-types';
