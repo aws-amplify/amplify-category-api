1d610cea8ba4d2966b19bf0f9eec2fc24ae73a97
phani-srikar <phanisrikar93ume@gmail.com>
2023-07-27 11:54:52 -0700

    chore: use explicit scripts for tag and latest releases

--- scripts/cloud-release.sh
+++ scripts/cloud-release.sh
@@ -4,8 +4,13 @@ export RELEASE_ROLE_NAME=CodebuildDeveloper
 export RELEASE_PROFILE_NAME=AmplifyAPIPluginRelease
 export RELEASE_PROJECT_NAME=amplify-category-api-release-workflow
 
-function release {
-  branch_name=$(git branch --show-current)
+function triggerRelease {
+  if [ -z "$1" ]
+  then
+    branch_name=$(git branch --show-current)
+  else
+    branch_name=$1
+  fi
   echo "Running release workflow from branch ${branch_name}"
   triggerProjectBatch $RELEASE_ACCOUNT_PROD $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Prod" $RELEASE_PROJECT_NAME $branch_name
 }
