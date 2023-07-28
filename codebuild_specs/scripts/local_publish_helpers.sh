641a9d795b496ba1bee855fd7f56051fb7b846b8
Zeyu Li <lizeyu@amazon.com>
2023-07-28 15:04:30 -0700

    change verdaccio version

--- codebuild_specs/scripts/local_publish_helpers.sh
+++ codebuild_specs/scripts/local_publish_helpers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 custom_registry_url=http://localhost:4873
-default_verdaccio_package=verdaccio@5.1.2
+default_verdaccio_package=verdaccio@4.5.1
 
 function startLocalRegistry {
     # Start local registry
