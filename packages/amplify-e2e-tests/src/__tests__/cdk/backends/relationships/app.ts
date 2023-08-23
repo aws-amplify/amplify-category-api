efb1d2119eaaaaf961880261f6aaa3b751de79bd
Al Harris <alharris@amazon.com>
2023-08-23 16:16:36 -0700

    chore: add additional cdk e2e tests for relationships

---
+++ packages/amplify-e2e-tests/src/__tests__/cdk/backends/relationships/app.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { App, Stack, Duration } from 'aws-cdk-lib';
+import { AmplifyGraphqlApi } from '@aws-amplify/graphql-construct-alpha';
+
+const packageJson = require('../package.json');
+
+const app = new App();
+const stack = new Stack(app, packageJson.name.replace(/_/g, '-'), {
+  env: { region: process.env.CLI_REGION || 'us-west-2' },
+});
+
+new AmplifyGraphqlApi(stack, 'GraphqlApi', {
+  schema: /* GraphQL */ `
+    type Blog @model @auth(rules: [{ allow: public }]) {
+      title: String!
+      description: String!
+      posts: [Post] @hasMany
+    }
+
+    type Post @model @auth(rules: [{ allow: public }]) {
+      title: String!
+      content: [String]
+      blog: Blog @belongsTo
+      tags: [Tag] @manyToMany(relationName: "PostTags")
+      author: Author @hasOne
+    }
+
+    type Author @model @auth(rules: [{ allow: public }]) {
+      byLine: String!
+      firstName: String
+      lastName: String
+      additionalNameParts: [String]
+    }
+
+    type Tag @model @auth(rules: [{ allow: public }]) {
+      name: String!
+      posts: [Post] @manyToMany(relationName: "PostTags")
+    }
+  `,
+  authorizationConfig: {
+    apiKeyConfig: { expires: Duration.days(7) },
+  },
+});
