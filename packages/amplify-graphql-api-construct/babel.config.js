c43d3a2c7d40e3677eb477b0aebb089c3d72925a
Edward Foyle <foyleef@amazon.com>
2023-09-25 16:37:40 -0700

    chore: update comment

--- packages/amplify-graphql-api-construct/babel.config.js
+++ packages/amplify-graphql-api-construct/babel.config.js
@@ -1,7 +1,7 @@
 /**
  * To all ye who venture into the world of jest and babel config, I wish ye the best of luck
  *
- * This file is here because for some reason babel-jest does not load config from the package.json file
+ * This file is here because for some reason babel-jest in jest 26 does not load config from the package.json file
  * https://github.com/jestjs/jest/issues/6229#issuecomment-419885857
  */
 
