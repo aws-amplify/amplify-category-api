99fcebe4bc2a7ec7903de82d8b6679fbd73f339b
Al Harris <alharris@amazon.com>
2023-10-20 15:39:04 -0700

    chore: refactor to enable scaling tests out quickly

--- packages/amplify-graphql-api-construct-tests/src/__tests__/deploy-velocity/deploy-velocity-test-core.ts
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/deploy-velocity/deploy-velocity-test-core.ts
@@ -1,23 +1,29 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import { createNewProjectDir, deleteProjectDir, initCDKProject, cdkDeploy, cdkDestroy } from 'amplify-category-api-e2e-core';
-import { graphql } from '../../graphql-request';
+import { ValidateGraphqlOptions } from '../../graphql-request';
 
 jest.setTimeout(1000 * 60 * 60 /* 1 hour */);
 
-export type TestManagedTableDeploymentProps = {
+export type EndpointConfig = Pick<ValidateGraphqlOptions, 'apiEndpoint' | 'apiKey'>;
+
+export type TestManagedTableDeploymentProps<SetupState> = {
   name: string;
   initialSchema: string;
   updatedSchema: string;
   testDurationLimitMs: number;
+  dataSetup: (endpointConfig: EndpointConfig) => Promise<SetupState>;
+  dataValidate: (endpointConfig: EndpointConfig, state: SetupState) => Promise<void>;
 };
 
-export const testManagedTableDeployment = ({
+export const testManagedTableDeployment = <SetupState>({
   name,
   initialSchema,
   updatedSchema,
   testDurationLimitMs,
-}: TestManagedTableDeploymentProps): void => {
+  dataSetup,
+  dataValidate,
+}: TestManagedTableDeploymentProps<SetupState>): void => {
   describe(name, () => {
     let projRoot: string;
     let projFolderName: string;
@@ -32,55 +38,31 @@ export const testManagedTableDeployment = ({
     });
 
     test('CDK Multi GSI', async () => {
+      // Setup App
       const templatePath = path.resolve(path.join(__dirname, '..', 'backends', 'managed-table-testbench'));
       const stackName = await initCDKProject(projRoot, templatePath);
       const schemaFilePath = path.join(projRoot, 'bin', 'schema.graphql');
 
+      // Deploy with initial schema, and generate config for data operations
       fs.writeFileSync(schemaFilePath, initialSchema);
       const outputs = await cdkDeploy(projRoot, '--all');
       const { awsAppsyncApiEndpoint: apiEndpoint, awsAppsyncApiKey: apiKey } = outputs[stackName];
+      const endpointConfig: EndpointConfig = { apiEndpoint, apiKey };
 
-      const createResult = await graphql(
-        apiEndpoint,
-        apiKey,
-        /* GraphQL */ `
-          mutation CREATE_TODO {
-            createTodo(input: { field1: "field1Value", field2: "field2Value" }) {
-              id
-            }
-          }
-        `,
-      );
-      expect(createResult.statusCode).toEqual(200);
-      const todoId = createResult.body.data.createTodo.id;
+      // Setup initial data
+      const setupState = await dataSetup(endpointConfig);
 
-      const getTodoQuery = /* GraphQL */ `
-        query GET_TODO {
-          getTodo(id: "${todoId}") {
-            id
-          }
-        }
-      `;
+      // Verify Data Correctness Pre-Deploy
+      await dataValidate(endpointConfig, setupState);
 
-      const getTodoResult = await graphql(apiEndpoint, apiKey, getTodoQuery);
-      expect(getTodoResult.statusCode).toEqual(200);
-
-      const retrievedTodo = getTodoResult.body.data.getTodo;
-      expect(retrievedTodo).toBeDefined();
-      expect(retrievedTodo.id).toEqual(todoId);
-
+      // Update Schema, and execute iterative deployment, timing the deploy
       fs.writeFileSync(schemaFilePath, updatedSchema);
       const deployStartTimestamp = Date.now();
       await cdkDeploy(projRoot, '--all');
       const deployDurationMs = Date.now() - deployStartTimestamp;
 
-      // Verify Data Correctness
-      const getTodoPostUpdateResult = await graphql(apiEndpoint, apiKey, getTodoQuery);
-      expect(getTodoPostUpdateResult.statusCode).toEqual(200);
-
-      const retrievedTodoPostUpdate = getTodoPostUpdateResult.body.data.getTodo;
-      expect(retrievedTodoPostUpdate).toBeDefined();
-      expect(retrievedTodoPostUpdate.id).toEqual(todoId);
+      // Verify Data Correctness Post-Deploy
+      await dataValidate(endpointConfig, setupState);
 
       // Verify Deploy Speed
       console.log(`Iterative Deploy Duration was ${deployDurationMs}ms`);
--- packages/amplify-graphql-api-construct-tests/src/__tests__/deploy-velocity/multi-gsi-1-record.test.ts
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/deploy-velocity/multi-gsi-1-record.test.ts
@@ -1,7 +1,8 @@
-import { testManagedTableDeployment } from './deploy-velocity-test-core';
+import { validateGraphql } from '../../graphql-request';
+import { EndpointConfig, testManagedTableDeployment } from './deploy-velocity-test-core';
 
 testManagedTableDeployment({
-  name: 'Single Row- 2 GSIs updated',
+  name: 'Single Record - 2 GSIs updated',
   testDurationLimitMs: 15 * 60 * 1000, // 15 Minutes
   initialSchema: /* GraphQL */ `
     type Todo @model @auth(rules: [{ allow: public }]) {
@@ -15,4 +16,34 @@ testManagedTableDeployment({
       field2: String! @index
     }
   `,
+  dataSetup: async (endpointConfig: EndpointConfig): Promise<string> => {
+    const result = await validateGraphql({
+      ...endpointConfig,
+      query: /* GraphQL */ `
+        mutation CREATE_TODO {
+          createTodo(input: { field1: "field1Value", field2: "field2Value" }) {
+            id
+          }
+        }
+      `,
+      expectedStatusCode: 200,
+    });
+    return result.body.data.createTodo.id;
+  },
+  dataValidate: async (endpointConfig: EndpointConfig, state: string): Promise<void> => {
+    const response = await validateGraphql({
+      ...endpointConfig,
+      query: /* GraphQL */ `
+      query GET_TODO {
+        getTodo(id: "${state}") {
+          id
+        }
+      }
+    `,
+      expectedStatusCode: 200,
+    });
+    const retrievedTodo = response.body.data.getTodo;
+    expect(retrievedTodo).toBeDefined();
+    expect(retrievedTodo.id).toEqual(state);
+  },
 });
