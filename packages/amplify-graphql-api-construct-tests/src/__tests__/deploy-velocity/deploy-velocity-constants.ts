e9e000fce0b0f9fc9c9b30939d5df244b0263cb5
Al Harris <91494052+alharris-at@users.noreply.github.com>
2023-10-24 13:16:52 -0700

    chore: add iterative update e2e test (#1973)
    * chore: add iterative update e2e test
    
    * chore: add reusable test harness
    
    * fix: use amplify table manager nested stack to avoid dependency issue
    
    * chore: refactor to enable scaling tests out quickly
    
    * chore: add additional data cases for single gsi update
    
    * fix: enable longer timeouts on iterative push, rewrite split tests a bit, tests run independently, tests pass
    
    * chore: add 1 gsi 100k record test
    
    * chore: add tests for 3 gsis added
    
    * chore: add gsi replacement test cases
    
    * chore: update multiple gsis with attr update
    
    * chore: forgot single record test
    
    * chore: all tests are composed of building blocks now
    
    * fix: only unlink if file exists
    
    * chore: updates per PR feedbakc
    
    * fix: shorten harness api name to mitigate layer name too long exceptions
    
    ---------
    
    Co-authored-by: Zeyu Li <lizeyu@amazon.com>

---
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/deploy-velocity/deploy-velocity-constants.ts
@@ -0,0 +1,83 @@
+import { AmplifyGraphqlApi } from '@aws-amplify/graphql-api-construct';
+
+const ONE_MINUTE = 60 * 1000;
+export const DURATION_10_MINUTES = 10 * ONE_MINUTE;
+export const DURATION_20_MINUTES = 20 * ONE_MINUTE;
+export const DURATION_30_MINUTES = 30 * ONE_MINUTE;
+export const DURATION_1_HOUR = 60 * ONE_MINUTE;
+
+export const COUNT_1_THOUSAND = 1000;
+export const COUNT_10_THOUSAND = 10000;
+export const COUNT_100_THOUSAND = 100000;
+
+export const SCHEMA_ONE_FIELD_NO_INDEX = /* GraphQL */ `
+  type Todo @model @auth(rules: [{ allow: public }]) {
+    field1: String!
+  }
+`;
+
+export const SCHEMA_ONE_FIELD_ALL_INDEXED = /* GraphQL */ `
+  type Todo @model @auth(rules: [{ allow: public }]) {
+    field1: String! @index
+  }
+`;
+
+export const SCHEMA_THREE_FIELDS_NO_INDEX = /* GraphQL */ `
+  type Todo @model @auth(rules: [{ allow: public }]) {
+    field1: String!
+    field2: String!
+    field3: String!
+  }
+`;
+
+export const SCHEMA_THREE_FIELDS_ALL_INDEXED = /* GraphQL */ `
+  type Todo @model @auth(rules: [{ allow: public }]) {
+    field1: String! @index
+    field2: String! @index
+    field3: String! @index
+  }
+`;
+
+export const SCHEMA_FOUR_FIELDS_INITIAL_TWO_INDEXED = /* GraphQL */ `
+  type Todo @model @auth(rules: [{ allow: public }]) {
+    field1: String! @index
+    field2: String! @index
+    field3: String!
+    field4: String!
+  }
+`;
+
+export const SCHEMA_FOUR_FIELDS_FINAL_TWO_INDEXED = /* GraphQL */ `
+  type Todo @model @auth(rules: [{ allow: public }]) {
+    field1: String!
+    field2: String!
+    field3: String! @index
+    field4: String! @index
+  }
+`;
+
+export const MUTATION_ONE_FIELD_CREATE = (uuid: string, i: number): string => `mut${i}: createTodo(input: { field1: "${uuid}" }) { id }`;
+
+export const MUTATION_THREE_FIELD_CREATE = (uuid: string, i: number): string =>
+  `mut${i}: createTodo(input: { field1: "${uuid}", field2: "${uuid}", field3: "${uuid}" }) { id }`;
+
+export const MUTATION_FOUR_FIELD_CREATE = (uuid: string, i: number): string =>
+  `mut${i}: createTodo(input: { field1: "${uuid}", field2: "${uuid}", field3: "${uuid}", field4: "${uuid}" }) { id }`;
+
+export const API_POST_PROCESSOR_SET_PROVISIONED_THROUGHPUT_TWO_GSIS = (api: AmplifyGraphqlApi): void => {
+  const billingMode = 'PROVISIONED';
+  const provisionedThroughput = { readCapacityUnits: 10, writeCapacityUnits: 10 };
+  const todoTable = api.resources.cfnResources.cfnAmplifyTables.Todo;
+  todoTable.addPropertyOverride('billingMode', billingMode);
+  todoTable.addPropertyOverride('provisionedThroughput', provisionedThroughput);
+  todoTable.addPropertyOverride('globalSecondaryIndexes.0.provisionedThroughput', provisionedThroughput);
+  todoTable.addPropertyOverride('globalSecondaryIndexes.1.provisionedThroughput', provisionedThroughput);
+};
+
+export const MUTATION_ONE_FIELD_CREATE_STATIC = 'createTodo(input: { field1: "field1Value" }) { id }';
+
+export const MUTATION_THREE_FIELD_CREATE_STATIC =
+  'createTodo(input: { field1: "field1Value", field2: "field2Value", field3: "field3Value" }) { id }';
+
+export const MUTATION_FOUR_FIELD_CREATE_STATIC =
+  'createTodo(input: { field1: "field1Value", field2: "field2Value", field3: "field3Value", field4: "field4Value" }) { id }';
