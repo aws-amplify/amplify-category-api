62be7abc347cc85f0f6c9295e2e944930acb242d
Dane Pilcher <dppilche@amazon.com>
2023-11-01 10:43:22 -0600

    style: some eslint changes

--- packages/amplify-graphql-schema-generator/src/__tests__/generate.test.ts
+++ packages/amplify-graphql-schema-generator/src/__tests__/generate.test.ts
@@ -1,13 +1,13 @@
-import { graphqlSchemaFromRDSSchema } from '../';
 import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
 import { schemas } from './__utils__/schemas';
+import { graphqlSchemaFromRDSSchema } from '..';
 
 describe('generate', () => {
   describe('graphqlSchemaFromRDSSchema', () => {
     const cases = Object.entries(schemas)
-      .map(([engineType, schemas]) => Object.entries(schemas).map((schema) => [engineType, ...schema]))
+      .map(([engineType, schemasForEngine]) => Object.entries(schemasForEngine).map((schema) => [engineType, ...schema]))
       .reduce((accumulator, value) => accumulator.concat(value), []);
-    test.each(cases)('creates graphql schema from %p %p schema', (engineType, schemaName, schema) => {
+    test.each(cases)('creates graphql schema from %p %p schema', (engineType, _, schema) => {
       expect(graphqlSchemaFromRDSSchema(schema, engineType as ImportedRDSType, 2)).toMatchSnapshot();
     });
   });
