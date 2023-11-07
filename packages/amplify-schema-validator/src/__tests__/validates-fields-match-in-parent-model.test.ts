211f7166d416a10ce48337cb14c5551e5f94ee54
Surbhi Bansal <bzsurbhi@amazon.com>
2023-11-07 11:48:51 -0800

    fix: add hasMany directive in fields-match-in-parent-model validation

--- packages/amplify-schema-validator/src/__tests__/validates-fields-match-in-parent-model.test.ts
+++ packages/amplify-schema-validator/src/__tests__/validates-fields-match-in-parent-model.test.ts
@@ -8,6 +8,12 @@ describe('Validate Schema', () => {
     expect(() => validateSchema(schema)).toThrow(errorRegex);
   });
 
+  it('fails validation when hasMany field is not in the parent model', () => {
+    const schema = readSchema('invalid-field-not-in-parent-model-hasMany.graphql');
+    const errorRegex = 'email is not a field in Test';
+    expect(() => validateSchema(schema)).toThrow(errorRegex);
+  });
+
   it('passes validation when field is in the parent model', () => {
     const schema = readSchema('valid-field-in-parent-model.graphql');
     expect(() => validateSchema(schema)).not.toThrow();
