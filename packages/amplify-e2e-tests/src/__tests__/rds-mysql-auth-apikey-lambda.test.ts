20e9604749157b27b8c518b3f2262047f9205d11
phani-srikar <phanisrikar93ume@gmail.com>
2023-11-07 09:44:18 -0800

    test: fixes to mysql auth tests, run in ICN region

--- packages/amplify-e2e-tests/src/__tests__/rds-mysql-auth-apikey-lambda.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/rds-mysql-auth-apikey-lambda.test.ts
@@ -6,10 +6,10 @@ import { testRdsApiKeyAndLambdaAuth } from '../rds-v2-tests-common/rds-auth-apik
 
 describe('RDS MySQL ApiKey and Lambda directive on Models', () => {
   const queries = [
-    'CREATE TABLE "Blog" (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255))',
-    'CREATE TABLE "Post" (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255), "blogId" VARCHAR(40))',
-    'CREATE TABLE "User" (id VARCHAR(40) PRIMARY KEY, name VARCHAR(255))',
-    'CREATE TABLE "Profile" (id VARCHAR(40) PRIMARY KEY, details VARCHAR(255), "userId" VARCHAR(40))',
+    'CREATE TABLE Blog (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255))',
+    'CREATE TABLE Post (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255), blogId VARCHAR(40))',
+    'CREATE TABLE User (id VARCHAR(40) PRIMARY KEY, name VARCHAR(255))',
+    'CREATE TABLE Profile (id VARCHAR(40) PRIMARY KEY, details VARCHAR(255), userId VARCHAR(40))',
   ];
 
   testRdsApiKeyAndLambdaAuth(ImportedRDSType.MYSQL, queries);
--- packages/amplify-e2e-tests/src/__tests__/rds-mysql-auth-iam-apikey-lambda-subscription.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/rds-mysql-auth-iam-apikey-lambda-subscription.test.ts
@@ -6,10 +6,10 @@ import { testApiKeyLambdaIamAuthSubscription } from '../rds-v2-tests-common/rds-
 
 describe('MySQL ApiKey, IAM and Lambda Subscription Auth on Models', () => {
   const queries = [
-    'CREATE TABLE "Blog" (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255))',
-    'CREATE TABLE "Post" (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255), "blogId" VARCHAR(40))',
-    'CREATE TABLE "User" (id VARCHAR(40) PRIMARY KEY, name VARCHAR(255))',
-    'CREATE TABLE "Profile" (id VARCHAR(40) PRIMARY KEY, details VARCHAR(255), "userId" VARCHAR(40))',
+    'CREATE TABLE Blog (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255))',
+    'CREATE TABLE Post (id VARCHAR(40) PRIMARY KEY, content VARCHAR(255), blogId VARCHAR(40))',
+    'CREATE TABLE User (id VARCHAR(40) PRIMARY KEY, name VARCHAR(255))',
+    'CREATE TABLE Profile (id VARCHAR(40) PRIMARY KEY, details VARCHAR(255), userId VARCHAR(40))',
   ];
 
   testApiKeyLambdaIamAuthSubscription(ImportedRDSType.MYSQL, queries);
