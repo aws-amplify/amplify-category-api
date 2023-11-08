65d2b5697533d0f5eda4e762ea1193de846f05fb
Al Harris <alharris@amazon.com>
2023-11-08 15:50:50 -0800

    chore: add dependency license verification

---
+++ scripts/verify-dependency-licenses.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+change_dependency_licenses=$(git status | grep dependency_licenses.txt | wc -l)
+
+if [[ change_dependency_licenses -gt 0 ]]; then
+  echo "Detected license change. Please run 'yarn extract-dependency-licenses' and add dependency_licenses.txt file changes to the change set."
+  git status | grep API.md
+  exit 1;
+fi
