66e8c978af4948ffe97e0e55c363d30cfb2ecc04
Dane Pilcher <dppilche@amazon.com>
2023-11-14 11:27:00 -0700

    test: fix prov concurrency test assertions

--- packages/amplify-graphql-api-construct-tests/src/__tests__/sql-models.test.ts
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/sql-models.test.ts
@@ -9,6 +9,7 @@ import {
   setupRDSInstanceAndData,
   storeDbConnectionConfig,
 } from 'amplify-category-api-e2e-core';
+import { LambdaClient, GetProvisionedConcurrencyConfigCommand } from '@aws-sdk/client-lambda';
 import generator from 'generate-password';
 import { initCDKProject, cdkDeploy, cdkDestroy } from '../commands';
 import { graphql } from '../graphql-request';
@@ -121,6 +122,15 @@ describe('CDK GraphQL Transformer', () => {
 
     expect(listResult.body.data.listTodos.items.length).toEqual(1);
     expect(todo.id).toEqual(listResult.body.data.listTodos.items[0].id);
+    const client = new LambdaClient({ region });
+    const functionName = outputs.SQLLambdaFunctionName;
+    const functionAlias = 'RDSLambdaLogicalIDAlias';
+    const command = new GetProvisionedConcurrencyConfigCommand({
+      FunctionName: functionName,
+      Qualifier: functionAlias,
+    });
+    const response = await client.send(command);
+    expect(response.RequestedProvisionedConcurrentExecutions).toEqual(2);
   });
 });
 
