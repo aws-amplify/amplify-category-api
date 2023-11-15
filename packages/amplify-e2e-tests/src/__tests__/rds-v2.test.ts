7233fe7c4e55acd665cc6a412adb0bb17e07b5b0
phani-srikar <phanisrikar93ume@gmail.com>
2023-11-14 18:09:22 -0800

    chore: update sql layer versions

--- packages/amplify-e2e-tests/src/__tests__/rds-v2.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/rds-v2.test.ts
@@ -128,61 +128,61 @@ describe('RDS Tests', () => {
     expect(rdsMappingFile.data).toBeDefined();
     expect(rdsMappingFile.data).toMatchObject({
       'ap-northeast-1': {
-        layerRegion: 'arn:aws:lambda:ap-northeast-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-northeast-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'us-east-1': {
-        layerRegion: 'arn:aws:lambda:us-east-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:us-east-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'ap-southeast-1': {
-        layerRegion: 'arn:aws:lambda:ap-southeast-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-southeast-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'eu-west-1': {
-        layerRegion: 'arn:aws:lambda:eu-west-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:eu-west-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'us-west-1': {
-        layerRegion: 'arn:aws:lambda:us-west-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:us-west-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'ap-east-1': {
-        layerRegion: 'arn:aws:lambda:ap-east-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-east-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'ap-northeast-2': {
-        layerRegion: 'arn:aws:lambda:ap-northeast-2:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-northeast-2:582037449441:layer:AmplifySQLLayer:1',
       },
       'ap-northeast-3': {
-        layerRegion: 'arn:aws:lambda:ap-northeast-3:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-northeast-3:582037449441:layer:AmplifySQLLayer:1',
       },
       'ap-south-1': {
-        layerRegion: 'arn:aws:lambda:ap-south-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-south-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'ap-southeast-2': {
-        layerRegion: 'arn:aws:lambda:ap-southeast-2:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ap-southeast-2:582037449441:layer:AmplifySQLLayer:1',
       },
       'ca-central-1': {
-        layerRegion: 'arn:aws:lambda:ca-central-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:ca-central-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'eu-central-1': {
-        layerRegion: 'arn:aws:lambda:eu-central-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:eu-central-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'eu-north-1': {
-        layerRegion: 'arn:aws:lambda:eu-north-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:eu-north-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'eu-west-2': {
-        layerRegion: 'arn:aws:lambda:eu-west-2:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:eu-west-2:582037449441:layer:AmplifySQLLayer:1',
       },
       'eu-west-3': {
-        layerRegion: 'arn:aws:lambda:eu-west-3:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:eu-west-3:582037449441:layer:AmplifySQLLayer:1',
       },
       'sa-east-1': {
-        layerRegion: 'arn:aws:lambda:sa-east-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:sa-east-1:582037449441:layer:AmplifySQLLayer:1',
       },
       'us-east-2': {
-        layerRegion: 'arn:aws:lambda:us-east-2:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:us-east-2:582037449441:layer:AmplifySQLLayer:1',
       },
       'us-west-2': {
-        layerRegion: 'arn:aws:lambda:us-west-2:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:us-west-2:582037449441:layer:AmplifySQLLayer:1',
       },
       'me-south-1': {
-        layerRegion: 'arn:aws:lambda:me-south-1:582037449441:layer:AmplifySQLLayer:24',
+        layerRegion: 'arn:aws:lambda:me-south-1:582037449441:layer:AmplifySQLLayer:1',
       },
     });
   });
--- packages/amplify-graphql-model-transformer/src/resolvers/rds/resolver.ts
+++ packages/amplify-graphql-model-transformer/src/resolvers/rds/resolver.ts
@@ -62,73 +62,73 @@ const getLatestLayers = (latestLayers?: RDSLayerMapping): RDSLayerMapping => {
 // For prod use account '582037449441', layer name 'AmplifySQLLayer' and layer version '3' as of 2023-06-20
 const getDefaultLayerMapping = (): RDSLayerMapping => ({
   'ap-northeast-1': {
-    layerRegion: 'arn:aws:lambda:ap-northeast-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-northeast-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'us-east-1': {
-    layerRegion: 'arn:aws:lambda:us-east-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:us-east-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'ap-southeast-1': {
-    layerRegion: 'arn:aws:lambda:ap-southeast-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-southeast-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'eu-west-1': {
-    layerRegion: 'arn:aws:lambda:eu-west-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:eu-west-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'us-west-1': {
-    layerRegion: 'arn:aws:lambda:us-west-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:us-west-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'ap-east-1': {
-    layerRegion: 'arn:aws:lambda:ap-east-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-east-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'ap-northeast-2': {
-    layerRegion: 'arn:aws:lambda:ap-northeast-2:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-northeast-2:582037449441:layer:AmplifySQLLayer:1',
   },
   'ap-northeast-3': {
-    layerRegion: 'arn:aws:lambda:ap-northeast-3:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-northeast-3:582037449441:layer:AmplifySQLLayer:1',
   },
   'ap-south-1': {
-    layerRegion: 'arn:aws:lambda:ap-south-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-south-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'ap-southeast-2': {
-    layerRegion: 'arn:aws:lambda:ap-southeast-2:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ap-southeast-2:582037449441:layer:AmplifySQLLayer:1',
   },
   'ca-central-1': {
-    layerRegion: 'arn:aws:lambda:ca-central-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:ca-central-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'eu-central-1': {
-    layerRegion: 'arn:aws:lambda:eu-central-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:eu-central-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'eu-north-1': {
-    layerRegion: 'arn:aws:lambda:eu-north-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:eu-north-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'eu-west-2': {
-    layerRegion: 'arn:aws:lambda:eu-west-2:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:eu-west-2:582037449441:layer:AmplifySQLLayer:1',
   },
   'eu-west-3': {
-    layerRegion: 'arn:aws:lambda:eu-west-3:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:eu-west-3:582037449441:layer:AmplifySQLLayer:1',
   },
   'sa-east-1': {
-    layerRegion: 'arn:aws:lambda:sa-east-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:sa-east-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'us-east-2': {
-    layerRegion: 'arn:aws:lambda:us-east-2:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:us-east-2:582037449441:layer:AmplifySQLLayer:1',
   },
   'us-west-2': {
-    layerRegion: 'arn:aws:lambda:us-west-2:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:us-west-2:582037449441:layer:AmplifySQLLayer:1',
   },
   'cn-north-1': {
-    layerRegion: 'arn:aws:lambda:cn-north-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:cn-north-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'cn-northwest-1': {
-    layerRegion: 'arn:aws:lambda:cn-northwest-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:cn-northwest-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'us-gov-west-1': {
-    layerRegion: 'arn:aws:lambda:us-gov-west-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:us-gov-west-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'us-gov-east-1': {
-    layerRegion: 'arn:aws:lambda:us-gov-east-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:us-gov-east-1:582037449441:layer:AmplifySQLLayer:1',
   },
   'me-south-1': {
-    layerRegion: 'arn:aws:lambda:me-south-1:582037449441:layer:AmplifySQLLayer:24',
+    layerRegion: 'arn:aws:lambda:me-south-1:582037449441:layer:AmplifySQLLayer:1',
   },
 });
 
