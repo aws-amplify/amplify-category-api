6c93a647ea6945232e3fe0662995b5eb23bcd622
Christopher Sundersingh <sundersc@amazon.com>
2023-12-08 18:00:47 -0800

    lint fix

--- packages/amplify-graphql-schema-generator/src/schema-generator/generate-schema.ts
+++ packages/amplify-graphql-schema-generator/src/schema-generator/generate-schema.ts
@@ -192,7 +192,11 @@ const constructEnumType = (type: EnumType): EnumWrapper => {
   // Check if the enum values imported from the database contain any invalid character.
   // Enum can contain A-Z, a-z, 0-9 or underscore(_) and must begin with an alphabet or an underscore(_).
   if (!validateEnumValues(type.values)) {
-    throw new Error(`Enum "${type.name}" (values: ${type.values.join(',')}) contains one or more invalid values. Enum values can contain A-Z, a-z, 0-9 or underscore(_) and must begin with an alphabet or an underscore(_).`);
+    throw new Error(
+      `Enum "${type.name}" (values: ${type.values.join(
+        ',',
+      )}) contains one or more invalid values. Enum values can contain A-Z, a-z, 0-9 or underscore(_) and must begin with an alphabet or an underscore(_).`,
+    );
   }
   const enumValues = type.values.map((t) => {
     return {
@@ -215,10 +219,10 @@ const constructEnumType = (type: EnumType): EnumWrapper => {
 };
 
 const validateEnumValues = (values: string[]): boolean => {
- const regex = new RegExp(/^[_A-Za-z][_0-9A-Za-z]*$/);
- const containsValidValues = values.every((value) => regex.test(value));
- return containsValidValues;
-}
+  const regex = new RegExp(/^[_A-Za-z][_0-9A-Za-z]*$/);
+  const containsValidValues = values.every((value) => regex.test(value));
+  return containsValidValues;
+};
 
 const addIndexes = (type: ObjectDefinitionWrapper, indexes: Index[]): void => {
   indexes.forEach((index) => {
