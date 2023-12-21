4b1a71fd4f23e2895b2db794892f89339700fbbb
bzsurbhi <115104450+bzsurbhi@users.noreply.github.com>
2023-12-21 12:35:54 -0800

    feat: add Schema validations (#2129)
    * fix(graphql-schema-validation): relationship names not unique
    
    * fix(graphql-schema-validation): updated package size limit
    
    * fix(graphql-schema-validation): fixed lint issues
    
    * fix(graphql-schema-validation): lint
    
    * fix(graphql-schema-validation): lint fix
    
    * fix: add hasMany directive in fields-match-in-parent-model validation
    
    * fix: added schema validations
    
    * fix: improved error message in schema validations

---
+++ packages/amplify-schema-validator/src/validators/many-to-many-has-a-relationname.ts
@@ -0,0 +1,35 @@
+import { DocumentNode, Kind, ObjectTypeDefinitionNode, StringValueNode } from 'graphql';
+import { ValidationError } from '../exceptions/validation-error';
+
+/**
+ * Validates that directive manyTomany has a relationName
+ *
+ * @param schema graphql schema
+ * @returns true if manytomany has a relationName
+ */
+
+export const validateManyToManyHasRelationName = (schema: DocumentNode): Error[] => {
+  const errors: Error[] = [];
+  const objectTypeDefinitions = schema.definitions.filter(
+    (defintion) => defintion.kind === Kind.OBJECT_TYPE_DEFINITION,
+  ) as ObjectTypeDefinitionNode[];
+  objectTypeDefinitions.forEach((objectTypeDefinition) => {
+    const directiveFields = objectTypeDefinition.fields?.filter((objectField) =>
+      objectField.directives?.find((directive) => directive.name.value === 'manyToMany'),
+    );
+
+    directiveFields?.forEach((directiveField) => {
+      const directiveArgs = directiveField.directives?.filter((directive) => directive.arguments && directive.arguments.length > 0);
+      if (!directiveArgs || directiveArgs.length == 0) {
+        errors.push(new ValidationError('@manyToMany relation does not have a relationName'));
+      }
+      directiveArgs?.forEach((directiveArg) => {
+        const fieldArg = directiveArg?.arguments?.find((arg) => arg.name.value === 'relationName');
+        if (!fieldArg) {
+          errors.push(new ValidationError('@manyToMany relation does not have a relationName'));
+        }
+      });
+    });
+  });
+  return errors;
+};
