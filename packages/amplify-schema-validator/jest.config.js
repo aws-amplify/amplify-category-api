b5deb59b344160c77e66f3466eb1b1a8dbc6ac11
Surbhi Bansal <bzsurbhi@amazon.com>
2024-01-02 13:05:24 -0800

    fix: remove multiple jest configuration

--- packages/amplify-schema-validator/jest.config.js
+++ packages/amplify-schema-validator/jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
+  coverageProvider: 'v8',
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js}'],
-  coverageReporters: ['cobertura', 'lcov', 'text'],
+  coverageReporters: ['cobertura', 'lcov', 'text', 'clover'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/src/__tests__/helpers/'],
   testEnvironment: 'jsdom',
   coverageThreshold: {
     global: {
       lines: 100,
       statements: 100,
+      functions: 90,
+      branches: 77,
     },
   },
 };
