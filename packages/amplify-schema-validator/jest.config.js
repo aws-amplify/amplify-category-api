06d90f8322f130598f137f3b2379186b970cd095
bzsurbhi <115104450+bzsurbhi@users.noreply.github.com>
2024-01-03 11:45:55 -0800

    test: Remove multiple jest configuration from amplify-schema-validation package (#2169)

--- packages/amplify-schema-validator/jest.config.js
+++ packages/amplify-schema-validator/jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
+  coverageProvider: 'v8',
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js}'],
-  coverageReporters: ['cobertura', 'lcov', 'text'],
+  coverageReporters: ['cobertura', 'lcov', 'text', 'clover'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/src/__tests__/helpers/'],
   testEnvironment: 'jsdom',
   coverageThreshold: {
     global: {
-      lines: 100,
-      statements: 100,
+      lines: 90,
+      functions: 90,
+      branches: 65,
     },
   },
 };
