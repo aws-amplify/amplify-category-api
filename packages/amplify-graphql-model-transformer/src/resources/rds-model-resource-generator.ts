e510a3266cda939b330793c91cec42e066bc6a6b
Dane Pilcher <dppilche@amazon.com>
2024-01-05 15:38:08 -0700

    fix: set correct env var for host

--- packages/amplify-graphql-model-transformer/src/resources/rds-model-resource-generator.ts
+++ packages/amplify-graphql-model-transformer/src/resources/rds-model-resource-generator.ts
@@ -130,7 +130,7 @@ export class RdsModelResourceGenerator extends ModelResourceGenerator {
       environment.secretArn = secretEntry.secretArn;
       environment.port = secretEntry.port.toString();
       environment.database = secretEntry.databaseName;
-      environment.hostname = secretEntry.hostname;
+      environment.host = secretEntry.hostname;
     }
 
     const lambda = createRdsLambda(
