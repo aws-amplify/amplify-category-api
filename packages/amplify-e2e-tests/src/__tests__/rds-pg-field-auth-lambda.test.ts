a5e11c850f9ef7ab1d48fc94f91f7bcab33002a8
Christopher Sundersingh <sundersc@amazon.com>
2024-01-08 10:45:11 -0800

    test(api): add rds e2e for lambda authorizer field auth

--- packages/amplify-e2e-tests/src/__tests__/rds-pg-field-auth-lambda.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/rds-pg-field-auth-lambda.test.ts
@@ -1,15 +1,15 @@
 import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testRdsApiKeyFieldAuth } from '../rds-v2-tests-common/rds-field-auth-apikey';
+import { testRdsLambdaAuthorizerFieldAuth } from '../rds-v2-tests-common/rds-field-auth-lambda';
 
 // to deal with bug in cognito-identity-js
 (global as any).fetch = require('node-fetch');
 
-describe('RDS Postgres ApiKey field auth rules', () => {
+describe('RDS Postgres Lambda Authorizer field auth rules', () => {
   const queries = [
     'CREATE TABLE "Person1" (id INT PRIMARY KEY, "firstName" VARCHAR(255), "lastName" VARCHAR(255), ssn VARCHAR(20))',
     'CREATE TABLE "Person2" (id INT PRIMARY KEY, "firstName" VARCHAR(255), "lastName" VARCHAR(255), ssn VARCHAR(20))',
     'CREATE TABLE "Person3" (id INT PRIMARY KEY, "firstName" VARCHAR(255), "lastName" VARCHAR(255), ssn VARCHAR(20))',
   ];
 
-  testRdsApiKeyFieldAuth(ImportedRDSType.POSTGRESQL, queries);
+  testRdsLambdaAuthorizerFieldAuth(ImportedRDSType.POSTGRESQL, queries);
 });
