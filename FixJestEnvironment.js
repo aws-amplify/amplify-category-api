47df9f1458ee5fa442ba9bf5e8a456c9a8a259d6
Tim Schmelter <schmelte+github@amazon.com>
2024-01-17 12:25:10 -0800

    test: Restore custom CLI jest environment (#2200)

--- FixJestEnvironment.js
+++ FixJestEnvironment.js
@@ -1,10 +1,13 @@
 const NodeEnvironment = require('jest-environment-node');
 
+/**
+ * NOTE: Any fixes in this file should also be ported to packages/amplify-e2e-core/src/cli-test-environment.js
+ */
 class FixJestEnvironment extends NodeEnvironment {
   constructor(...args) {
     super(...args);
     // https://github.com/jestjs/jest/issues/12628
-    // sturcturedClone is missing in the jest node environment.
+    // structuredClone is missing in the jest node environment.
     // Newer versions of jest have fixed this, but is not possible to upgrade at this time.
     // Newer versions of jest have a performance issue when not used with Node ^20.11.0
     // https://github.com/jestjs/jest/issues/11956
