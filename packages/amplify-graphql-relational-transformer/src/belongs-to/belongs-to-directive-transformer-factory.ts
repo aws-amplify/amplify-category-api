f6c74221902829b6acfbf920016350edfd3fdb04
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-03-11 16:44:19 -0400

    split belongsTo DDB based on references vs fields

--- packages/amplify-graphql-relational-transformer/src/belongs-to/belongs-to-directive-transformer-factory.ts
+++ packages/amplify-graphql-relational-transformer/src/belongs-to/belongs-to-directive-transformer-factory.ts
@@ -3,15 +3,31 @@ import { BelongsToDirectiveConfiguration } from '../types';
 import { DataSourceBasedDirectiveTransformer } from '../data-source-based-directive-transformer';
 import { BelongsToDirectiveSQLTransformer } from './belongs-to-directive-sql-transformer';
 import { BelongsToDirectiveDDBFieldsTransformer } from './belongs-to-directive-ddb-fields-transformer';
+import { BelongsToDirectiveDDBReferencesTransformer } from './belongs-to-directive-ddb-references-transformer';
 
 export const getBelongsToDirectiveTransformer = (
   dbType: ModelDataSourceStrategyDbType,
+  config: BelongsToDirectiveConfiguration,
 ): DataSourceBasedDirectiveTransformer<BelongsToDirectiveConfiguration> => {
   switch (dbType) {
     case 'MYSQL':
     case 'POSTGRES':
       return new BelongsToDirectiveSQLTransformer(dbType);
     case 'DYNAMODB':
-      return new BelongsToDirectiveDDBFieldsTransformer(dbType);
+      // TODO: bifurcate and validate before getting to this stage.
+      if (config.references !== undefined && config.references.length >= 1) {
+        if (config.fields !== undefined && config.fields.length > 0) {
+          throw new Error(
+            'Something went wrong >> cannot have both references and fields.'
+          )
+        }
+        return new BelongsToDirectiveDDBReferencesTransformer(dbType);
+      } else if (config.fields !== undefined && config.fields.length >= 1) {
+        return new BelongsToDirectiveDDBFieldsTransformer(dbType);
+      } else {
+        throw new Error(
+          'Something went wrong >> cannot have both references and fields.'
+        )
+      }
   }
 };
