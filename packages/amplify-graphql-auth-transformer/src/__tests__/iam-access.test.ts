355ebb07272c4ecb53f475be76529e0469718fe2
Kamil Sobol <sobkamil@amazon.com>
2024-03-21 13:20:04 -0700

    add some sandbox tests

--- packages/amplify-graphql-auth-transformer/src/__tests__/iam-access.test.ts
+++ packages/amplify-graphql-auth-transformer/src/__tests__/iam-access.test.ts
@@ -9,6 +9,7 @@ const IAM_ACCESS_CHECK_RDS =
   '#if( $util.authType() == "IAM Authorization" && $util.isNull($ctx.identity.cognitoIdentityPoolId) && $util.isNull($ctx.identity.cognitoIdentityId) )';
 const IAM_ACCESS_CHECK_POST_AUTH =
   '#if( !($util.authType() == "IAM Authorization" && $util.isNull($ctx.identity.cognitoIdentityPoolId) && $util.isNull($ctx.identity.cognitoIdentityId)) )';
+const API_KEY_ACCESS_CHECK_POST_AUTH = '#if( $util.authType() != "API Key Authorization" )';
 
 describe('ddb', () => {
   test('simple model with apiKey and iam access', () => {
@@ -180,7 +181,31 @@ describe('ddb', () => {
     });
     expect(out).toBeDefined();
     expectOperationsWithDirectives(out.schema, ['@aws_iam'], ['@aws_iam']);
-    expectNoResolvers(out.resolvers);
+    expectNoAuthResolvers(out.resolvers);
+  });
+
+  test('simple model with no auth directive and both sandbox and iam access enabled', () => {
+    const validSchema = `
+      type Post @model {
+        id: ID!
+        title: String!
+        createdAt: String
+        updatedAt: String
+      }`;
+    const out = testTransform({
+      schema: validSchema,
+      synthParameters: {
+        enableIamAccess: true,
+      },
+      transformParameters: {
+        sandboxModeEnabled: true,
+      },
+      transformers: [new ModelTransformer(), new AuthTransformer()],
+    });
+    expect(out).toBeDefined();
+    expectOperationsWithDirectives(out.schema, ['@aws_api_key', '@aws_iam'], ['@aws_api_key', '@aws_iam']);
+    expectNoAuthResolvers(out.resolvers);
+    expectSandboxResolvers(out.resolvers, true, true);
   });
 });
 
@@ -361,7 +386,32 @@ describe('rds', () => {
     });
     expect(out).toBeDefined();
     expectOperationsWithDirectives(out.schema, ['@aws_iam'], ['@aws_iam']);
-    expectNoResolvers(out.resolvers);
+    expectNoAuthResolvers(out.resolvers);
+  });
+
+  test('simple model with no auth directive and sandbox and iam access enabled', () => {
+    const validSchema = `
+      type Post @model {
+        id: ID! @primaryKey
+        title: String!
+        createdAt: String
+        updatedAt: String
+      }`;
+    const out = testTransform({
+      schema: validSchema,
+      synthParameters: {
+        enableIamAccess: true,
+      },
+      transformParameters: {
+        sandboxModeEnabled: true,
+      },
+      transformers: [new ModelTransformer(), new AuthTransformer(), new PrimaryKeyTransformer()],
+      dataSourceStrategies: constructDataSourceStrategies(validSchema, mysqlStrategy),
+    });
+    expect(out).toBeDefined();
+    expectOperationsWithDirectives(out.schema, ['@aws_api_key', '@aws_iam'], ['@aws_api_key', '@aws_iam']);
+    expectNoAuthResolvers(out.resolvers);
+    expectSandboxResolvers(out.resolvers, true, true);
   });
 });
 
@@ -509,7 +559,7 @@ const expectResolversWithoutIamAccessCheck = (resolvers: Record<string, string>)
   expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).toMatchSnapshot();
 };
 
-const expectNoResolvers = (resolvers: Record<string, string>): void => {
+const expectNoAuthResolvers = (resolvers: Record<string, string>): void => {
   expect(resolvers['Mutation.createPost.auth.1.req.vtl']).toBeUndefined();
   expect(resolvers['Mutation.updatePost.auth.1.res.vtl']).toBeUndefined();
   expect(resolvers['Mutation.deletePost.auth.1.res.vtl']).toBeUndefined();
@@ -518,4 +568,103 @@ const expectNoResolvers = (resolvers: Record<string, string>): void => {
   expect(resolvers['Subscription.onCreatePost.auth.1.req.vtl']).toBeUndefined();
   expect(resolvers['Subscription.onUpdatePost.auth.1.req.vtl']).toBeUndefined();
   expect(resolvers['Subscription.onDeletePost.auth.1.req.vtl']).toBeUndefined();
+};
+
+const expectSandboxResolvers = (resolvers: Record<string, string>, expectIamAccessCheck: boolean, expectApiKeyCheck: boolean): void => {
+  expect(resolvers['Mutation.createPost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Mutation.createPost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Mutation.createPost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Mutation.createPost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Mutation.createPost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Mutation.createPost.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Mutation.updatePost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Mutation.updatePost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Mutation.updatePost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Mutation.updatePost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Mutation.updatePost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Mutation.updatePost.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Mutation.deletePost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Mutation.deletePost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Mutation.deletePost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Mutation.deletePost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Mutation.deletePost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Mutation.deletePost.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Query.getPost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Query.getPost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Query.getPost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Query.getPost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Query.getPost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Query.getPost.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Query.listPosts.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Query.listPosts.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Query.listPosts.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Query.listPosts.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Query.listPosts.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Query.listPosts.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Subscription.onCreatePost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Subscription.onCreatePost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Subscription.onCreatePost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Subscription.onCreatePost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Subscription.onCreatePost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Subscription.onCreatePost.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Subscription.onUpdatePost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Subscription.onUpdatePost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Subscription.onUpdatePost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Subscription.onUpdatePost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Subscription.onUpdatePost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Subscription.onUpdatePost.postAuth.1.req.vtl']).toMatchSnapshot();
+  expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).toBeDefined();
+  if (expectIamAccessCheck) {
+    expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).not.toContain(IAM_ACCESS_CHECK_POST_AUTH);
+  }
+  if (expectApiKeyCheck) {
+    expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  } else {
+    expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).not.toContain(API_KEY_ACCESS_CHECK_POST_AUTH);
+  }
+  expect(resolvers['Subscription.onDeletePost.postAuth.1.req.vtl']).toMatchSnapshot();
 };
