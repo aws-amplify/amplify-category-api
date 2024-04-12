661ceac031c13dd97fa5d865609a8fc882ff1fb6
Dane Pilcher <dppilche@amazon.com>
2024-04-12 13:22:11 -0600

    test: add test to ensure separate temp dirs

---
+++ packages/amplify-graphql-api-construct/src/__tests__/internal/asset-manager.test.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { AssetManager } from '../../internal';
+
+describe('assetManager', () => {
+  test('uses a uniqe directory for each asset manager', () => {
+    const stack = new cdk.Stack();
+    const mockConstruct1 = new Construct(stack, 'MockConstruct1');
+    const assetManager1 = new AssetManager(mockConstruct1);
+
+    const mockConstruct2 = new Construct(stack, 'MockConstruct2');
+    const assetManager2 = new AssetManager(mockConstruct2);
+
+    // disable ts to access private field
+    // @ts-ignore
+    expect(assetManager1.tempAssetDir).not.toEqual(assetManager2.tempAssetDir);
+  });
+});
