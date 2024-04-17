c5e258ca0ea4029343c91bf6b1d49f3aaedae431
Dane Pilcher <dppilche@amazon.com>
2024-04-17 11:42:58 -0600

    refactor: asset manager to asset provider in test

--- packages/amplify-graphql-api-construct/src/__tests__/internal/asset-provider.test.ts
+++ packages/amplify-graphql-api-construct/src/__tests__/internal/asset-provider.test.ts
@@ -6,13 +6,13 @@ describe('AssetProvider', () => {
   test('uses a unique directory for each asset manager', () => {
     const stack = new cdk.Stack();
     const mockConstruct1 = new Construct(stack, 'MockConstruct1');
-    const assetManager1 = new AssetProvider(mockConstruct1);
+    const assetProvider1 = new AssetProvider(mockConstruct1);
 
     const mockConstruct2 = new Construct(stack, 'MockConstruct2');
-    const assetManager2 = new AssetProvider(mockConstruct2);
+    const assetProvider2 = new AssetProvider(mockConstruct2);
 
     // disable ts to access private field
     // @ts-ignore
-    expect(assetManager1.tempAssetDir).not.toEqual(assetManager2.tempAssetDir);
+    expect(assetProvider1.tempAssetDir).not.toEqual(assetProvider2.tempAssetDir);
   });
 });
