cbb734dd01dab14adaa289be764e7c8752cd93a4
Dane Pilcher <dppilche@amazon.com>
2024-05-06 14:58:22 -0600

    ci: remove unused files

--- scripts/components/npm_client.ts
+++ scripts/components/npm_client.ts
@@ -35,7 +35,7 @@ export class NpmClient {
    *
    * By default the client operates in the process cwd
    */
-  constructor(private readonly npmToken: string | null, private readonly cwd: string = process.cwd()) {
+  constructor(private readonly cwd: string = process.cwd()) {
     this.exec = (command: string, args: string[]) => execa.sync(command, args, { cwd });
     this.execWithIO = (command: string, args: string[]) => execa.sync(command, args, { cwd, stdio: 'inherit' });
   }
@@ -70,44 +70,4 @@ export class NpmClient {
   install = async (packageSpecifiers: string[], options: { dev: boolean } = { dev: false }) => {
     await this.execWithIO('npm', ['install', `${options.dev ? '-D' : ''}`, ...packageSpecifiers]);
   };
-
-  /**
-   * Configure the .npmrc file with the provided npm token
-   * If no token is specified, the .npmrc is configured for a local npm proxy
-   */
-  configureNpmRc = async () => {
-    if (this.npmToken) {
-      await writeFile(
-        path.join(this.cwd, '.npmrc'),
-        // eslint-disable-next-line spellcheck/spell-checker
-        `//registry.npmjs.org/:_authToken=${this.npmToken}${EOL}`,
-      );
-    } else {
-      // if there's no npm token, assume we are configuring for a local proxy
-      // copy local config into .npmrc
-      await writeFile(path.join(this.cwd, '.npmrc'), npmrcLocalTemplate);
-    }
-  };
 }
-
-/**
- * Loads the github token from the GITHUB_TOKEN environment variable.
- * If not present, an error is thrown
- */
-export const loadNpmTokenFromEnvVar = () => {
-  const npmToken = process.env.NPM_TOKEN;
-  if (!npmToken) {
-    throw new Error(`
-      The NPM access token must be set in the NPM_TOKEN environment variable.
-    `);
-  }
-  return npmToken;
-};
-
-const npmrcLocalTemplate = `
-# this is a test config file used to publish locally
-//localhost:4873/:_authToken=garbage
-
-# this prevents the script-shell setting set by setup:local from being overwritten when copying the localhost config
-script-shell=bash
-`;
--- scripts/deprecate_release.ts
+++ scripts/deprecate_release.ts
@@ -1,5 +1,5 @@
 import { GitClient } from './components/git_client';
-import { NpmClient, loadNpmTokenFromEnvVar } from './components/npm_client';
+import { NpmClient } from './components/npm_client';
 import { ReleaseDeprecator } from './components/release_deprecator';
 import { DistTagMover } from './components/dist_tag_mover';
 
@@ -12,11 +12,8 @@ import { DistTagMover } from './components/dist_tag_mover';
   if (!searchForReleaseStartingFrom) {
     throw new Error('SEARCH_FOR_RELEASE_STARTING_FROM not set.');
   }
-  const useNpmRegistry = process.env.USE_NPM_REGISTRY === 'true';
 
-  const npmClient = new NpmClient(useNpmRegistry ? loadNpmTokenFromEnvVar() : null);
-
-  await npmClient.configureNpmRc();
+  const npmClient = new NpmClient();
 
   const releaseDeprecator = new ReleaseDeprecator(
     searchForReleaseStartingFrom,
