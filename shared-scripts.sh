238d7d5747e6e46b39830da8e9926d9f50684ea2
Dane Pilcher <dppilche@amazon.com>
2024-05-06 12:50:22 -0600

    ci: remove tag list

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -465,7 +465,6 @@ function _deprecate {
   echo "Authenticate with NPM"
   # PUBLISH_TOKEN=$(echo "$NPM_PUBLISH_TOKEN" | jq -r '.token')
   # echo "//registry.npmjs.org/:_authToken=$PUBLISH_TOKEN" > ~/.npmrc
-  git tag | head -100
   yarn deprecate
 }
 
