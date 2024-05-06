d820d3258bc76818012e75c0eeb47cfb39233749
Dane Pilcher <dppilche@amazon.com>
2024-05-06 12:38:05 -0600

    ci: don't pull tags

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -465,7 +465,6 @@ function _deprecate {
   echo "Authenticate with NPM"
   # PUBLISH_TOKEN=$(echo "$NPM_PUBLISH_TOKEN" | jq -r '.token')
   # echo "//registry.npmjs.org/:_authToken=$PUBLISH_TOKEN" > ~/.npmrc
-  git fetch --tags
   git tag | head -100
   yarn deprecate
 }
