7b3cf0e6fe1d19dffd97723a57deaee693ab448b
Tim Schmelter <schmelte+github@amazon.com>
2024-05-08 17:37:18 -0700

    fix: fix reference-style relationship validation (#2533)
    * fix: support multiple relationships between models
    * fix: fix recursive schema support
    * chore: add primary key utilities, tests for model-utils

--- packages/amplify-graphql-relational-transformer/src/__tests__/amplify-graphql-has-one-references-transformer.test.ts
+++ packages/amplify-graphql-relational-transformer/src/__tests__/amplify-graphql-has-one-references-transformer.test.ts
@@ -1,7 +1,7 @@
 import { PrimaryKeyTransformer } from '@aws-amplify/graphql-index-transformer';
 import { ModelTransformer } from '@aws-amplify/graphql-model-transformer';
 import { validateModelSchema } from '@aws-amplify/graphql-transformer-core';
-import { parse } from 'graphql';
+import { Kind, parse } from 'graphql';
 import { testTransform } from '@aws-amplify/graphql-transformer-test-utils';
 import { BelongsToTransformer, HasManyTransformer, HasOneTransformer } from '..';
 
@@ -95,7 +95,7 @@ test('fails if an empty list of fields is passed in', () => {
       schema: inputSchema,
       transformers: [new ModelTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
     }),
-  ).toThrowError('Invalid @hasMany directive on team - empty references list');
+  ).toThrowError('Invalid @hasOne directive on team - empty references list');
 });
 
 test('fails if any of the fields passed in are not in the related model', () => {
@@ -453,4 +453,263 @@ test('has one references with multiple sort keys', () => {
   expect(out.resolvers['Team.project.req.vtl']).toMatchSnapshot();
   expect(out.resolvers['Project.team.req.vtl']).toBeDefined();
   expect(out.resolvers['Project.team.req.vtl']).toMatchSnapshot();
+});
+
+test('has one query with implicit IDs', () => {
+  const inputSchema = `
+    type Team @model {
+      name: String!
+      members: Member @hasOne(references: ["teamID"])
+    }
+    type Member @model {
+      teamID: ID!
+      team: Team @belongsTo(references: ["teamID"])
+    }
+  `;
+
+  const out = testTransform({
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  });
+
+  expect(out).toBeDefined();
+  const schema = parse(out.schema);
+  validateModelSchema(schema);
+  expect(out.schema).toMatchSnapshot();
+  expect((out.stacks as any).ConnectionStack.Resources.TeammembersResolver).toBeTruthy();
+
+  const testObjType = schema.definitions.find((def: any) => def.name && def.name.value === 'Team') as any;
+  expect(testObjType).toBeDefined();
+
+  const relatedField = testObjType.fields.find((f: any) => f.name.value === 'members');
+  expect(relatedField).toBeDefined();
+  expect((relatedField.type as any).name.value).toEqual('Member');
+  expect(relatedField.type.kind).toEqual(Kind.NAMED_TYPE);
+  expect(relatedField.arguments.length).toEqual(0);
+
+  expect(out.resolvers['Team.members.req.vtl']).toBeDefined();
+  expect(out.resolvers['Team.members.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Member.team.req.vtl']).toBeDefined();
+  expect(out.resolvers['Member.team.req.vtl']).toMatchSnapshot();
+});
+
+test('fails if reference field has different type than primary key with implicit id and primary key', () => {
+  const inputSchema = `
+    type Team @model {
+      name: String!
+      members: Member @hasOne(references: ["teamID"])
+    }
+    type Member @model {
+      teamID: Int
+      team: Team @belongsTo(references: ["teamID"])
+    }
+  `;
+
+  const transformOptions = {
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  };
+  expect(() => testTransform(transformOptions)).toThrowError(
+    'Type mismatch between primary key field(s) of Team and reference fields of Member. Type of Team.id does not match type of Member.teamID',
+  );
+});
+
+test('fails if reference field has different type than primary key with explicit id and implicit primary key', () => {
+  const inputSchema = `
+    type Team @model {
+      id: ID!
+      name: String!
+      members: Member @hasOne(references: ["teamID"])
+    }
+    type Member @model {
+      teamID: Int
+      team: Team @belongsTo(references: ["teamID"])
+    }
+  `;
+
+  const transformOptions = {
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  };
+  expect(() => testTransform(transformOptions)).toThrowError(
+    'Type mismatch between primary key field(s) of Team and reference fields of Member. Type of Team.id does not match type of Member.teamID',
+  );
+});
+
+test('fails if reference field has different type than primary key with explicit id and explicit primary key', () => {
+  const inputSchema = `
+    type Team @model {
+      id: ID! @primaryKey
+      name: String!
+      members: Member @hasOne(references: ["teamID"])
+    }
+    type Member @model {
+      teamID: Int
+      team: Team @belongsTo(references: ["teamID"])
+    }
+  `;
+
+  const transformOptions = {
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  };
+  expect(() => testTransform(transformOptions)).toThrowError(
+    'Type mismatch between primary key field(s) of Team and reference fields of Member. Type of Team.id does not match type of Member.teamID',
+  );
+});
+
+test('fails to validate if reference lengths do not match', () => {
+  const inputSchema = /* GraphQL */ `
+    type Primary @model {
+      content: String
+      related: Related @hasOne(references: ["primaryPk"])
+    }
+
+    type Related @model {
+      content: String
+      primaryPk: ID
+      primarySk: ID
+      primary: Primary @belongsTo(references: ["primaryPk", "primarySk"])
+    }
+  `;
+
+  const transformParams = {
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  };
+
+  expect(() => testTransform(transformParams)).toThrowError(
+    'Uni-directional relationships are not supported. Add a @belongsTo field in Related to match the @hasOne field Primary.related, and ' +
+      'ensure the number and type of reference fields match the number and type of primary key fields in Primary.',
+  );
+});
+
+test('has one references with multiple relationships to the same model', () => {
+  const inputSchema = /* GraphQL */ `
+    type Primary @model {
+      content: String
+      related1: Related @hasOne(references: ["primaryId1"])
+      related2: Related @hasOne(references: ["primaryId2"])
+    }
+
+    type Related @model {
+      content: String
+      primaryId1: ID
+      primaryId2: ID
+      primary1: Primary @belongsTo(references: ["primaryId1"])
+      primary2: Primary @belongsTo(references: ["primaryId2"])
+    }
+  `;
+
+  const out = testTransform({
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  });
+
+  expect(out).toBeDefined();
+  const schema = parse(out.schema);
+  validateModelSchema(schema);
+  // The important assertions for Primary.related* are that the GSI being used to query is associated with the correct field
+  expect(out.resolvers['Primary.related1.req.vtl']).toBeDefined();
+  expect(out.resolvers['Primary.related1.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Primary.related1.req.vtl']).toContain('"index": "gsi-Primary.related1"');
+  expect(out.resolvers['Primary.related2.req.vtl']).toBeDefined();
+  expect(out.resolvers['Primary.related2.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Primary.related2.req.vtl']).toContain('"index": "gsi-Primary.related2"');
+
+  // The important assertions for Related.primary* are that the correct reference field is being used for primary lookup
+  expect(out.resolvers['Related.primary1.req.vtl']).toBeDefined();
+  expect(out.resolvers['Related.primary1.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Related.primary1.req.vtl']).toContain('connectionAttibutes.get("primaryId1")');
+  expect(out.resolvers['Related.primary2.req.vtl']).toBeDefined();
+  expect(out.resolvers['Related.primary2.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Related.primary2.req.vtl']).toContain('connectionAttibutes.get("primaryId2")');
+});
+
+test('has one references with multiple relationships to the same model with composite primary key', () => {
+  const inputSchema = /* GraphQL */ `
+    type Primary @model {
+      pk: ID! @primaryKey(sortKeyFields: ["sk1", "sk2"])
+      sk1: ID!
+      sk2: ID!
+      content: String
+      related1: Related @hasOne(references: ["primaryPk1", "primarySk11", "primarySk12"])
+      related2: Related @hasOne(references: ["primaryPk2", "primarySk21", "primarySk22"])
+    }
+
+    type Related @model {
+      content: String
+      primaryPk1: ID
+      primarySk11: ID
+      primarySk12: ID
+      primaryPk2: ID
+      primarySk21: ID
+      primarySk22: ID
+      primary1: Primary @belongsTo(references: ["primaryPk1", "primarySk11", "primarySk12"])
+      primary2: Primary @belongsTo(references: ["primaryPk2", "primarySk21", "primarySk22"])
+    }
+  `;
+
+  const out = testTransform({
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  });
+
+  expect(out).toBeDefined();
+  const schema = parse(out.schema);
+  validateModelSchema(schema);
+
+  // The important assertions for Primary.related* are that the GSI being used to query is associated with the correct field
+  expect(out.resolvers['Primary.related1.req.vtl']).toBeDefined();
+  expect(out.resolvers['Primary.related1.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Primary.related1.req.vtl']).toContain('"index": "gsi-Primary.related1"');
+
+  expect(out.resolvers['Primary.related2.req.vtl']).toBeDefined();
+  expect(out.resolvers['Primary.related2.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Primary.related2.req.vtl']).toContain('"index": "gsi-Primary.related2"');
+
+  // The important assertions for Related.primary* are that the correct reference field is being used for primary lookup
+  expect(out.resolvers['Related.primary1.req.vtl']).toBeDefined();
+  expect(out.resolvers['Related.primary1.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Related.primary1.req.vtl']).toContain('connectionAttibutes.get("primaryPk1")');
+  // eslint-disable-next-line no-template-curly-in-string
+  expect(out.resolvers['Related.primary1.req.vtl']).toContain('"${ctx.source.primarySk11}#${ctx.source.primarySk12}"');
+
+  expect(out.resolvers['Related.primary2.req.vtl']).toBeDefined();
+  expect(out.resolvers['Related.primary2.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['Related.primary2.req.vtl']).toContain('connectionAttibutes.get("primaryPk2")');
+  // eslint-disable-next-line no-template-curly-in-string
+  expect(out.resolvers['Related.primary2.req.vtl']).toContain('"${ctx.source.primarySk21}#${ctx.source.primarySk22}"');
+});
+
+test('supports recursive schemas', () => {
+  const inputSchema = /* GraphQL */ `
+    type TreeNode @model {
+      pk: ID!
+      value: String
+      parentId: ID
+      parent: TreeNode @belongsTo(references: ["parentId"])
+      child: TreeNode @hasOne(references: ["parentId"])
+    }
+  `;
+
+  const out = testTransform({
+    schema: inputSchema,
+    transformers: [new ModelTransformer(), new PrimaryKeyTransformer(), new HasOneTransformer(), new BelongsToTransformer()],
+  });
+
+  expect(out).toBeDefined();
+  const schema = parse(out.schema);
+  validateModelSchema(schema);
+
+  // The important assertion for `child` is that the GSI being used to query is associated with the `parentId` field
+  expect(out.resolvers['TreeNode.child.req.vtl']).toBeDefined();
+  expect(out.resolvers['TreeNode.child.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['TreeNode.child.req.vtl']).toContain('"index": "gsi-TreeNode.child"');
+  expect(out.resolvers['TreeNode.child.req.vtl']).toContain('"#partitionKey": "parentId"');
+
+  // The important assertion for `parent` is that the query is being performed on the `parentId` field
+  expect(out.resolvers['TreeNode.parent.req.vtl']).toBeDefined();
+  expect(out.resolvers['TreeNode.parent.req.vtl']).toMatchSnapshot();
+  expect(out.resolvers['TreeNode.parent.req.vtl']).toContain('connectionAttibutes.get("parentId")');
 });
