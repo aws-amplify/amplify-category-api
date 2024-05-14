66cb1a3e541eac99b4b534104fe5d6f5a36810ec
Dane Pilcher <dppilche@amazon.com>
2024-05-14 14:06:29 -0600

    test: add tests for isDynamicAuthOrCustomAuth util

--- packages/amplify-graphql-auth-transformer/src/__tests__/utils/role-definition.test.ts
+++ packages/amplify-graphql-auth-transformer/src/__tests__/utils/role-definition.test.ts
@@ -7,35 +7,36 @@ import {
 import { AuthStrategy, AuthProvider } from '../../utils/definitions';
 
 describe('RoleDefinition', () => {
-  describe('isIdenticalAuthRole', () => {
-    const authStrategies: AuthStrategy[] = ['owner', 'groups', 'public', 'private', 'custom'];
-    const authProviders: AuthProvider[] = ['apiKey', 'iam', 'identityPool', 'oidc', 'userPools', 'function'];
-    const staticOptions = [true, false];
-    const claims = ['claim', '', undefined];
-    const entities = ['entity', '', undefined];
+  const authStrategies: AuthStrategy[] = ['owner', 'groups', 'public', 'private', 'custom'];
+  const authProviders: AuthProvider[] = ['apiKey', 'iam', 'identityPool', 'oidc', 'userPools', 'function'];
+  const staticOptions = [true, false];
+  const claims = ['claim', '', undefined];
+  const entities = ['entity', '', undefined];
 
-    const nestedAllRoles: RoleDefinition[][][][][] = authStrategies.map((strategy): RoleDefinition[][][][] =>
-      authProviders.map((provider): RoleDefinition[][][] =>
-        staticOptions.map((staticOption): RoleDefinition[][] =>
-          claims.map((claim): RoleDefinition[] =>
-            entities.map((entity): RoleDefinition => ({ strategy, provider, static: staticOption, claim, entity })),
-          ),
+  const nestedAllRoles: RoleDefinition[][][][][] = authStrategies.map((strategy): RoleDefinition[][][][] =>
+    authProviders.map((provider): RoleDefinition[][][] =>
+      staticOptions.map((staticOption): RoleDefinition[][] =>
+        claims.map((claim): RoleDefinition[] =>
+          entities.map((entity): RoleDefinition => ({ strategy, provider, static: staticOption, claim, entity })),
         ),
       ),
-    );
-    // flat()/flatMap() not available with current build config
-    const allRoles = Array.prototype.concat(
-      ...Array.prototype.concat(...Array.prototype.concat(...Array.prototype.concat(...nestedAllRoles))),
-    );
+    ),
+  );
+  // flat()/flatMap() not available with current build config
+  const allRoles = Array.prototype.concat(
+    ...Array.prototype.concat(...Array.prototype.concat(...Array.prototype.concat(...nestedAllRoles))),
+  );
 
-    test('matching roles', () => {
-      allRoles.forEach((role) => {
+  describe('isIdenticalAuthRole', () => {
+    describe('matching roles', () => {
+      test.each(allRoles)('%j', (role) => {
         expect(isIdenticalAuthRole(role, { ...role })).toBeTruthy();
       });
     });
 
     test('non-matching roles', () => {
       const roleCombinations = [].concat(...allRoles.map((roleA, i) => allRoles.slice(i + 1).map((roleB) => [roleA, roleB])));
+      // not using test.each here because there are 145,530 combinations
       roleCombinations.forEach(([roleA, roleB]) => {
         expect(isIdenticalAuthRole(roleA, roleB)).toBeFalsy();
       });
@@ -49,8 +50,8 @@ describe('RoleDefinition', () => {
   });
 
   describe('isDynamicAuthOrCustomAuth', () => {
-    test('dynamic', () => {});
-
-    test('custom', () => {});
+    test.each(allRoles)('%j', (role) => {
+      expect({ role, result: isDynamicAuthOrCustomAuth(role) ? 'dynamic' : 'custom' }).toMatchSnapshot();
+    });
   });
 });
