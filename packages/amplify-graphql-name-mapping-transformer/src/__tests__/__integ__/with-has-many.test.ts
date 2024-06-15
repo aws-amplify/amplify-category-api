07f59163d2e47601b7a42e16b7ec632e94dfdd8e
Kevin Kamimura <kevin@yepstr.com>
2024-06-15 12:20:55 +0200

    test: 💍 add tests
    add tests to ensure array/non-scalar fields are properly set on
    relational objects

--- packages/amplify-graphql-name-mapping-transformer/src/__tests__/__integ__/with-has-many.test.ts
+++ packages/amplify-graphql-name-mapping-transformer/src/__tests__/__integ__/with-has-many.test.ts
@@ -6,16 +6,24 @@ import { ModelDataSourceStrategy } from '@aws-amplify/graphql-transformer-interf
 import { PrimaryKeyTransformer } from '@aws-amplify/graphql-index-transformer';
 import { RefersToTransformer } from '../../graphql-refers-to-transformer';
 import { MapsToTransformer } from '../../graphql-maps-to-transformer';
-import { expectedResolversForModelWithRenamedField, testRelationalFieldMapping, testTableNameMapping } from './common';
+import {
+  expectedResolversForModelWithRenamedField,
+  testRelationalArrayFieldsMapping,
+  testRelationalFieldMapping,
+  testRelationalNonScalarFieldsMapping,
+  testTableNameMapping,
+} from './common';
 
 const mappedHasMany = /* GraphQL */ `
   type Employee @model @mapsTo(name: "Person") {
     id: ID!
+    tags: [String]
     tasks: [Task] @hasMany
   }
 
   type Task @model {
     id: ID!
+    tags: [String]
     title: String
   }
 `;
@@ -77,5 +85,9 @@ describe('@refersTo with @hasMany for RDS Models', () => {
     testTableNameMapping('Task', 'Todo', out);
     testRelationalFieldMapping('Employee.tasks.req.vtl', 'Todo', out);
     testRelationalFieldMapping('Task.employee.req.vtl', 'Person', out);
+    testRelationalNonScalarFieldsMapping('Employee.tasks.req.vtl', '["tags", "employee"]', out);
+    testRelationalNonScalarFieldsMapping('Task.employee.req.vtl', '["tags", "tasks"]', out);
+    testRelationalArrayFieldsMapping('Employee.tasks.req.vtl', '["tags"]', out);
+    testRelationalArrayFieldsMapping('Task.employee.req.vtl', '["tags"]', out);
   });
 });
