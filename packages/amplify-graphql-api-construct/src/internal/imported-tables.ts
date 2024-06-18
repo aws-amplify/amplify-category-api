b74b21bfb94b951c9ec9c453fd3ba03a648e1532
Dane Pilcher <dppilche@amazon.com>
2024-06-18 14:48:57 -0600

    docs: add todo comment

--- packages/amplify-graphql-api-construct/src/internal/imported-tables.ts
+++ packages/amplify-graphql-api-construct/src/internal/imported-tables.ts
@@ -2,15 +2,16 @@ import { isDynamoDbType } from '@aws-amplify/graphql-transformer-core';
 import { ImportedAmplifyDynamoDbModelDataSourceStrategy, ModelDataSourceStrategy } from '../model-datasource-strategy-types';
 import { IAmplifyGraphqlDefinition } from '../types';
 
+// TODO: add unit testing
 export const validateImportedTableMap = (definition: IAmplifyGraphqlDefinition, importedTableMap?: Record<string, string>): void => {
   const { dataSourceStrategies } = definition;
   const importedModels = Object.keys(dataSourceStrategies).filter((modelTypeName) =>
     isImportedAmplifyDynamoDbModelDataSourceStrategy(dataSourceStrategies[modelTypeName]),
   );
+  if (importedModels.length < Object.keys(importedTableMap || {}).length) {
+    throw new Error('Table mapping includes tables not specified as imported in schema.');
+  }
   if (importedModels.length > 0) {
-    if (importedModels.length < Object.keys(importedTableMap || {}).length) {
-      throw new Error('Table mapping does not include a mapping for all models.');
-    }
     if (!importedTableMap) {
       throw new Error('Table mapping is missing for imported Amplify DynamoDB table strategy');
     }
