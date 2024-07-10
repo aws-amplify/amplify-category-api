48ea51bab0601d4ecbaa76951329162c56bf767d
Dane Pilcher <dppilche@amazon.com>
2024-07-10 15:11:26 -0600

    docs: add comment to decide naming

--- packages/amplify-graphql-model-transformer/src/graphql-model-transformer.ts
+++ packages/amplify-graphql-model-transformer/src/graphql-model-transformer.ts
@@ -343,6 +343,7 @@ export class ModelTransformer extends TransformerModelBase implements Transforme
     });
     if (context.transformParameters.enableGen2Migration && context.transformParameters.enableTransformerCfnOutputs) {
       const { scope } = context.stackManager;
+      // TODO: decide final naming before merge to main
       new cdk.CfnOutput(cdk.Stack.of(scope), 'DataSourceMappingOutput', {
         value: cdk.Stack.of(scope).toJsonString(dataSourceMapping),
         description: 'Mapping of model name to data source table name.',
