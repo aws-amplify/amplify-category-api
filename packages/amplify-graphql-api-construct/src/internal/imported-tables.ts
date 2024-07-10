40391386e8eba9b21fa1eea61187420e64059697
Dane Pilcher <dppilche@amazon.com>
2024-07-10 10:39:53 -0600

    docs: fix grammar on comment

--- packages/amplify-graphql-api-construct/src/internal/imported-tables.ts
+++ packages/amplify-graphql-api-construct/src/internal/imported-tables.ts
@@ -4,19 +4,19 @@ import { IAmplifyGraphqlDefinition } from '../types';
 
 export const validateImportedTableMap = (definition: IAmplifyGraphqlDefinition, importedTableMap?: Record<string, string>): void => {
   const { dataSourceStrategies } = definition;
-  const importedModels = Object.keys(dataSourceStrategies).filter((modelTypeName) =>
+  const importedModels = Object.keys(dataSourceStrategies).filter(modelTypeName =>
     isImportedAmplifyDynamoDbModelDataSourceStrategy(dataSourceStrategies[modelTypeName]),
   );
   const modelsInMap = Object.keys(importedTableMap || {});
   if (importedModels.length < modelsInMap.length) {
-    const missingModels = modelsInMap.filter((model) => !importedModels.includes(model));
+    const missingModels = modelsInMap.filter(model => !importedModels.includes(model));
     throw new Error(`Table mapping includes tables not specified as imported in schema. (${missingModels.join(', ')})`);
   }
   if (importedModels.length > 0) {
     if (!importedTableMap) {
       throw new Error('Table mapping is missing for imported Amplify DynamoDB table strategy.');
     }
-    const missingModels = importedModels.filter((model) => !modelsInMap.includes(model));
+    const missingModels = importedModels.filter(model => !modelsInMap.includes(model));
     if (missingModels.length > 0) {
       throw new Error(`Cannot find imported Amplify DynamoDB table mapping for models ${missingModels.join(', ')}.`);
     }
@@ -24,7 +24,7 @@ export const validateImportedTableMap = (definition: IAmplifyGraphqlDefinition, 
 };
 
 /**
- * Type predicate that returns true if `obj` is a AmplifyDynamoDbModelDataSourceStrategy
+ * Type predicate that returns true if `obj` is an AmplifyDynamoDbModelDataSourceStrategy
  */
 export const isImportedAmplifyDynamoDbModelDataSourceStrategy = (
   strategy: ModelDataSourceStrategy,
