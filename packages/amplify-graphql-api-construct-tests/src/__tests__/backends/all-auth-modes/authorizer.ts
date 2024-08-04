fa416af536b9be47dc738d22ede55869fd99602d
Doesnt Matter <not@used.com>
2024-08-04 13:56:23 -0700

    chore: remove data, api constructs

--- packages/amplify-graphql-api-construct-tests/src/__tests__/backends/all-auth-modes/authorizer.ts
+++
@@ -1,3 +0,0 @@
-export const handler = async ({ authorizationToken }: { authorizationToken: string }): Promise<{ isAuthorized: boolean }> => ({
-  isAuthorized: authorizationToken === 'letmein',
-});
--- packages/amplify-graphql-api-construct/src/internal/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './authorization-modes';
-export * from './construct-exports';
-export * from './default-parameters';
-export * from './conflict-resolution';
-export * from './asset-provider';
-export * from './codegen-assets';
-export * from './model-type-name';
