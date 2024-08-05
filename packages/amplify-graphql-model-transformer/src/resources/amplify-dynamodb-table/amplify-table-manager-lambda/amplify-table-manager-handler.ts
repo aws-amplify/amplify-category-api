df3a50b2effb88bc56525c628045c84e9d259a4c
Dane Pilcher <dppilche@amazon.com>
2024-08-05 11:22:52 -0600

    fix: adjust default table properties (#2726)

--- packages/amplify-graphql-model-transformer/src/resources/amplify-dynamodb-table/amplify-table-manager-lambda/amplify-table-manager-handler.ts
+++ packages/amplify-graphql-model-transformer/src/resources/amplify-dynamodb-table/amplify-table-manager-lambda/amplify-table-manager-handler.ts
@@ -1044,14 +1044,15 @@ export const getExpectedTableProperties = (createTableInput: CreateTableCommandI
       BillingMode: createTableInput.BillingMode,
     },
     StreamSpecification: createTableInput.StreamSpecification,
-    ProvisionedThroughput: createTableInput.ProvisionedThroughput,
-    SSEDescription: createTableInput.SSESpecification
-      ? {
-          SSEType: createTableInput.SSESpecification.SSEType || 'KMS',
-          Status: 'ENABLED',
-        }
-      : undefined,
-    DeletionProtectionEnabled: createTableInput.DeletionProtectionEnabled,
+    ProvisionedThroughput: createTableInput.ProvisionedThroughput || { ReadCapacityUnits: 0, WriteCapacityUnits: 0 },
+    SSEDescription:
+      createTableInput.SSESpecification && createTableInput.SSESpecification.Enabled
+        ? {
+            SSEType: createTableInput.SSESpecification.SSEType || 'KMS',
+            Status: 'ENABLED',
+          }
+        : undefined,
+    DeletionProtectionEnabled: createTableInput.DeletionProtectionEnabled || false,
   };
 };
 
@@ -1425,7 +1426,7 @@ const sleep = async (milliseconds: number): Promise<void> => new Promise((resolv
 const importTable = async (tableDef: CustomDDB.Input): Promise<AWSCDKAsyncCustomResource.OnEventResponse> => {
   // TODO: Add import validation
   console.log('Initiating table import process');
-  console.log(`Fetching current state of table ${tableDef.tableName}`)
+  console.log(`Fetching current state of table ${tableDef.tableName}`);
   const describeTableResult = await ddbClient.describeTable({ TableName: tableDef.tableName });
   if (!describeTableResult.Table) {
     throw new Error(`Could not find ${tableDef.tableName} to update`);
