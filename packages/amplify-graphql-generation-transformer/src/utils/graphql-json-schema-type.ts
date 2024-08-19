4c71954911be9243cffd990d87c2f84563a08b2a
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-08-19 13:08:38 -0400

    fix(generation): scalar descriptions, add AWSDateTime, properly assign required fields in jsonschema

--- packages/amplify-graphql-generation-transformer/src/utils/graphql-json-schema-type.ts
+++ packages/amplify-graphql-generation-transformer/src/utils/graphql-json-schema-type.ts
@@ -12,7 +12,7 @@ export function generateJSONSchemaFromTypeNode(
       const properties = def.fields?.reduce((acc: Record<string, JSONSchema>, field) => {
         acc[field.name.value] = generateJSONSchemaFromTypeNode(field.type, ctx, { type: '' });
         if (field.type.kind === Kind.NON_NULL_TYPE) {
-          schema.required = [...(acc[field.name.value].required || []), field.name.value];
+          schema.required = [...(schema.required || []), field.name.value];
         }
         return acc;
       }, {});
@@ -62,19 +62,76 @@ function processNamedType(namedType: NamedTypeNode, ctx: TransformerContextProvi
   // TODO: For AWSDate / AWSTime / AWSTimestamp, return a description detailing valid formats.
   switch (namedType.name.value) {
     case 'Int':
+      return {
+        type: 'number',
+        description: 'A signed 32-bit integer value.',
+      };
     case 'Float':
-      return { type: 'number' };
+      return {
+        type: 'number',
+        description: 'An IEEE 754 floating point value.',
+      };
     case 'String':
+      return {
+        type: 'string',
+        description: 'A UTF-8 character sequence.',
+      };
     case 'ID':
-      return { type: 'string' };
+      return {
+        type: 'string',
+        description: "A unique identifier for an object. This scalar is serialized like a String but isn't meant to be human-readable.",
+      };
     case 'Boolean':
       return { type: 'boolean' };
-    case 'AWSDate':
-    case 'AWSTime':
-    case 'AWSTimestamp':
-    case 'AWSEmail':
     case 'AWSJSON':
-      return { type: 'string' };
+      return {
+        type: 'string',
+        description:
+          'A JSON string. Any valid JSON construct is automatically parsed and loaded in the resolver code as maps, lists, or scalar values rather than as the literal input strings. Unquoted strings or otherwise invalid JSON result in a GraphQL validation error.',
+      };
+    case 'AWSEmail':
+      return {
+        type: 'string',
+        description: 'An email address in the format local-part@domain-part as defined by RFC 822.',
+      };
+    case 'AWSDate':
+      return {
+        type: 'string',
+        description: 'An extended ISO 8601 date string in the format YYYY-MM-DD.',
+      };
+    case 'AWSTime':
+      return {
+        type: 'string',
+        description: 'An extended ISO 8601 time string in the format hh:mm:ss.sss.',
+      };
+    case 'AWSDateTime':
+      return {
+        type: 'string',
+        description: 'An extended ISO 8601 date and time string in the format YYYY-MM-DDThh:mm:ss.sssZ.',
+      };
+    case 'AWSTimestamp':
+      return {
+        type: 'string',
+        description: 'An integer value representing the number of seconds before or after 1970-01-01-T00:00Z.',
+      };
+    case 'AWSPhone':
+      return {
+        type: 'string',
+        description:
+          'A phone number. This value is stored as a string. Phone numbers can contain either spaces or hyphens to separate digit groups. Phone numbers without a country code are assumed to be US/North American numbers adhering to the North American Numbering Plan (NANP).',
+      };
+    case 'AWSURL':
+      return {
+        type: 'string',
+        description:
+          "A URL as defined by RFC 1738. For example, https://www.amazon.com/dp/B000NZW3KC/ or mailto:example@example.com. URLs must contain a schema (http, mailto) and can't contain two forward slashes (//) in the path part.",
+      };
+    case 'AWSIPAddress':
+      return {
+        type: 'string',
+        description:
+          'A valid IPv4 or IPv6 address. IPv4 addresses are expected in quad-dotted notation (123.12.34.56). IPv6 addresses are expected in non-bracketed, colon-separated format (1a2b:3c4b::1234:4567). You can include an optional CIDR suffix (123.45.67.89/16) to indicate subnet mask.',
+      };
     default:
       // For custom object types
       return {
