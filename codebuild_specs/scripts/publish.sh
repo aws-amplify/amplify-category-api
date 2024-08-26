7d4143b16a9b736019e07dce1690d88f7a97b856
phani-srikar <phanisrikar93ume@gmail.com>
2024-08-26 12:35:54 -0700

    chore: update dist-tag in local publish

--- codebuild_specs/scripts/publish.sh
+++ codebuild_specs/scripts/publish.sh
@@ -9,8 +9,6 @@ if [ -z "$BRANCH_NAME" ]; then
   exit 1
 else
   # These are more of an extra caution to make sure we have latest commits
-  git checkout main
-  git pull origin main
   git checkout $BRANCH_NAME
   git pull origin $BRANCH_NAME
   git fetch --all
