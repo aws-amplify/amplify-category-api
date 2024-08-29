a6abe4e17fcb955a9c137efb02d8edef9bd18972
phani-srikar <phanisrikar93ume@gmail.com>
2024-08-28 21:17:48 -0700

    chore: use version from local manifest

--- codebuild_specs/scripts/post_publish.sh
+++ codebuild_specs/scripts/post_publish.sh
@@ -1,27 +1,35 @@
 #!/bin/bash -e
 
 REGISTRY="https://registry.npmjs.org"
-PACKAGE_NAME="@aws-amplify/amplify-category-api"
-TARGET_TAG="stable-tag-8"
+PACKAGE_NAME="amplify-category-api"
 NEW_TAG="test-tag-1"
 RETRIES=10
-DELAY=60 # 3 minutes delay between retries
+DELAY=60 # 1 minute delay between retries
 
-# Function to attempt to set the "NEW_TAG" tag to the version tagged with "TARGET_TAG"
+# Read package name and version from package.json
+PACKAGE_MANIFEST_PATH="packages/$PACKAGE_NAME/package.json"
+PACKAGE_NAME=$(jq -r '.name' "$PACKAGE_MANIFEST_PATH")
+PACKAGE_VERSION=$(jq -r '.version' "$PACKAGE_MANIFEST_PATH")
+
+# Validate package name and version
+if [ -z "$PACKAGE_NAME" ] || [ -z "$PACKAGE_VERSION" ]; then
+  echo "Unable to read package name or version from package.json."
+  exit 1
+fi
+echo "Using package: $PACKAGE_NAME, version: $PACKAGE_VERSION"
+
+# Sets the "NEW_TAG" tag to the specified version
 update_dist_tag() {
-  # Fetch the version tagged with "TARGET_TAG"
-  version=$(npm info $PACKAGE_NAME@$TARGET_TAG version 2>&1)
-  
-  # Check if npm returned a 404 error
-  if [ -z "$version" ] || echo "$version" | grep -q "404"; then
-    echo "404 error encountered. The version might not be available yet."
+  # Check if the version is available on npm
+  if npm view $PACKAGE_NAME@$PACKAGE_VERSION > /dev/null 2>&1; then
+    echo "Version $PACKAGE_VERSION of package $PACKAGE_NAME is available."
+    # Add the dist-tag
+    # npm dist-tag add $PACKAGE_NAME@$PACKAGE_VERSION $NEW_TAG --registry $REGISTRY
+    return $?
+  else
+    echo "Version $PACKAGE_VERSION of package $PACKAGE_NAME is not available yet."
     return 1
   fi
-  
-  echo "Found version $version tagged with $TARGET_TAG."
-  # Attempt to add the dist-tag
-  npm dist-tag add $PACKAGE_NAME@$version $NEW_TAG --registry $REGISTRY
-  return $? # Return success or failure based on the npm command
 }
 
 # Retry loop
