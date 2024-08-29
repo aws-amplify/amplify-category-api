0afa822590b85db7e3ae1b9a0bf7204aa92b89ed
phani-srikar <phanisrikar93ume@gmail.com>
2024-08-28 19:25:28 -0700

    chore: testing without re-authenticating

--- codebuild_specs/scripts/post_publish.sh
+++ codebuild_specs/scripts/post_publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 PACKAGE_NAME="@aws-amplify/amplify-category-api"
-TARGET_TAG="stable-tag-5"
+TARGET_TAG="stable-tag-7"
 NEW_TAG="test-tag-1"
 RETRIES=10
-DELAY=5
+DELAY=180 # 3 minutes delay between retries
 
 # Function to attempt to set the "NEW_TAG" tag to the version tagged with "TARGET_TAG"
 update_dist_tag() {
@@ -18,12 +18,6 @@ update_dist_tag() {
   fi
   
   echo "Found version $version tagged with $TARGET_TAG."
-  npm set registry "$REGISTRY"
-  yarn config set registry "$REGISTRY"
-  echo "Authenticate with NPM"
-  PUBLISH_TOKEN=$(echo "$NPM_PUBLISH_TOKEN" | jq -r '.token')
-  echo "//registry.npmjs.org/:_authToken=$PUBLISH_TOKEN" > ~/.npmrc
-
   # Attempt to add the dist-tag
   npm dist-tag add $PACKAGE_NAME@$version $NEW_TAG --registry $REGISTRY
   return $? # Return success or failure based on the npm command
