118f4d2dd78829ff7c56f08c0976ecbd9ea4ba76
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-08-30 10:48:04 -0400

    remove graphqlurl change that snuck in again

--- packages/amplify-graphql-transformer-interfaces/src/graphql-api-provider.ts
+++ packages/amplify-graphql-transformer-interfaces/src/graphql-api-provider.ts
@@ -128,7 +128,6 @@ export type MappingTemplateProvider = InlineMappingTemplateProvider | S3MappingT
 
 export interface GraphQLAPIProvider extends IConstruct {
   readonly apiId: string;
-  readonly graphqlUrl: string;
   readonly host: TransformHostProvider;
   readonly name: string;
   readonly assetProvider: AssetProvider;
