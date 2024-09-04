be2b144eda763d96dc25c04ae6d2e28927b0a889
Bobby Yu <bobbyu@amazon.com>
2024-09-04 16:43:53 -0700

    update: bump node to use 18.20.4

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -140,8 +140,8 @@ function _verifyAmplifyBackendCompatability {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install 18.16.0
-  nvm use 18.16.0
+  nvm install 18.20.4
+  nvm use 18.20.4
 
   # Echo to verify the Node.js version
   echo "Node.js version in use:"
