515fbc41bdc18cd318327745dffc28a59a963b0d
Bobby Yu <bobbyu@amazon.com>
2024-09-06 15:05:50 -0700

    bump: node version to 20.12.0

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -140,8 +140,8 @@ function _verifyAmplifyBackendCompatability {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install 18.20.4
-  nvm use 18.20.4
+  nvm install 20.12.0
+  nvm use 20.12.0
 
   # Echo to verify the Node.js version
   echo "Node.js version in use:"
