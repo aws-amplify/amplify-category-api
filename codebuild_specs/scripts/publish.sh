77b4f63bd51216dac129fdda68b5d45461a64aa6
phani-srikar <phanisrikar93ume@gmail.com>
2024-09-10 15:25:35 -0700

    chore: update yarn lock

--- codebuild_specs/scripts/publish.sh
+++ codebuild_specs/scripts/publish.sh
@@ -32,8 +32,8 @@ else
 fi
 
 if [ "$RUN_POST_PUBLISH_ONLY" = "true" ]; then
-  # yarn postpublish:$BRANCH_NAME
-  echo "Post publish script is called"
+  echo "Post publish script is invoked on branch $BRANCH_NAME"
+  yarn postpublish:$BRANCH_NAME
   exit 0
 fi
 
@@ -59,5 +59,5 @@ if [[ "$BRANCH_NAME" =~ ^tagged-release ]]; then
     yarn publish:tag
   fi
 else
-  # yarn publish:$BRANCH_NAME
+  yarn publish:$BRANCH_NAME
 fi
