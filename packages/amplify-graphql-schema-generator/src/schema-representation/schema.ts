616126468c8aa0986758c650adc8545500231cb9
Tejas Khanolkar <tejas.khanolkar@somaiya.edu>
2024-09-13 15:16:31 -0700

    added naming in npascal case

--- packages/amplify-graphql-schema-generator/src/schema-representation/schema.ts
+++ packages/amplify-graphql-schema-generator/src/schema-representation/schema.ts
@@ -1,8 +1,9 @@
-import { Model } from './types';
+import { Model,EnumType } from './types';
 import { Engine } from './engine';
 
 export class Schema {
   private models: Model[] = [];
+  private enums: EnumType[] = [];
 
   constructor(private engine: Engine) {}
 
@@ -14,13 +15,30 @@ export class Schema {
     return this.engine;
   }
 
+  public getEnums(): EnumType[] {
+    return this.enums;
+  }
+
   public addModel(model: Model): void {
+    console.log(this.engine);
     const modelName = model.getName();
     if (this.hasModel(modelName)) {
       throw new Error(`Model "${modelName}" already exists`);
     }
+    // const fields = model.fields
     this.models.push(model);
   }
+
+  // public addEnums(e: EnumType): void {
+  //   if(this.engine.type === 'Postgres') {
+  //     const models = this.models;
+  //     this.enums
+
+  //   }
+  //   else{
+
+  //   }
+  // }
 
   public hasModel(name: string): boolean {
     return this.models.some((model) => model.getName() === name);
