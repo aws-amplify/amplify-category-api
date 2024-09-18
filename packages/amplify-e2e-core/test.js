4918878d7620b21af9ae1838a44d6c9fd91400ce
Bobby Yu <bobbyu@amazon.com>
2024-09-18 16:29:52 -0700

    add: log all credential providers

--- packages/amplify-e2e-core/test.js
+++ packages/amplify-e2e-core/test.js
@@ -1,5 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// disable eslint rule for this file
+/* eslint-disable */
 const { fromNodeProviderChain } = require('@aws-sdk/credential-providers');
+const fs = require('fs');
+const path = require('path');
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const loadCredentials = async () => {
@@ -13,6 +16,27 @@ const loadCredentials = async () => {
   } catch (e) {
     console.error(e);
   }
+
+  // log env var AWS_ACCESS_KEY_ID
+  console.log('AWS_ACCESS_KEY_ID env var:', process.env.AWS_ACCESS_KEY_ID);
+  // log env var AWS_SECRET_ACCESS_KEY
+  console.log('AWS_SECRET_ACCESS_KEY env var:', process.env.AWS_SECRET_ACCESS_KEY);
+
+  // log contents in ~/.aws/credentials
+  const credentialsPath = path.join(process.env.HOME, '.aws', 'credentials');
+  console.log('Contents in ~/.aws/credentials:');
+  console.log(fs.readFileSync(credentialsPath, 'utf8'));
+  // log contents in ~/.aws/config
+  const configPath = path.join(process.env.HOME, '.aws', 'config');
+  console.log('Contents in ~/.aws/config:');
+  console.log(fs.readFileSync(configPath, 'utf8'));
+
+  // log env var AWS_WEB_IDENTITY_TOKEN_FILE
+  console.log('AWS_WEB_IDENTITY_TOKEN_FILE env var:', process.env.AWS_WEB_IDENTITY_TOKEN_FILE);
+  // log env var AWS_ROLE_ARN
+  console.log('AWS_ROLE_ARN env var:', process.env.AWS_ROLE_ARN);
+  // log env var AWS_ROLE_SESSION_NAME
+  console.log('AWS_ROLE_SESSION_NAME env var:', process.env.AWS_ROLE_SESSION_NAME);
 };
 
 void loadCredentials();
