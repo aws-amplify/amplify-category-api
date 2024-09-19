ff99709b6e12d5ae35f57f4b4f368414852acc4e
Bobby Yu <bobbyu@amazon.com>
2024-09-19 11:25:50 -0700

    add: unset some more env var

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -136,6 +136,11 @@ function _verifyAmplifyBackendCompatability {
   echo "Verify Amplify Backend Compatibility"
   loadCacheFromBuildJob
 
+  unset AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
+  unset ECS_AGENT_URI
+  unset ECS_CONTAINER_METADATA_URI_V4
+  unset ECS_CONTAINER_METADATA_URI
+
   echo "Running fromNodeProviderChain()"
   yarn node packages/amplify-e2e-core/test.js
 
