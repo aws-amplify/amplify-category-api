66bc61eef827e8fe9fa09e80cfbe656dd68b08dd
Vieltojarvi <lvielto@amazon.com>
2024-09-24 15:18:02 -0700

    removed redundant test

--- packages/amplify-graphql-api-construct/src/__tests__/__functional__/stack.test.ts
+++ packages/amplify-graphql-api-construct/src/__tests__/__functional__/stack.test.ts
@@ -3,23 +3,9 @@ import { AmplifyGraphqlApi } from '../../amplify-graphql-api';
 import { AmplifyGraphqlDefinition } from '../../amplify-graphql-definition';
 
 describe('stack parameter access and verification', () => {
-  it('allows access to stack parameter', () => {
-    const api = new AmplifyGraphqlApi(new cdk.Stack(), 'TestApi', {
-      definition: AmplifyGraphqlDefinition.fromString(/* GraphQL */ `
-        type Todo @model @auth(rules: [{ allow: public }]) {
-          description: String!
-        }
-      `),
-      authorizationModes: {
-        apiKeyConfig: { expires: cdk.Duration.days(7) },
-      },
-    });
-
-    expect(api.stack).toBeDefined();
-  });
-
-  it('verifies the stack property is set to the expected value', () => {
-    const api = new AmplifyGraphqlApi(new cdk.Stack(), 'TestApi', {
+  it('verifies the stack property is accessible and set to the expected value', () => {
+    const stack = new cdk.Stack();
+    const api = new AmplifyGraphqlApi(stack, 'TestApi', {
         definition: AmplifyGraphqlDefinition.fromString(/* GraphQL */ `
             type Todo @model @auth(rules: [{ allow: public }]) {
             description: String!
@@ -30,6 +16,6 @@ describe('stack parameter access and verification', () => {
         },
     });
 
-    expect(api.stack).toBe(cdk.Stack.of(api));
+    expect(api.stack).toBe(stack);
   });
 });
