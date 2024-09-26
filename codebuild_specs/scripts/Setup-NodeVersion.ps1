9e1683b7862214dec002f025eec4c7e25d64b083
Bobby Yu <bobbyu@amazon.com>
2024-09-26 14:54:12 -0700

    add: nvm install and use 18.20.4

--- codebuild_specs/scripts/Setup-NodeVersion.ps1
+++ codebuild_specs/scripts/Setup-NodeVersion.ps1
@@ -6,3 +6,9 @@ Write-Host "Starting Node.js setup with version $version`n"
 
 Write-Host "Installing NVM"
 choco install nvm -y
+
+nvm install 18.20.4
+nvm use 18.20.4
+
+Write-Host "Verifying Node Version"
+node -v
