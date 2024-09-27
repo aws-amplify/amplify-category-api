d75f541e9e0d38d7012352391eb2623fb8dc9e2a
Bobby Yu <bobbyu@amazon.com>
2024-09-26 17:36:21 -0700

    update: print env var

--- codebuild_specs/scripts/Setup-NodeVersion.ps1
+++ codebuild_specs/scripts/Setup-NodeVersion.ps1
@@ -2,38 +2,38 @@ param (
   [string]$version
 )
 
-# Function to ensure the script runs with Administrator privileges
-function Ensure-Administrator {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-  if (-not $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Warning "Administrator privileges are required. Attempting to restart as Administrator..."
+# # Function to ensure the script runs with Administrator privileges
+# function Ensure-Administrator {
+#   $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+#   if (-not $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+#     Write-Warning "Administrator privileges are required. Attempting to restart as Administrator..."
 
-    # Retrieve the script path dynamically
-    $scriptPath = $PSCommandPath
+#     # Retrieve the script path dynamically
+#     $scriptPath = $PSCommandPath
 
-    if (-not (Test-Path $scriptPath)) {
-      Write-Error "Script path not found: $scriptPath"
-      exit 1
-    }
+#     if (-not (Test-Path $scriptPath)) {
+#       Write-Error "Script path not found: $scriptPath"
+#       exit 1
+#     }
 
-    # Create a new process with elevated privileges
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`" -version `"$version`"" -Verb RunAs
+#     # Create a new process with elevated privileges
+#     Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`" -version `"$version`"" -Verb RunAs
 
-    # Exit the current non-elevated script
-    exit
-  }
-}
+#     # Exit the current non-elevated script
+#     exit
+#   }
+# }
 
-# Call the self-elevation function
-Write-Host "Elevating script to Administrator privileges"
-Ensure-Administrator
+# # Call the self-elevation function
+# Write-Host "Elevating script to Administrator privileges"
+# Ensure-Administrator
 
-Write-Host "Starting Node.js setup with version $version`n" -ForegroundColor Cyan
+Write-Host "Starting Node.js setup with version $version`n"
 
 Write-Host "Installing NVM"
 choco install nvm -y
 
-Write-Host "Setting Environment Variables" -ForegroundColor Yellow
+Write-Host "Setting Environment Variables"
 setx NVM_HOME "C:\ProgramData\nvm" /M
 setx NVM_SYMLINK "C:\Program Files\nodejs" /M
 
@@ -42,9 +42,18 @@ $env:NVM_HOME = 'C:\ProgramData\nvm'
 $env:NVM_SYMLINK = 'C:\Program Files\nodejs'
 $env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "Machine") + ";$env:NVM_HOME;$env:NVM_SYMLINK"
 
-Write-Host "Installing Node.js version $version" -ForegroundColor Yellow
+Write-Host "PATH: "
+$env:PATH
+
+Write-Host "NVM_HOME: "
+Get-ChildItem Env:NVM_HOME
+
+Write-Host "NVM_SYMLINK: "
+Get-ChildItem Env:NVM_SYMLINK
+
+Write-Host "Installing Node.js version $version"
 nvm install $version
 nvm use $version
 
-Write-Host "Verifying Node Version" -ForegroundColor Cyan
+Write-Host "Verifying Node Version"
 node -v
