741df0978e53c9964adfac49944def35ae58abc1
Bobby Yu <bobbyu@amazon.com>
2024-09-27 11:37:50 -0700

    test: which way set env var

--- codebuild_specs/scripts/Install-NVM.ps1
+++ codebuild_specs/scripts/Install-NVM.ps1
@@ -1,10 +1,28 @@
 Write-Host "Installing NVM"
 choco install nvm -y
 
-# Permanently set environment variables. Should be available in the new session.
 Write-Host "Setting Environment Variables"
 setx NVM_HOME "C:\ProgramData\nvm" /M
 setx NVM_SYMLINK "C:\Program Files\nodejs" /M
+setx PATH "%PATH%;%NVM_HOME%;%NVM_SYMLINK%" /M
+
+if (-not (Test-Path $env:NVM_HOME)) {
+  Write-Host "NVM not found at $env:NVM_HOME"
+  exit 1
+}
+
+Write-Host "NVM found at $env:NVM_HOME"
+
+
+
+$env:Path = "$env:Path;$env:NVM_HOME;$env:NVM_SYMLINK"
+
+if (-not (Test-Path $env:NVM_HOME)) {
+  Write-Host "NVM not found at $env:NVM_HOME"
+  exit 1
+}
+
+Write-Host "NVM found at $env:NVM_HOME"
 
 # Temporarily set environment variables. Should be available in the current session. 
 # Write-Host "Adding NVM to PATH"
