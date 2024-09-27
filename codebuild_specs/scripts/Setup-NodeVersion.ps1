2e165576d592eb1239d13f6d1b73b2bb2f211c81
Bobby Yu <bobbyu@amazon.com>
2024-09-27 12:53:34 -0700

    update: build_windows working, running all tests

--- codebuild_specs/scripts/Setup-NodeVersion.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Verifying NVM Installation"
-
-if (-not (Test-Path $env:NVM_HOME)) {
-  Write-Host "NVM not found at $env:NVM_HOME"
-  exit 1
-}
-
-Write-Host "NVM found at $env:NVM_HOME"
-
