c869db688f89827b5c18a61fb646e081ff3200cb
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-10-03 14:41:37 -0400

    revert tool description prompt change, not helpful

--- packages/amplify-graphql-generation-transformer/src/utils/tools.ts
+++ packages/amplify-graphql-generation-transformer/src/utils/tools.ts
@@ -57,8 +57,7 @@ export const createResponseTypeTool = (field: FieldDefinitionNode, ctx: Transfor
     required: ['value'],
   };
 
-  const description =
-    'Generate a response type for the given field. Use well-structured JSON. JSON.parse(value) will be used to parse the response.';
+  const description = 'Generate a response type for the given field.';
   const tools: Tool[] = [
     {
       toolSpec: {
