8536347e901f05a0cfaaca2bfccc7f3f11d15012
Kevin Shan <siqishan@amazon.com>
2024-10-04 18:15:30 -0700

    chore: revert back construct changes to another PR
    Signed-off-by: Kevin Shan <siqishan@amazon.com>

--- packages/amplify-graphql-api-construct/src/internal/authorization-modes.ts
+++ packages/amplify-graphql-api-construct/src/internal/authorization-modes.ts
@@ -91,8 +91,8 @@ const convertAuthModeToAuthProvider = (authMode: AuthorizationConfigMode): AppSy
           name: authMode.oidcProviderName,
           issuerUrl: authMode.oidcIssuerUrl,
           clientId: authMode.clientId,
-          iatTTL: authMode.tokenExpiryFromIssue.toMilliseconds(),
-          authTTL: authMode.tokenExpiryFromAuth.toMilliseconds(),
+          iatTTL: authMode.tokenExpiryFromIssue.toSeconds(),
+          authTTL: authMode.tokenExpiryFromAuth.toSeconds(),
         },
       };
     case 'AWS_LAMBDA':
