713e2b6d52fd17ba29968934e416604564a0132f
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-10-07 18:22:28 -0400

    fix(conversation): allow changes to systemPrompt, inferenceConfig, aiModel to be hotswapped (#2923)

--- packages/amplify-graphql-conversation-transformer/src/resolvers/init-resolver.ts
+++ packages/amplify-graphql-conversation-transformer/src/resolvers/init-resolver.ts
@@ -1,8 +1,7 @@
 import { MappingTemplate } from '@aws-amplify/graphql-transformer-core';
-import { MappingTemplateProvider } from '@aws-amplify/graphql-transformer-interfaces';
+import { MappingTemplateProvider, TransformerContextProvider } from '@aws-amplify/graphql-transformer-interfaces';
 import fs from 'fs';
 import path from 'path';
-import { ConversationDirectiveConfiguration } from '../grapqhl-conversation-transformer';
 
 /**
  * Creates and returns the mapping template for the init resolver.
@@ -10,8 +9,16 @@ import { ConversationDirectiveConfiguration } from '../grapqhl-conversation-tran
  *
  * @returns {MappingTemplateProvider} An object containing request and response MappingTemplateProviders.
  */
-export const initMappingTemplate = (config: ConversationDirectiveConfiguration): MappingTemplateProvider => {
-  const resolver = fs.readFileSync(path.join(__dirname, 'init-resolver-fn.template.js'), 'utf8');
-  const templateName = `Mutation.${config.field.name.value}.init.js`;
-  return MappingTemplate.s3MappingFunctionCodeFromString(resolver, templateName);
+export const initMappingTemplate = (ctx: TransformerContextProvider): MappingTemplateProvider => {
+  const substitutions = {
+    GRAPHQL_API_ENDPOINT: ctx.api.graphqlUrl,
+  };
+
+  let resolver = fs.readFileSync(path.join(__dirname, 'init-resolver-fn.template.js'), 'utf8');
+  Object.entries(substitutions).forEach(([key, value]) => {
+    const replaced = resolver.replace(new RegExp(`\\[\\[${key}\\]\\]`, 'g'), value);
+    resolver = replaced;
+  });
+
+  return MappingTemplate.inlineTemplateFromString(resolver);
 };
