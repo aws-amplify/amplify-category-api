d30adf88e247cd334b1da9b460eff1e41c6bb6f5
Kevin Shan <siqishan@amazon.com>
2024-10-09 13:36:59 -0700

    refactor: add pg auto increment test from main
    Signed-off-by: Kevin Shan <siqishan@amazon.com>

---
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/sql-pg-auto-increment.test.ts
@@ -0,0 +1,51 @@
+import generator from 'generate-password';
+import { getResourceNamesForStrategyName, ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
+import { SqlDatatabaseController } from '../sql-datatabase-controller';
+import { TestOptions } from '../utils/sql-test-config-helper';
+import { DURATION_1_HOUR } from '../utils/duration-constants';
+import { testGraphQLAPIAutoIncrement } from '../sql-tests-common/sql-auto-increment';
+import { sqlCreateStatements } from '../sql-tests-common/tests-sources/sql-auto-increment/provider';
+
+jest.setTimeout(DURATION_1_HOUR);
+
+describe('CDK GraphQL Transformer deployments with Postgres SQL datasources', () => {
+  const projFolderName = 'pgmodels';
+
+  // sufficient password length that meets the requirements for RDS cluster/instance
+  const [username, password, identifier] = generator.generateMultiple(3, { length: 11 });
+  const region = process.env.CLI_REGION ?? 'us-west-2';
+  const engine = 'postgres';
+
+  const databaseController: SqlDatatabaseController = new SqlDatatabaseController(sqlCreateStatements(ImportedRDSType.POSTGRESQL), {
+    identifier,
+    engine,
+    username,
+    password,
+    region,
+  });
+
+  const strategyName = `${engine}DBStrategy`;
+  const resourceNames = getResourceNamesForStrategyName(strategyName);
+
+  beforeAll(async () => {
+    await databaseController.setupDatabase();
+  });
+
+  afterAll(async () => {
+    await databaseController.cleanupDatabase();
+  });
+
+  const constructTestOptions = (connectionConfigName: string): TestOptions => ({
+    projFolderName,
+    region,
+    connectionConfigName,
+    dbController: databaseController,
+    resourceNames,
+  });
+
+  testGraphQLAPIAutoIncrement(
+    constructTestOptions('connectionUri'),
+    'creates a GraphQL API from SQL-based models using Connection String SSM parameter',
+    ImportedRDSType.POSTGRESQL,
+  );
+});
---
+++ packages/amplify-graphql-api-construct-tests/src/sql-tests-common/tests-sources/sql-auto-increment/stack-config.ts
@@ -0,0 +1,13 @@
+import { AUTH_TYPE } from 'aws-appsync';
+import { schema as generateSchema } from './provider';
+import { StackConfig } from '../../../utils/sql-stack-config';
+
+export const stackConfig = (): StackConfig => {
+  const stackConfig: StackConfig = {
+    schema: generateSchema(),
+    authMode: AUTH_TYPE.API_KEY,
+    useSandbox: true,
+  };
+
+  return stackConfig;
+};
