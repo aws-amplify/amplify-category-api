afbb2360e77e1f26c863a7715676302412f2a33c
Dane Pilcher <dppilche@amazon.com>
2024-10-15 09:40:14 -0600

    feat: create basic aurora cluster

--- packages/amplify-database-construct/lib/amplify-database.d.ts
+++ packages/amplify-database-construct/lib/amplify-database.d.ts
@@ -1,7 +1,11 @@
 import { Construct } from 'constructs';
 import { Stack } from 'aws-cdk-lib';
-import { AmplifyDatabaseProps } from './types';
+import { AmplifyDatabaseProps, AmplifyDatabaseResources } from './types';
 export declare class AmplifyDatabase extends Construct {
+    /**
+     * Generated L1 and L2 CDK resources.
+     */
+    readonly resources: AmplifyDatabaseResources;
     /**
      * Reference to parent stack of database construct
      */
--- packages/amplify-database-construct/lib/types.d.ts
+++ packages/amplify-database-construct/lib/types.d.ts
@@ -1,6 +1,15 @@
+import { IVpc } from 'aws-cdk-lib/aws-ec2';
+import { IDatabaseCluster } from 'aws-cdk-lib/aws-rds';
 /**
  * Input props for the AmplifyDatabase construct.
  */
 export interface AmplifyDatabaseProps {
     readonly definition: string;
+    readonly vpc: IVpc;
+}
+export interface AmplifyDatabaseResources {
+    /**
+     * The database cluster created by the construct.
+     */
+    readonly databaseCluster: IDatabaseCluster;
 }
