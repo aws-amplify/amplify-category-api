fba25d731902a3dbfe7f62676b33d79bc347affa
Tim Schmelter <schmelte@amazon.com>
2024-10-21 08:54:44 -0700

    chore: add error handling to dependency licenses script

--- scripts/extract-dependency-licenses.sh
+++ scripts/extract-dependency-licenses.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+TEMP_FILE=dependency_licenses_temp.txt
+DEST_FILE=dependency_licenses.txt
+
+set -e
+
 yarn licenses generate-disclaimer \
     --ignore-platform \
     --ignore-engines \
     --ignore-optional \
     --silent \
     --no-progress \
-    --frozen-lockfile > dependency_licenses_temp.txt
+    --frozen-lockfile > $TEMP_FILE
+
+if [[ ! -s $TEMP_FILE ]] ; then
+    echo "Error processing dependencies" >&2
+    exit 1
+fi
 
 sed \
     -e '/WORKSPACE AGGREGATOR/d' \
     -e '/workspace-aggregator/d' \
-    dependency_licenses_temp.txt > dependency_licenses.txt
+    $TEMP_FILE > $DEST_FILE
 
-rm dependency_licenses_temp.txt
+rm $TEMP_FILE
