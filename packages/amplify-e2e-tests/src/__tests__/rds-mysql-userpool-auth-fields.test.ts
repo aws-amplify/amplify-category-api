fc374539329f5dbf00e43d923e44a8b2d0bc84f5
Tim Schmelter <schmelte@amazon.com>
2024-12-12 06:17:22 -0800

    test: Remove RDS E2E tests

--- packages/amplify-e2e-tests/src/__tests__/rds-mysql-userpool-auth-fields.test.ts
+++
@@ -1,9 +0,0 @@
-import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testUserPoolFieldAuth } from '../rds-v2-tests-common/rds-auth-userpool-fields';
-
-// to deal with bug in cognito-identity-js
-(global as any).fetch = require('node-fetch');
-
-describe('SQL MySQL UserPool field Auth', () => {
-  testUserPoolFieldAuth(ImportedRDSType.MYSQL);
-});
--- packages/amplify-e2e-tests/src/__tests__/rds-pg-userpool-auth-fields.test.ts
+++
@@ -1,9 +0,0 @@
-import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testUserPoolFieldAuth } from '../rds-v2-tests-common/rds-auth-userpool-fields';
-
-// to deal with bug in cognito-identity-js
-(global as any).fetch = require('node-fetch');
-
-describe('SQL Postgres UserPool field Auth', () => {
-  testUserPoolFieldAuth(ImportedRDSType.POSTGRESQL);
-});
