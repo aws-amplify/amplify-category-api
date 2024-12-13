8f57b789570a82bc497977bab43f5e21ef134f9d
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-12-13 14:11:38 -0500

    test: update snapshots

--- packages/amplify-graphql-generation-transformer/src/resolvers/invoke-bedrock.ts
+++ packages/amplify-graphql-generation-transformer/src/resolvers/invoke-bedrock.ts
@@ -20,7 +20,7 @@ export const createInvokeBedrockResolverFunction = (config: GenerationConfigurat
   const TOOL_CONFIG = JSON.stringify(toolConfig);
   const SYSTEM_PROMPT = JSON.stringify(config.systemPrompt);
   const INFERENCE_CONFIG = getInferenceConfigResolverDefinition(inferenceConfiguration);
-  const NON_STRING_RESPONSE_TYPE = (!stringTypedScalarTypes.includes(getBaseType(config.field.type))).toString()
+  const NON_STRING_RESPONSE_TYPE = (!stringTypedScalarTypes.includes(getBaseType(config.field.type))).toString();
   const PACKAGE_METADATA = `'${packageName}#${packageVersion}'`;
 
   const resolver = generateResolver('invoke-bedrock-resolver-fn.template.js', {
