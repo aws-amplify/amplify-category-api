49cc9af3f1e79a6ba99b69939841663422ea5cc1
Tim Schmelter <schmelte@amazon.com>
2025-01-13 08:25:49 -0800

    test: remove SQL OIDC auth tests that are in Gen 2 branch

--- packages/amplify-e2e-tests/src/__tests__/rds-mysql-oidc-auth-fields.test.ts
+++
@@ -1,9 +0,0 @@
-import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testOIDCFieldAuth } from '../rds-v2-tests-common/rds-auth-oidc-fields';
-
-// to deal with bug in cognito-identity-js
-(global as any).fetch = require('node-fetch');
-
-describe('SQL MySQL OIDC field Auth', () => {
-  testOIDCFieldAuth(ImportedRDSType.MYSQL);
-});
