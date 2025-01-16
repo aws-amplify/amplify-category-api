dcb0277bb38383f01c8bfb6fb297a6e57dd28a0c
Tim Schmelter <schmelte@amazon.com>
2025-01-16 09:23:46 -0800

    test: remove RDS tests that are running in Gen 2 branch

--- packages/amplify-e2e-tests/src/__tests__/rds-pg-custom-claims-refersto-auth.test.ts
+++
@@ -1,9 +0,0 @@
-import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testCustomClaimsRefersTo } from '../rds-v2-tests-common/rds-auth-custom-claims-refersto-auth';
-
-// to deal with bug in cognito-identity-js
-(global as any).fetch = require('node-fetch');
-
-describe('RDS Postgres Custom Claims Auth With RefersTo', () => {
-  testCustomClaimsRefersTo(ImportedRDSType.POSTGRESQL);
-});
