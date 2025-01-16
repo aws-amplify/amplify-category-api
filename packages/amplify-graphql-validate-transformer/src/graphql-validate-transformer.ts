69e9da441779cdb5e8ef97f5a4668c00b62d8508
Bobby Yu <bobbyu@amazon.com>
2025-01-16 15:17:01 -0800

    add a filler code for generateResolvers

--- packages/amplify-graphql-validate-transformer/src/graphql-validate-transformer.ts
+++ packages/amplify-graphql-validate-transformer/src/graphql-validate-transformer.ts
@@ -1,8 +1,13 @@
 import { ValidateDirective } from '@aws-amplify/graphql-directives';
 import { TransformerPluginBase } from '@aws-amplify/graphql-transformer-core';
+import { TransformerContextProvider } from '@aws-amplify/graphql-transformer-interfaces';
 
 export class ValidateTransformer extends TransformerPluginBase {
   constructor() {
     super('amplify-graphql-validate-transformer', ValidateDirective.definition);
   }
+
+  generateResolvers = (ctx: TransformerContextProvider): void => {
+    console.log('generateResolvers', ctx);
+  };
 }
