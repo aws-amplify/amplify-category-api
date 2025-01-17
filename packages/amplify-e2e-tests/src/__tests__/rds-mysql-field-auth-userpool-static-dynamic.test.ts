aec00e1021fac262335e53674344c2e24b970a9a
Tim Schmelter <schmelte+github@amazon.com>
2025-01-17 10:40:13 -0800

    test: remove RDS tests that are running in Gen 2 branch (#3119)

--- packages/amplify-e2e-tests/src/__tests__/rds-mysql-field-auth-userpool-static-dynamic.test.ts
+++
@@ -1,9 +0,0 @@
-import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testRdsUserpoolStaticAndDynamicFieldAuth } from '../rds-v2-tests-common/rds-auth-userpool-static-dynamic-fields';
-
-// to deal with bug in cognito-identity-js
-(global as any).fetch = require('node-fetch');
-
-describe('RDS MySQL userpool static & dynamic field auth rules', () => {
-  testRdsUserpoolStaticAndDynamicFieldAuth(ImportedRDSType.MYSQL, []);
-});
--- packages/amplify-e2e-tests/src/__tests__/rds-pg-userpool-auth.test.ts
+++
@@ -1,9 +0,0 @@
-import { ImportedRDSType } from '@aws-amplify/graphql-transformer-core';
-import { testUserPoolAuth } from '../rds-v2-tests-common/rds-auth-userpool';
-
-// to deal with bug in cognito-identity-js
-(global as any).fetch = require('node-fetch');
-
-describe('RDS Postgres UserPool Auth', () => {
-  testUserPoolAuth(ImportedRDSType.POSTGRESQL);
-});
