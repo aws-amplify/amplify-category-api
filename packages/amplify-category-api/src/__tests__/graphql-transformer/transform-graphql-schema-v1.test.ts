da3d73e53673a231b1045f5c252e7176c6bdae44
Dane Pilcher <dppilche@amazon.com>
2025-02-12 15:48:57 -0700

    test: test inverse

--- packages/amplify-category-api/src/__tests__/graphql-transformer/transform-graphql-schema-v1.test.ts
+++ packages/amplify-category-api/src/__tests__/graphql-transformer/transform-graphql-schema-v1.test.ts
@@ -1,11 +1,14 @@
-import { $TSContext, FeatureFlags } from '@aws-amplify/amplify-cli-core';
+import { $TSContext, FeatureFlags, success } from '@aws-amplify/amplify-cli-core';
+import { printer } from '@aws-amplify/amplify-prompts';
 import { readProjectConfiguration, collectDirectivesByTypeNames, readTransformerConfiguration } from 'graphql-transformer-core';
 import { transformGraphQLSchemaV1 } from '../../graphql-transformer/transform-graphql-schema-v1';
 
 jest.mock('@aws-amplify/amplify-cli-core');
+jest.mock('@aws-amplify/amplify-prompts');
 jest.mock('graphql-transformer-core');
 
 describe('transformGraphQLSchemaV1', () => {
+  const printerMock = printer as jest.Mocked<typeof printer>;
   const readProjectConfigurationMock = readProjectConfiguration as jest.Mock;
   const collectDirectivesByTypeNamesMock = collectDirectivesByTypeNames as jest.Mock;
   const readTransformerConfigurationMock = readTransformerConfiguration as jest.Mock;
@@ -49,9 +52,16 @@ describe('transformGraphQLSchemaV1', () => {
       parameters: {
         options: { resourcesDir: 'resourceDir', projectDirectory: __dirname },
       },
+      print: {
+        success: jest.fn(),
+      },
     } as unknown as $TSContext;
-    expect(() => transformGraphQLSchemaV1(contextMock, { dryRun: true, parameters: {} })).rejects.toThrowError(
+    await expect(() => transformGraphQLSchemaV1(contextMock, { dryRun: true, parameters: {} })).rejects.toThrowError(
       'V1 transformer is not supported for Amplify Gen 2 migrations. Migrate to V2 transformer first.',
     );
+
+    // v1 transformer works when not enabling gen 2 migrations
+    FeatureFlagsMock.getBoolean.mockReturnValue(false);
+    await transformGraphQLSchemaV1(contextMock, { dryRun: true, parameters: {} });
   });
 });
