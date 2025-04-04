0bb44f4a8a530d2d9582597c69e9143c5c9ec5fb
Dane Pilcher <dppilche@amazon.com>
2025-04-04 10:10:26 -0600

    feat: upgrade lambda runtimes to node 20

--- packages/amplify-graphql-name-mapping-transformer/src/field-mapping-lambda.ts
+++ packages/amplify-graphql-name-mapping-transformer/src/field-mapping-lambda.ts
@@ -33,7 +33,7 @@ export const createMappingLambda = (host: TransformHostProvider, stackManager: S
     `functions/${funcLogicalId}.zip`, // function key
     'index.handler', // function handler
     path.join(__dirname, 'assets', 'mapping-lambda.zip'),
-    lambda.Runtime.NODEJS_18_X,
+    lambda.Runtime.NODEJS_20_X,
     undefined, // layers
     role, // execution role,
     undefined, // env vars
