e5d3690a82075c0fe8dc50fe6d5a146591ecdeef
Rico Huijbers <rix0rrr@gmail.com>
2025-09-19 16:17:47 +0200

    Downgrade to 3.12

--- packages/amplify-graphql-searchable-transformer/src/cdk/create-streaming-lambda.ts
+++ packages/amplify-graphql-searchable-transformer/src/cdk/create-streaming-lambda.ts
@@ -30,7 +30,7 @@ export const createLambda = (
     `functions/${OpenSearchStreamingLambdaFunctionLogicalID}.zip`,
     parameterMap.get(OpenSearchStreamingLambdaHandlerName)!.valueAsString,
     path.resolve(__dirname, '..', '..', 'lib', 'streaming-lambda.zip'),
-    Runtime.PYTHON_3_13,
+    Runtime.PYTHON_3_12,
     [
       LayerVersion.fromLayerVersionArn(
         scope,
