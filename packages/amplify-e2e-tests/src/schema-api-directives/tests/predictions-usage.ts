00a555a1b8e4f40d560c1de32b283d7790bef6c4
Jon Wire <iambipedal@gmail.com>
2025-10-01 08:52:10 -0500

    fix: use regionalized S3 client in predictions test to resolve PermanentRedirect error

--- packages/amplify-e2e-tests/src/schema-api-directives/tests/predictions-usage.ts
+++ packages/amplify-e2e-tests/src/schema-api-directives/tests/predictions-usage.ts
@@ -40,8 +40,49 @@ export async function runTest(projectDir: string, testModule: any) {
   }
 }
 
-async function uploadImageFile(projectDir: string) {
-  const imageFilePath = path.join(__dirname, 'predictions-usage-image.jpg');
+// special handling needed to test prediction
+// This test will faile due to a possible AppSync bug, see details below the test code
+import path from 'path';
+import fs from 'fs-extra';
+import { S3Client, PutObjectCommand, GetBucketLocationCommand } from '@aws-sdk/client-s3';
+import gql from 'graphql-tag';
+import { addAuthWithDefault, addS3Storage, getBackendAmplifyMeta, addApi, amplifyPush } from 'amplify-category-api-e2e-core';
+
+import { getApiKey, configureAmplify, getConfiguredAppsyncClientAPIKeyAuth } from '../authHelper';
+import { updateSchemaInTestProject } from '../common';
+
+const imageKey = 'public/myimage.jpg';
+
+export async function runTest(projectDir: string, testModule: any) {
+  await addAuthWithDefault(projectDir);
+  await addS3Storage(projectDir);
+  await addApi(projectDir, { transformerVersion: 1 });
+  updateSchemaInTestProject(projectDir, testModule.schema);
+
+  await amplifyPush(projectDir);
+
+  await uploadImageFile(projectDir);
+
+  const apiKey = getApiKey(projectDir);
+  const awsconfig = configureAmplify(projectDir);
+  const appSyncClient = getConfiguredAppsyncClientAPIKeyAuth(awsconfig.aws_appsync_graphqlEndpoint, awsconfig.aws_appsync_region, apiKey);
+
+  try {
+    const result = await appSyncClient.query({
+      query: gql(query),
+      fetchPolicy: 'no-cache',
+    });
+
+    expect(result).toBeDefined();
+    const pollyURL = result.data.speakTranslatedImageText;
+    // check that return format is a url
+    expect(pollyURL).toMatch(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/);
+  } catch (err) {
+    // #error: the query will fail due to an AppSync bug, see below
+  }
+}
+
+async function getBucketRegion(bucketName: string): Promise<string> {
   const s3Client = new S3Client({
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -51,12 +92,32 @@ async function uploadImageFile(projectDir: string) {
     region: process.env.AWS_DEFAULT_REGION,
   });
 
+  const location = await s3Client.send(new GetBucketLocationCommand({ Bucket: bucketName }));
+  const region = location.LocationConstraint ?? 'us-east-1';
+  return region;
+}
+
+async function uploadImageFile(projectDir: string) {
+  const imageFilePath = path.join(__dirname, 'predictions-usage-image.jpg');
+
   const amplifyMeta = getBackendAmplifyMeta(projectDir);
   const storageResourceName = Object.keys(amplifyMeta.storage).find((key: any) => {
     return amplifyMeta.storage[key].service === 'S3';
   }) as any;
 
   const bucketName = amplifyMeta.storage[storageResourceName].output.BucketName;
+
+  // Get the bucket's actual region and create a regionalized S3 client
+  const bucketRegion = await getBucketRegion(bucketName);
+  const s3Client = new S3Client({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN,
+    },
+    region: bucketRegion,
+  });
+
   try {
     const fileStream = fs.createReadStream(imageFilePath);
     const uploadParams = {
