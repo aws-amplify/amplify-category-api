312f8c8faf8b75a24a49d045499ab228b89d7d5f
Jon Wire <iambipedal@gmail.com>
2025-10-03 11:19:52 -0500

    Fix amplify-category-api migration: update package.json and revert SSM client
    - Remove aws-sdk v2 dependency from package.json
    - Add missing v3 client dependencies: @aws-sdk/client-rds, @aws-sdk/client-rds-data, @aws-sdk/client-secrets-manager
    - Revert SSM client to support both v2/v3 until provider is migrated
    - Keep appSync-rds-walkthrough.ts v3 migration (RDS, SecretsManager, RDSData clients)
    - All tests passing with proper dual-client support

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/rds-resources/ssmClient.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/rds-resources/ssmClient.ts
@@ -7,6 +7,7 @@ import {
   PutParameterCommand,
   SSMClient as SSM_Client,
 } from '@aws-sdk/client-ssm';
+import aws from 'aws-sdk';
 
 export type Secret = {
   secretName: string;
@@ -26,8 +27,8 @@ export class SSMClient {
     return SSMClient.instance;
   };
 
-  // Use only AWS SDK v3 client
-  private constructor(private readonly ssmClient: SSM_Client) {}
+  // Support both v2 and v3 clients until provider is migrated
+  private constructor(private readonly ssmClient: aws.SSM | SSM_Client) {}
 
   /**
    * Returns a list of secret name value pairs
@@ -36,15 +37,24 @@ export class SSMClient {
     if (!secretNames || secretNames?.length === 0) {
       return [];
     }
-    
-    const result = await this.ssmClient.send(
-      new GetParametersCommand({
+    if (typeof (this.ssmClient as SSM_Client).send === 'function') {
+      const result = await (this.ssmClient as SSM_Client).send(
+        new GetParametersCommand({
+          Names: secretNames,
+          WithDecryption: true,
+        }),
+      );
+
+      return result ? result?.Parameters?.map(({ Name, Value }) => ({ secretName: Name, secretValue: Value })) : [];
+    }
+    const result = await (this.ssmClient as aws.SSM)
+      .getParameters({
         Names: secretNames,
         WithDecryption: true,
-      }),
-    );
+      })
+      .promise();
 
-    return result ? result?.Parameters?.map(({ Name, Value }) => ({ secretName: Name, secretValue: Value })) : [];
+    return result?.Parameters?.map(({ Name, Value }) => ({ secretName: Name, secretValue: Value }));
   };
 
   /**
@@ -54,21 +64,38 @@ export class SSMClient {
     let nextToken: string | undefined;
     const secretNames: string[] = [];
     do {
-      const result = await this.ssmClient.send(
-        new GetParametersByPathCommand({
-          Path: secretPath,
-          MaxResults: 10,
-          ParameterFilters: [
-            {
-              Key: 'Type',
-              Option: 'Equals',
-              Values: ['SecureString'],
-            },
-          ],
-          NextToken: nextToken,
-        }),
-      );
-      
+      let result;
+      if (typeof (this.ssmClient as SSM_Client).send === 'function') {
+        result = await (this.ssmClient as SSM_Client).send(
+          new GetParametersByPathCommand({
+            Path: secretPath,
+            MaxResults: 10,
+            ParameterFilters: [
+              {
+                Key: 'Type',
+                Option: 'Equals',
+                Values: ['SecureString'],
+              },
+            ],
+            NextToken: nextToken,
+          }),
+        );
+      } else {
+        result = await (this.ssmClient as aws.SSM)
+          .getParametersByPath({
+            Path: secretPath,
+            MaxResults: 10,
+            ParameterFilters: [
+              {
+                Key: 'Type',
+                Option: 'Equals',
+                Values: ['SecureString'],
+              },
+            ],
+            NextToken: nextToken,
+          })
+          .promise();
+      }
       secretNames.push(...result?.Parameters?.map((param) => param?.Name));
       nextToken = result?.NextToken;
     } while (nextToken);
@@ -79,26 +106,48 @@ export class SSMClient {
    * Sets the given secretName to the secretValue. If secretName is already present, it is overwritten.
    */
   setSecret = async (secretName: string, secretValue: string): Promise<void> => {
-    await this.ssmClient.send(
-      new PutParameterCommand({
-        Name: secretName,
-        Value: secretValue,
-        Type: 'SecureString',
-        Overwrite: true,
-      }),
-    );
+    if (typeof (this.ssmClient as SSM_Client).send === 'function') {
+      await (this.ssmClient as SSM_Client).send(
+        new PutParameterCommand({
+          Name: secretName,
+          Value: secretValue,
+          Type: 'SecureString',
+          Overwrite: true,
+        }),
+      );
+    } else {
+      await (this.ssmClient as aws.SSM)
+        .putParameter({
+          Name: secretName,
+          Value: secretValue,
+          Type: 'SecureString',
+          Overwrite: true,
+        })
+        .promise();
+    }
   };
 
   /**
    * Deletes secretName. If it already doesn't exist, this is treated as success. All other errors will throw.
    */
   deleteSecret = async (secretName: string): Promise<void> => {
-    try {
-      await this.ssmClient.send(new DeleteParameterCommand({ Name: secretName }));
-    } catch (err) {
-      if (err?.name !== 'ParameterNotFound') {
-        // if the value didn't exist in the first place, consider it deleted
-        throw err;
+    if (typeof (this.ssmClient as SSM_Client).send === 'function') {
+      try {
+        await (this.ssmClient as SSM_Client).send(new DeleteParameterCommand({ Name: secretName }));
+      } catch (err) {
+        if (err?.name !== 'ParameterNotFound') {
+          // if the value didn't exist in the first place, consider it deleted
+          throw err;
+        }
+      }
+    } else {
+      try {
+        await (this.ssmClient as aws.SSM).deleteParameter({ Name: secretName }).promise();
+      } catch (err) {
+        if (err?.code !== 'ParameterNotFound') {
+          // if the value didn't exist in the first place, consider it deleted
+          throw err;
+        }
       }
     }
   };
@@ -107,22 +156,33 @@ export class SSMClient {
    * Deletes all secrets in secretNames.If secret doesn't exist, this is treated as success. All other errors will throw.
    */
   deleteSecrets = async (secretNames: string[]): Promise<void> => {
-    try {
-      await this.ssmClient.send(new DeleteParametersCommand({ Names: secretNames }));
-    } catch (err) {
-      // if the value didn't exist in the first place, consider it deleted
-      if (err?.name !== 'ParameterNotFound') {
-        throw err;
+    if (typeof (this.ssmClient as SSM_Client).send === 'function') {
+      try {
+        await (this.ssmClient as SSM_Client).send(new DeleteParametersCommand({ Names: secretNames }));
+      } catch (err) {
+        // if the value didn't exist in the first place, consider it deleted
+        if (err?.name !== 'ParameterNotFound') {
+          throw err;
+        }
+      }
+    } else {
+      try {
+        await (this.ssmClient as aws.SSM).deleteParameters({ Names: secretNames }).promise();
+      } catch (err) {
+        // if the value didn't exist in the first place, consider it deleted
+        if (err?.code !== 'ParameterNotFound') {
+          throw err;
+        }
       }
     }
   };
 }
 
 // The Provider plugin holds all the configured service clients. Fetch from there.
-const getSSMClient = async (context: $TSContext): Promise<SSM_Client> => {
+const getSSMClient = async (context: $TSContext): Promise<aws.SSM | SSM_Client> => {
   const { client } = (await context.amplify.invokePluginMethod(context, 'awscloudformation', undefined, 'getConfiguredSSMClient', [
     context,
   ])) as any;
-  // Return only AWS SDK v3 client
+  // Return the client from provider (currently v2, will be v3 when provider is migrated)
   return client;
 };
