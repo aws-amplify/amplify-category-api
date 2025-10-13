ffa510596ef9e810162e736f4140ed219366acc0
Jon Wire <iambipedal@gmail.com>
2025-10-13 11:52:14 -0500

    fix: update @aws-sdk/client-lambda to support nodejs22.x runtime
    - Add explicit dependency on @aws-sdk/client-lambda@^3.908.0
    - Revert Lambda runtime back to nodejs22.x
    - Previous version 3.637.0 didn't include nodejs22.x in Runtime enum

--- packages/graphql-transformers-e2e-tests/src/LambdaHelper.ts
+++ packages/graphql-transformers-e2e-tests/src/LambdaHelper.ts
@@ -24,7 +24,7 @@ export class LambdaHelper {
         Code: {
           ZipFile: zipContents,
         },
-        Runtime: 'nodejs20.x',
+        Runtime: 'nodejs22.x',
         Handler: `${filePrefix}.handler`,
         Role: roleArn,
       }),
