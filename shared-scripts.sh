a0d300b7f0fbc7e723a398ebfb5e2d905f34fd93
Jon Wire <iambipedal@gmail.com>
2025-11-05 18:15:54 -0600

    chore: upgrade to node 20

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -132,6 +132,26 @@ function _lint {
   loadCacheFromBuildJob
   chmod +x codebuild_specs/scripts/lint_pr.sh && ./codebuild_specs/scripts/lint_pr.sh
 }
+function _setupNodeVersion {
+  local version=$1  # Version number passed as an argument
+
+  echo "Installing NVM and setting Node.js version to $version"
+
+  # Install NVM
+  curl -o - https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+  # Load NVM
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+  # Install and use the specified Node.js version
+  nvm install "$version"
+  nvm use "$version"
+
+  # Verify the Node.js version in use
+  echo "Node.js version in use:"
+  node -v
+}
 function _publishToLocalRegistry {
     echo "Publish To Local Registry"
     loadCacheFromBuildJob
