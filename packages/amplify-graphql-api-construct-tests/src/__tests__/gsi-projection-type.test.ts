573b43055fa65f41c39eb1f9b232b6d059e2ecb4
Test <not@used.com>
2025-12-10 13:31:15 +0100

    test(gsi-projection-type): refactor tests to use parameterized approach

--- packages/amplify-graphql-api-construct-tests/src/__tests__/gsi-projection-type.test.ts
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/gsi-projection-type.test.ts
@@ -7,11 +7,9 @@ jest.setTimeout(DURATION_1_HOUR);
 
 describe('GSI Projection Type', () => {
   let projRoot: string;
-  let projFolderName: string;
 
   beforeEach(async () => {
-    projFolderName = 'gsiprojectiontype';
-    projRoot = await createNewProjectDir(projFolderName);
+    projRoot = await createNewProjectDir('gsiprojectiontype');
   });
 
   afterEach(async () => {
@@ -20,50 +18,44 @@ describe('GSI Projection Type', () => {
     } catch (_) {
       /* No-op */
     }
-
     deleteProjectDir(projRoot);
+    if (global.gc) global.gc();
   });
 
-  test('creates GSI with KEYS_ONLY projection', async () => {
-    const templatePath = path.resolve(path.join(__dirname, 'backends', 'gsi-projection-type', 'keys-only'));
+  test.each([
+    [
+      'KEYS_ONLY',
+      'keys-only',
+      (gsi: any) => {
+        expect(gsi.Projection.ProjectionType).toBe('KEYS_ONLY');
+      },
+    ],
+    [
+      'INCLUDE',
+      'include',
+      (gsi: any) => {
+        expect(gsi.Projection.ProjectionType).toBe('INCLUDE');
+        expect(gsi.Projection.NonKeyAttributes).toEqual(expect.arrayContaining(['name', 'price']));
+      },
+    ],
+    [
+      'ALL',
+      'all',
+      (gsi: any) => {
+        expect(gsi.Projection.ProjectionType).toBe('ALL');
+      },
+    ],
+  ])('creates GSI with %s projection', async (_projectionType, templateDir, validator) => {
+    const templatePath = path.resolve(path.join(__dirname, 'backends', 'gsi-projection-type', templateDir));
     const name = await initCDKProject(projRoot, templatePath);
     const outputs = await cdkDeploy(projRoot, '--all');
     const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
-    const tableName = `Product-${apiId}-NONE`;
-    const table = await getDDBTable(tableName, region);
+    const table = await getDDBTable(`Product-${apiId}-NONE`, region);
+    const gsi = table.Table.GlobalSecondaryIndexes[0];
 
     expect(table.Table.GlobalSecondaryIndexes).toBeDefined();
     expect(table.Table.GlobalSecondaryIndexes.length).toBe(1);
-    expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byCategory');
-    expect(table.Table.GlobalSecondaryIndexes[0].Projection.ProjectionType).toBe('KEYS_ONLY');
-  });
-
-  test('creates GSI with INCLUDE projection', async () => {
-    const templatePath = path.resolve(path.join(__dirname, 'backends', 'gsi-projection-type', 'include'));
-    const name = await initCDKProject(projRoot, templatePath);
-    const outputs = await cdkDeploy(projRoot, '--all');
-    const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
-    const tableName = `Product-${apiId}-NONE`;
-    const table = await getDDBTable(tableName, region);
-
-    expect(table.Table.GlobalSecondaryIndexes).toBeDefined();
-    expect(table.Table.GlobalSecondaryIndexes.length).toBe(1);
-    expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byCategory');
-    expect(table.Table.GlobalSecondaryIndexes[0].Projection.ProjectionType).toBe('INCLUDE');
-    expect(table.Table.GlobalSecondaryIndexes[0].Projection.NonKeyAttributes).toEqual(expect.arrayContaining(['name', 'price']));
-  });
-
-  test('creates GSI with ALL projection', async () => {
-    const templatePath = path.resolve(path.join(__dirname, 'backends', 'gsi-projection-type', 'all'));
-    const name = await initCDKProject(projRoot, templatePath);
-    const outputs = await cdkDeploy(projRoot, '--all');
-    const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
-    const tableName = `Product-${apiId}-NONE`;
-    const table = await getDDBTable(tableName, region);
-
-    expect(table.Table.GlobalSecondaryIndexes).toBeDefined();
-    expect(table.Table.GlobalSecondaryIndexes.length).toBe(1);
-    expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byCategory');
-    expect(table.Table.GlobalSecondaryIndexes[0].Projection.ProjectionType).toBe('ALL');
+    expect(gsi.IndexName).toBe('byCategory');
+    validator(gsi);
   });
 });
