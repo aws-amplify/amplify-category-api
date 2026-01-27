d986592f8ce8503e2c3f37dcaf0443bf7436baf7
Ahmed Hamouda <ahmedhamouda78@users.noreply.github.com>
2026-01-27 22:33:10 +0100

    feat: add projection support for @index directive (#3359)

--- packages/amplify-graphql-directives/src/directives/index-directive.ts
+++ packages/amplify-graphql-directives/src/directives/index-directive.ts
@@ -2,7 +2,18 @@ import { Directive } from './directive';
 
 const name = 'index';
 const definition = /* GraphQL */ `
-  directive @${name}(name: String, sortKeyFields: [String], queryField: String) repeatable on FIELD_DEFINITION
+  directive @${name}(name: String, sortKeyFields: [String], queryField: String, projection: ProjectionInput) repeatable on FIELD_DEFINITION
+  
+  input ProjectionInput {
+    type: ProjectionType
+    nonKeyAttributes: [String]
+  }
+  
+  enum ProjectionType {
+    ALL
+    KEYS_ONLY
+    INCLUDE
+  }
 `;
 const defaults = {};
 
