04b59288c5c75dbe6434fcb6a50f434ef9590ceb
Galib Sarayev <sarayev@amazon.com>
2026-03-12 10:27:38 +0000

    fix: run prettier on amplify-table test files
    The PR lint check failed because amplify-table-1.test.ts and
    amplify-table-5.test.ts had Prettier formatting issues (test function
    argument wrapping). Run prettier --write to fix.

--- packages/amplify-graphql-api-construct-tests/src/__tests__/amplify-table-1.test.ts
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/amplify-table-1.test.ts
@@ -24,55 +24,59 @@ describe('CDK amplify table 1', () => {
     deleteProjectDir(projRoot);
   });
 
-  test('can update multiple GSIs along with other non-GSI updates', async () => {
-    const templatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo'));
-    const name = await initCDKProject(projRoot, templatePath);
-    const outputs = await cdkDeploy(projRoot, '--all');
-    const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
-    const tableName = `Todo-${apiId}-NONE`;
-    const table = await getDDBTable(tableName, region);
-    expect(table).toBeDefined();
-    expect(table.Table.BillingModeSummary.BillingMode).toBe('PAY_PER_REQUEST');
-    expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byName');
-    // Important: When SSE is undefined or set to false,
-    // it doesn't mean SSE is disabled, it means that the table is using AWS owned key for SSE.
-    expect(table.Table.SSEDescription).toBeUndefined();
-    expect(table.Table.StreamSpecification.StreamViewType).toBe('NEW_AND_OLD_IMAGES');
+  test(
+    'can update multiple GSIs along with other non-GSI updates',
+    async () => {
+      const templatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo'));
+      const name = await initCDKProject(projRoot, templatePath);
+      const outputs = await cdkDeploy(projRoot, '--all');
+      const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
+      const tableName = `Todo-${apiId}-NONE`;
+      const table = await getDDBTable(tableName, region);
+      expect(table).toBeDefined();
+      expect(table.Table.BillingModeSummary.BillingMode).toBe('PAY_PER_REQUEST');
+      expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byName');
+      // Important: When SSE is undefined or set to false,
+      // it doesn't mean SSE is disabled, it means that the table is using AWS owned key for SSE.
+      expect(table.Table.SSEDescription).toBeUndefined();
+      expect(table.Table.StreamSpecification.StreamViewType).toBe('NEW_AND_OLD_IMAGES');
 
-    // Verify the tags on the table
-    const tableTags = await getDDBTableTags(table.Table.TableArn, region);
-    expect(tableTags.Tags).toBeDefined();
-    expect(tableTags.Tags.length).toBe(3);
-    expect(tableTags.Tags).toEqual(
-      expect.arrayContaining([
-        { Key: 'amplify:deployment-type', Value: 'sandbox-original' },
-        { Key: 'amplify:friendly-name', Value: 'amplifyData-original' },
-        { Key: 'created-by', Value: 'amplify-original' },
-      ]),
-    );
+      // Verify the tags on the table
+      const tableTags = await getDDBTableTags(table.Table.TableArn, region);
+      expect(tableTags.Tags).toBeDefined();
+      expect(tableTags.Tags.length).toBe(3);
+      expect(tableTags.Tags).toEqual(
+        expect.arrayContaining([
+          { Key: 'amplify:deployment-type', Value: 'sandbox-original' },
+          { Key: 'amplify:friendly-name', Value: 'amplifyData-original' },
+          { Key: 'created-by', Value: 'amplify-original' },
+        ]),
+      );
 
-    const updateTemplatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo', 'updateIndex'));
-    updateCDKAppWithTemplate(projRoot, updateTemplatePath);
-    await cdkDeploy(projRoot, '--all', { timeoutMs: DURATION_30_MINUTES });
-    const updatedTable = await getDDBTable(tableName, region);
-    expect(updatedTable).toBeDefined();
-    expect(updatedTable.Table.BillingModeSummary.BillingMode).toBe('PROVISIONED');
-    expect(updatedTable.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byName2');
-    expect(updatedTable.Table.SSEDescription.Status).toBe('ENABLED');
-    expect(updatedTable.Table.StreamSpecification.StreamViewType).toBe('KEYS_ONLY');
+      const updateTemplatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo', 'updateIndex'));
+      updateCDKAppWithTemplate(projRoot, updateTemplatePath);
+      await cdkDeploy(projRoot, '--all', { timeoutMs: DURATION_30_MINUTES });
+      const updatedTable = await getDDBTable(tableName, region);
+      expect(updatedTable).toBeDefined();
+      expect(updatedTable.Table.BillingModeSummary.BillingMode).toBe('PROVISIONED');
+      expect(updatedTable.Table.GlobalSecondaryIndexes[0].IndexName).toBe('byName2');
+      expect(updatedTable.Table.SSEDescription.Status).toBe('ENABLED');
+      expect(updatedTable.Table.StreamSpecification.StreamViewType).toBe('KEYS_ONLY');
 
-    // Verify the tags on the table after update
-    const updatedTableTags = await getDDBTableTags(updatedTable.Table.TableArn, region);
-    expect(updatedTableTags.Tags).toBeDefined();
-    expect(updatedTableTags.Tags.length).toBe(5);
-    expect(updatedTableTags.Tags).toEqual(
-      expect.arrayContaining([
-        { Key: 'amplify:deployment-type', Value: 'pipeline-updated' },
-        { Key: 'amplify:deployment-branch', Value: 'main-updated' },
-        { Key: 'amplify:appId', Value: '123456-updated' },
-        { Key: 'amplify:friendly-name', Value: 'amplifyData-updated' },
-        { Key: 'created-by', Value: 'amplify-updated' },
-      ]),
-    );
-  }, DURATION_45_MINUTES);
+      // Verify the tags on the table after update
+      const updatedTableTags = await getDDBTableTags(updatedTable.Table.TableArn, region);
+      expect(updatedTableTags.Tags).toBeDefined();
+      expect(updatedTableTags.Tags.length).toBe(5);
+      expect(updatedTableTags.Tags).toEqual(
+        expect.arrayContaining([
+          { Key: 'amplify:deployment-type', Value: 'pipeline-updated' },
+          { Key: 'amplify:deployment-branch', Value: 'main-updated' },
+          { Key: 'amplify:appId', Value: '123456-updated' },
+          { Key: 'amplify:friendly-name', Value: 'amplifyData-updated' },
+          { Key: 'created-by', Value: 'amplify-updated' },
+        ]),
+      );
+    },
+    DURATION_45_MINUTES,
+  );
 });
--- packages/amplify-graphql-api-construct-tests/src/__tests__/amplify-table-5.test.ts
+++ packages/amplify-graphql-api-construct-tests/src/__tests__/amplify-table-5.test.ts
@@ -24,37 +24,41 @@ describe('CDK amplify table 5', () => {
     deleteProjectDir(projRoot);
   });
 
-  test('datatype change on indexed field should replace the index', async () => {
-    const templatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo'));
-    const name = await initCDKProject(projRoot, templatePath);
-    const outputs = await cdkDeploy(projRoot, '--all');
-    const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
-    const tableName = `Todo-${apiId}-NONE`;
-    const table = await getDDBTable(tableName, region);
-    expect(table.Table.AttributeDefinitions).toHaveLength(2);
-    const nameAttr = table.Table.AttributeDefinitions.find((attr) => attr.AttributeName === 'name');
-    expect(nameAttr.AttributeType).toEqual('S');
-    expect(table.Table.GlobalSecondaryIndexes).toHaveLength(1);
-    expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toEqual('byName');
+  test(
+    'datatype change on indexed field should replace the index',
+    async () => {
+      const templatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo'));
+      const name = await initCDKProject(projRoot, templatePath);
+      const outputs = await cdkDeploy(projRoot, '--all');
+      const { awsAppsyncApiId: apiId, awsAppsyncRegion: region } = outputs[name];
+      const tableName = `Todo-${apiId}-NONE`;
+      const table = await getDDBTable(tableName, region);
+      expect(table.Table.AttributeDefinitions).toHaveLength(2);
+      const nameAttr = table.Table.AttributeDefinitions.find((attr) => attr.AttributeName === 'name');
+      expect(nameAttr.AttributeType).toEqual('S');
+      expect(table.Table.GlobalSecondaryIndexes).toHaveLength(1);
+      expect(table.Table.GlobalSecondaryIndexes[0].IndexName).toEqual('byName');
 
-    // deploy with modified attribute type
-    const updateTemplatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo', 'attributeTypeChange'));
-    updateCDKAppWithTemplate(projRoot, updateTemplatePath);
-    await cdkDeploy(projRoot, '--all', { timeoutMs: DURATION_30_MINUTES });
-    const modifiedTable = await getDDBTable(tableName, region);
-    expect(modifiedTable.Table.AttributeDefinitions).toHaveLength(3);
-    const modifiedNameAttr = modifiedTable.Table.AttributeDefinitions.find((attr) => attr.AttributeName === 'name');
-    expect(modifiedNameAttr.AttributeType).toEqual('N');
-    expect(modifiedTable.Table.GlobalSecondaryIndexes).toHaveLength(2);
-    expect(modifiedTable.Table.GlobalSecondaryIndexes).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          IndexName: 'byNameDescription',
-        }),
-        expect.objectContaining({
-          IndexName: 'byName',
-        }),
-      ]),
-    );
-  }, DURATION_45_MINUTES);
+      // deploy with modified attribute type
+      const updateTemplatePath = path.resolve(path.join(__dirname, 'backends', 'amplify-table', 'simple-todo', 'attributeTypeChange'));
+      updateCDKAppWithTemplate(projRoot, updateTemplatePath);
+      await cdkDeploy(projRoot, '--all', { timeoutMs: DURATION_30_MINUTES });
+      const modifiedTable = await getDDBTable(tableName, region);
+      expect(modifiedTable.Table.AttributeDefinitions).toHaveLength(3);
+      const modifiedNameAttr = modifiedTable.Table.AttributeDefinitions.find((attr) => attr.AttributeName === 'name');
+      expect(modifiedNameAttr.AttributeType).toEqual('N');
+      expect(modifiedTable.Table.GlobalSecondaryIndexes).toHaveLength(2);
+      expect(modifiedTable.Table.GlobalSecondaryIndexes).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            IndexName: 'byNameDescription',
+          }),
+          expect.objectContaining({
+            IndexName: 'byName',
+          }),
+        ]),
+      );
+    },
+    DURATION_45_MINUTES,
+  );
 });
