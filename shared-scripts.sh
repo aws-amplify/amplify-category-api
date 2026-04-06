3cb6fc23393f8dd45b6627e0b1911dc9852606fa
Galib Sarayev <sarayev@amazon.com>
2026-04-06 09:30:49 +0000

    fix(e2e): use Node 24 in E2E/CDK test setup to fix lru-cache crash

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -334,17 +334,17 @@ function _setupE2ETestsLinux {
     echo "Setup E2E Tests Linux"
     loadCacheFromBuildJob
     loadCache verdaccio-cache $CODEBUILD_SRC_DIR/../verdaccio-cache
-    # Ignore engines while we're still on Node 18.x
-    yarn config set ignore-engines true
+    _setupNodeVersion $AMPLIFY_NODE_VERSION
     _installCLIFromLocalRegistry
     _loadTestAccountCredentials
     _setShell
 }
 
 function _setupCDKTestsLinux {
-    echo "Setup E2E Tests Linux"
+    echo "Setup CDK Tests Linux"
     loadCacheFromBuildJob
     loadCache verdaccio-cache $CODEBUILD_SRC_DIR/../verdaccio-cache
+    _setupNodeVersion $AMPLIFY_NODE_VERSION
     _installCLIFromLocalRegistry
     yarn package
     _loadTestAccountCredentials
