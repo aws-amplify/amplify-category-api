76f6f367ee8e7752b78ab31e08f305121dd10632
sarayev <sarayev@amazon.com>
2026-09-18 06:28:55 +0000

    fix(graphql-auth-transformer): no-op marker to trigger e2e baseline run on release-api-plugin-stable

--- packages/amplify-graphql-auth-transformer/src/index.ts
+++ packages/amplify-graphql-auth-transformer/src/index.ts
@@ -5,3 +5,4 @@ export { RoleDefinition, RolesByProvider } from './utils/role-definition';
 export { AccessControlMatrix } from './accesscontrol';
 export { getAuthDirectiveRules } from './utils';
 // No-op change to trigger publish
+// e2e baseline run marker (release-api-plugin-stable, no functional change)
